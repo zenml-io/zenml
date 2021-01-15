be24262ca0fa31ff34e62d8b3d3f6eb39b54cef5
benkoller <kontakt@benkoller.de>
2021-01-15 17:27:15 +0100

    orchestration logic

--- zenml/core/backends/orchestrator/kubernetes/orchestrator_kubernetes_backend.py
+++ zenml/core/backends/orchestrator/kubernetes/orchestrator_kubernetes_backend.py
@@ -13,26 +13,162 @@
 #  permissions and limitations under the License.
 """Definition of the Kubeflow Orchestrator Backend"""
 
+import base64
+import json
+import time
+import os
 from typing import Text
+from typing import Dict, Any
+
+from kubernetes import client as k8s_client
+from kubernetes import config as k8s_config
+from kubernetes.config.config_exception import ConfigException
 
 from tfx.orchestration import pipeline
 
 from zenml.core.backends.orchestrator.local.orchestrator_local_backend import \
     OrchestratorLocalBackend
+from zenml.core.repo.repo import Repository
+from zenml.core.standards import standard_keys as keys
+from zenml.utils import path_utils
+from zenml.utils.string_utils import to_dns1123, get_id
+from zenml.utils.constants import ZENML_BASE_IMAGE_NAME, K8S_ENTRYPOINT
+from zenml.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+EXTRACTED_TAR_DIR_NAME = 'zenml_working'
+TAR_PATH_ARG = 'tar_path'
+SOURCE_DISK_IMAGE = "projects/cos-cloud/global/images/cos-85-13310-1041-38"
+STAGING_AREA = 'staging'
+DEFAULT_K8S_CONFIG = os.path.join(os.environ["HOME"], '.kube/config-lol')
 
 
 class OrchestratorKubernetesBackend(OrchestratorLocalBackend):
     """
     Runs pipeline on a Kubernetes cluster.
 
-    This backend is not implemented yet.
+    This orchestrator creates a .tar.gz of the current ZenML repository, sends
+    it over to the artifact store, then launches a job in a Kubernetes cluster
+    taken from your environment or specified via a passed-on kubectl config.
     """
     BACKEND_TYPE = 'kubernetes'
 
-    def __init__(self, image: Text = None, **kwargs):
+    def __init__(self,
+                 image: Text = 'eu.gcr.io/maiot-zenml/zenml:k8s-testing',
+                 job_prefix: Text = 'zenml-',
+                 extra_labels: Dict[Text, Any] = None,
+                 extra_annotations: Dict[Text, Any] = None,
+                 namespace: Text = None,
+                 kubernetes_config_path: Text = DEFAULT_K8S_CONFIG,
+                 **kwargs):
         self.image = image
-        super().__init__(**kwargs)
-        raise NotImplementedError('Its coming soon!')
+        self.job_prefix = job_prefix
+        self.extra_labels = extra_labels  # custom k8s labels
+        self.extra_annotations = extra_annotations  # custom k8s annotations
+        self.namespace = namespace
 
-    def run(self, tfx_pipeline: pipeline.Pipeline):
-        raise NotImplementedError('Coming soon!')
+        try:
+            k8s_config.load_kube_config(kubernetes_config_path)
+        except ConfigException as cfg_exc:
+            logger.error("The path you provided does not contain a valid"
+                         " Kubernetes config.")
+            raise
+        super().__init__(**kwargs)
+
+    def create_job_object(self, config):
+        experiment_name = config[keys.GlobalKeys.ENV][
+            keys.EnvironmentKeys.EXPERIMENT_NAME]
+        job_name = to_dns1123(f'{self.job_prefix}{experiment_name}', length=63)
+        labels = self.extra_labels or {}
+        job_labels = {
+            "app": "zenml",
+            "pipeline": experiment_name,
+            "datasource-id": config[keys.GlobalKeys.DATASOURCE][
+                keys.DatasourceKeys.ID],
+            "pipeline-id": get_id(experiment_name)
+        }
+        labels.update(job_labels)  # make sure our labels are present
+
+        config_encoded = base64.b64encode(json.dumps(config).encode()).decode(
+            'utf-8')  # kubernetes needs the config as string
+
+        command = ['python', '-m', K8S_ENTRYPOINT, 'run_pipeline',
+                   '--config_b64', config_encoded]
+
+        container = k8s_client.V1Container(
+            name=job_name,
+            image=self.image,
+            command=command)
+
+        # Create and configure a spec section
+        template = k8s_client.V1PodTemplateSpec(
+            metadata=k8s_client.V1ObjectMeta(labels=labels),
+            spec=k8s_client.V1PodSpec(restart_policy="Never",
+                                      containers=[container]))
+
+        # Create the specification of deployment
+        spec = k8s_client.V1JobSpec(
+            template=template,
+            backoff_limit=4)
+
+        # Instantiate the job object
+        job = k8s_client.V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=k8s_client.V1ObjectMeta(
+                annotations=self.extra_annotations,
+                labels=labels,
+                name=job_name,
+                namespace=self.namespace),
+            spec=spec)
+
+        return job
+
+    def launch_job(self, config: Dict[Text, Any]):
+        batch_client = k8s_client.BatchV1Api()
+        job_object = self.create_job_object(config)
+
+        namespace = self.namespace or 'default'
+
+        api_response = batch_client.create_namespaced_job(
+            body=job_object,
+            namespace=namespace)
+        print(str(api_response.status))
+
+        return None
+
+    def run(self, config: Dict[Text, Any]):
+        # Extract the paths to create the tar
+        logger.info('Orchestrating pipeline on Kubernetes..')
+
+        repo: Repository = Repository.get_instance()
+        repo_path = repo.path
+        config_dir = repo.zenml_config.config_dir
+        tar_file_name = \
+            f'{EXTRACTED_TAR_DIR_NAME}_{str(int(time.time()))}.tar.gz'
+        path_to_tar = os.path.join(config_dir, tar_file_name)
+
+        # Create tarfile but exclude .zenml folder if exists
+        path_utils.create_tarfile(repo_path, path_to_tar)
+        logger.info(f'Created tar of current repository at: {path_to_tar}')
+
+        # Upload tar to artifact store
+        store_path = \
+            config[keys.GlobalKeys.ENV][keys.EnvironmentKeys.ARTIFACT_STORE]
+        store_staging_area = os.path.join(store_path, STAGING_AREA)
+        store_path_to_tar = os.path.join(store_staging_area, tar_file_name)
+        path_utils.copy(path_to_tar, store_path_to_tar)
+        logger.info(f'Copied tar to artifact store at: {store_path_to_tar}')
+
+        # Remove tar
+        path_utils.rm_dir(path_to_tar)
+        logger.info(f'Removed tar at: {path_to_tar}')
+
+        # Append path of tar in config orchestrator utils
+        config[keys.GlobalKeys.ENV][keys.EnvironmentKeys.BACKENDS][
+            OrchestratorKubernetesBackend.BACKEND_KEY][keys.BackendKeys.ARGS][
+            TAR_PATH_ARG] = store_path_to_tar
+
+        # Launch the instance
+        self.launch_job(config)
