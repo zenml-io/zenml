604d1ab2cfd750bbf2efe27cbd4e2a9301957164
Baris Can Durak <bariscandurak@hotmail.com>
2021-02-08 12:56:38 +0100

    new entrypoint for aws

--- zenml/utils/constants.py
+++ zenml/utils/constants.py
@@ -58,5 +58,5 @@ EVALUATOR_MODULE_FN = 'zenml.core.components.evaluator.evaluator_module'
 
 # GCP Orchestration
 GCP_ENTRYPOINT = 'zenml.core.backends.orchestrator.entrypoint'
-AWS_ENTRYPOINT = 'zenml.core.backends.orchestrator.entrypoint'
+AWS_ENTRYPOINT = 'zenml.core.backends.orchestrator.aws.entrypoint'
 K8S_ENTRYPOINT = 'zenml.core.backends.orchestrator.entrypoint'
