8edb3688ad4ce550ed29f3cc9f576f926363fde0
Baris Can Durak <bariscandurak@hotmail.com>
2021-02-09 19:06:55 +0100

    reached a checkpoint

--- zenml/core/backends/orchestrator/aws/utils.py
+++ zenml/core/backends/orchestrator/aws/utils.py
@@ -12,23 +12,23 @@ AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
 AWS_REGION = 'AWS_REGION'
 
 
-def _get_container_params(config, region):
+def _get_container_params(config):
     config_encoded = base64.b64encode(json.dumps(config).encode())
     return f'python -m {AWS_ENTRYPOINT} run_pipeline --config_b64 ' \
-           f'{config_encoded.decode()} --region {region}'
+           f'{config_encoded.decode()}'
 
 
 def get_startup_script(config: Dict,
                        region: Text,
                        zenml_image: Text = ZENML_BASE_IMAGE_NAME):
-    c_params = _get_container_params(config, region)
+    c_params = _get_container_params(config)
     return f"#!/bin/bash\n" \
            f"mkdir ~/aws_config\n" \
            f"touch ~/aws_config/config\n" \
            f"echo \"[default]\">>~/aws_config/config\n" \
            f"echo \"region = {region}\">>~/aws_config/config\n" \
            f"sudo HOME=/home/root docker run --net=host " \
-           f"--env AWS_REGION={region} -v ~/aws_config:~/.aws " \
+           f"--env AWS_REGION={region} -v ~/aws_config:/root/.aws " \
            f"{zenml_image} {c_params}"
 
 
