7396f0a480bf605a5469e1e3bb60d0fe6de9cd6d
Nicholas Junge <nicholas@maiot.io>
2021-02-16 10:29:26 +0100

    Add checks and exits to avoid local setup destruction

--- tests/testing.sh
+++ tests/testing.sh
@@ -13,6 +13,26 @@
 # or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 #
+#! /usr/bin/env bash
+
+# bash way of splitting of the current directory name
+CWD=${PWD##*/}
+ZENML_DIR=".zenml"
+GIT=".git"
+PIPELINES="pipelines"
+
+if [ -d "$ZENML_DIR" ] || [ -d "$PIPELINES" ] || [ -d "$GIT" ]; then
+  # Take action if $DIR exists. #
+  echo "Either the .zenml, .git or pipelines directory exists already. Make sure to run tests in a clean directory."
+  exit 1
+fi
+
+# make sure that the current directory is tests, otherwise complain
+if [ "$CWD" != "tests" ]; then
+  echo "Make sure to run ZenML tests directly from the tests directory."
+  exit 1
+fi
+
 git init
 git add .
 git commit -m 'test commit'
@@ -21,7 +41,7 @@ python generate_test_pipelines.py
 
 pytest ../zenml
 
-rm -rf .git
-rm -rf .zenml
-rm -rf pipelines
+rm -rf $GIT
+rm -rf $ZENML_DIR
+rm -rf $PIPELINES
 rm .gitignore
