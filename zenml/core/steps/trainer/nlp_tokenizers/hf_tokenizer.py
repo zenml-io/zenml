2a4ae6cae731d38a0a8a1d7e7a9827f9895e96ad
Nicholas Junge <nicholas@maiot.io>
2021-02-22 16:11:37 +0100

    Hardcoded tokenizer for now until figuring out how to HF tokenizers from config

--- zenml/core/steps/trainer/nlp_tokenizers/hf_tokenizer.py
+++ zenml/core/steps/trainer/nlp_tokenizers/hf_tokenizer.py
@@ -15,13 +15,16 @@
 import tensorflow as tf
 import tensorflow_transform as tft
 from typing import Text, List, Any
+from zenml.utils import path_utils
 from zenml.core.steps.trainer.base_trainer import BaseTrainerStep
 
-try:
-    import tokenizers
-    from tokenizers import Tokenizer
-except ImportError:
-    print("Error: Library \"tokenizers\" not installed.")
+import tokenizers
+import tokenizers.models
+import tokenizers.implementations
+from tokenizers.implementations import BertWordPieceTokenizer
+
+# except ImportError:
+#     print("Error: Library \"tokenizers\" not installed.")
 
 
 class HFTokenizerStep(BaseTrainerStep):
@@ -34,38 +37,71 @@ class HFTokenizerStep(BaseTrainerStep):
                  special_tokens: List[Text] = None,
                  train_files: Any = None,
                  eval_files: Any = None,
-                 ):
+                 batch_size: int = 1024,
+                 **kwargs):
 
         super(HFTokenizerStep, self).__init__(train_files=train_files,
-                                              eval_files=eval_files)
+                                              eval_files=eval_files,
+                                              vocab_size=vocab_size,
+                                              min_frequency=min_frequency,
+                                              model=model,
+                                              tokenizer=tokenizer,
+                                              special_tokens=special_tokens,
+                                              batch_size=batch_size,
+                                              **kwargs)
 
         self.model = model
+        self.batch_size = batch_size
 
         # for predefined tokenizer implementations from HuggingFace
-        if tokenizer is not None:
-            self.tokenizer = tokenizer
-        else:
-            # for customized tokenizers
-            self.tokenizer = Tokenizer(self.model)
+        # if tokenizer is not None:
+        #     self.tokenizer = tokenizer
+        # else:
+        # for customized tokenizers
+        self.tokenizer = BertWordPieceTokenizer()
         self.vocab_size = vocab_size
         self.min_frequency = min_frequency
         self.special_tokens = special_tokens or []
+
+    @staticmethod
+    def _gzip_reader_fn(filenames):
+        """
+        Small utility returning a record reader that can read gzipped files.
+
+        Args:
+            filenames: Names of the compressed TFRecord data files.
+        """
+        return tf.data.TFRecordDataset(filenames, compression_type='GZIP')
 
     def input_fn(self,
                  file_pattern: List[Text],
                  tf_transform_output: tft.TFTransformOutput = None):
 
-        return tf.data.TFRecordDataset(filenames=file_pattern,
-                                       compression_type="GZIP")
+        features = {"category": tf.io.FixedLenFeature([], tf.int64),
+                    "label": tf.io.FixedLenFeature([], tf.int64),
+                    "news": tf.io.FixedLenFeature([], tf.string)}
+
+        dataset = tf.data.experimental.make_batched_features_dataset(
+            file_pattern=file_pattern,
+            batch_size=self.batch_size,
+            features=features,
+            reader=self._gzip_reader_fn,
+            num_epochs=1)
+
+        return dataset
 
     def model_fn(self,
                  train_dataset: tf.data.Dataset,
                  eval_dataset: tf.data.Dataset):
 
-        train_dataset = train_dataset.batch(1024).as_numpy_iterator()
-        eval_dataset = eval_dataset.batch(1024).as_numpy_iterator()
+        train_dataset = train_dataset.as_numpy_iterator()
 
-        self.tokenizer.train_from_iterator(train_dataset, trainer=None)
+        news_in_memory = list(train_dataset)[0]["news"]
+
+        for i, n in enumerate(news_in_memory):
+            news_in_memory[i] = n.decode("utf-8")
+
+        self.tokenizer.train_from_iterator(news_in_memory)
 
         return self.tokenizer
 
@@ -75,6 +111,8 @@ class HFTokenizerStep(BaseTrainerStep):
 
         tokenizer = self.model_fn(train_dataset=train_dataset,
                                   eval_dataset=eval_dataset)
+
+        path_utils.create_dir_if_not_exists(self.serving_model_dir)
 
         tokenizer.save_model(self.serving_model_dir)
 
