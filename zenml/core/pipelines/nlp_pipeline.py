19000b70871cc9ef30b256561cdc182d275c4432
Nicholas Junge <nicholas@maiot.io>
2021-02-22 16:10:24 +0100

    Trainer expects train and eval output splits, so add a dummy split

--- zenml/core/pipelines/nlp_pipeline.py
+++ zenml/core/pipelines/nlp_pipeline.py
@@ -18,6 +18,7 @@ from typing import Dict, Text, Any, List, Optional
 from tfx.components.schema_gen.component import SchemaGen
 from tfx.components.statistics_gen.component import StatisticsGen
 from zenml.core.components.data_gen.component import DataGen
+from zenml.core.components.split_gen.component import SplitGen
 from tfx.components.transform.component import Transform
 from tfx.components.trainer.component import Trainer
 from zenml.core.pipelines.base_pipeline import BasePipeline
@@ -31,6 +32,7 @@ from zenml.utils import constants
 from zenml.core.steps.preprocesser.base_preprocesser import \
     BasePreprocesserStep
 from zenml.core.steps.trainer.base_trainer import BaseTrainerStep
+from zenml.core.steps.split.base_split_step import BaseSplit
 
 TOKENIZER = "tokenizer"
 
@@ -65,6 +67,8 @@ class NLPPipeline(BasePipeline):
             source_args=data_config[keys.StepKeys.ARGS]
         ).with_id(GDPComponent.DataGen.name)
 
+        datapoints = data.outputs.examples
+
         statistics_data = StatisticsGen(
             examples=data.outputs.examples
         ).with_id(GDPComponent.DataStatistics.name)
@@ -73,11 +77,22 @@ class NLPPipeline(BasePipeline):
             statistics=statistics_data.outputs.output,
         ).with_id(GDPComponent.DataSchema.name)
 
+        split_config = steps[keys.TrainingSteps.SPLIT]
+        splits = SplitGen(
+            input_examples=datapoints,
+            source=split_config[keys.StepKeys.SOURCE],
+            source_args=split_config[keys.StepKeys.ARGS],
+            schema=schema_data.outputs.schema,
+            statistics=statistics_data.outputs.output,
+        ).with_id(GDPComponent.SplitGen.name)
+
+        examples = splits.outputs.examples
+
         component_list.extend([data,
                                statistics_data,
-                               schema_data])
+                               schema_data,
+                               splits])
 
-        datapoints = data.outputs.examples
         schema = schema_data.outputs.schema
 
         #############
@@ -98,7 +113,7 @@ class NLPPipeline(BasePipeline):
             tokenizer_backend.get_custom_config())
 
         tokenizer = Trainer(
-            transformed_examples=data.outputs.examples,
+            examples=examples,
             run_fn=constants.TRAINER_FN,
             schema=schema,
             train_args=trainer_pb2.TrainArgs(),
@@ -107,6 +122,8 @@ class NLPPipeline(BasePipeline):
         ).with_id(TOKENIZER)
 
         component_list.extend([tokenizer])
+
+        return component_list
 
         #################
         # PREPROCESSING #
@@ -118,7 +135,7 @@ class NLPPipeline(BasePipeline):
             custom_config=steps[keys.TrainingSteps.PREPROCESSER]
         ).with_id(GDPComponent.Transform.name)
 
-        component_list.extend([transform])
+        # component_list.extend([transform])
 
         ############
         # TRAINING #
@@ -147,14 +164,14 @@ class NLPPipeline(BasePipeline):
             **training_kwargs
         ).with_id(GDPComponent.Trainer.name)
 
-        component_list.extend([trainer])
+        # component_list.extend([trainer])
 
         return component_list
 
     def steps_completed(self) -> bool:
         mandatory_steps = [TOKENIZER,
-                           keys.TrainingSteps.PREPROCESSER,
-                           keys.TrainingSteps.TRAINER,
+                           # keys.TrainingSteps.PREPROCESSER,
+                           # keys.TrainingSteps.TRAINER,
                            keys.TrainingSteps.DATA]
 
         for step_name in mandatory_steps:
@@ -164,6 +181,9 @@ class NLPPipeline(BasePipeline):
 
     def add_tokenizer(self, tokenizer_step: Any):
         self.steps_dict[TOKENIZER] = tokenizer_step
+
+    def add_split(self, split_step: BaseSplit):
+        self.steps_dict[keys.TrainingSteps.SPLIT] = split_step
 
     def add_preprocesser(self, preprocessor_step: BasePreprocesserStep):
         self.steps_dict[keys.TrainingSteps.PREPROCESSER] = preprocessor_step
