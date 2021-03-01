f8215d831418c6788b326379740de9f3b6dd5308
Nicholas Junge <nicholas@maiot.io>
2021-03-01 13:41:29 +0100

    Programmatic tokenizer selection by name

--- zenml/core/steps/trainer/nlp_tokenizers/hf_tokenizer.py
+++ zenml/core/steps/trainer/nlp_tokenizers/hf_tokenizer.py
@@ -12,45 +12,42 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from typing import Text, List, Any
+from typing import Dict, Text, List, Any
 from zenml.utils import path_utils
 from zenml.core.steps.base_step import BaseStep
-
-import tokenizers.models
-import tokenizers.implementations
-from tokenizers.implementations import BertWordPieceTokenizer
+from zenml.core.steps.trainer.nlp_tokenizers.utils import tokenizer_map
 
 
 class TokenizerStep(BaseStep):
 
     def __init__(self,
                  text_feature: Text,
-                 vocab_size: int,
-                 min_frequency: int,
-                 model: tokenizers.models.Model = None,
-                 tokenizer: tokenizers.implementations.BaseTokenizer = None,
+                 tokenizer: Text,
+                 tokenizer_params: Dict[Text, Any] = None,
+                 vocab_size: int = 30000,
+                 min_frequency: int = 2,
+                 sentence_length: int = 128,
                  special_tokens: List[Text] = None,
                  **kwargs):
 
         super(TokenizerStep, self).__init__(text_feature=text_feature,
                                             vocab_size=vocab_size,
                                             min_frequency=min_frequency,
-                                            model=model,
+                                            sentence_length=sentence_length,
                                             tokenizer=tokenizer,
+                                            tokenizer_params=tokenizer_params,
                                             special_tokens=special_tokens,
                                             **kwargs)
 
-        self.model = model
         self.text_feature = text_feature
+        self.tokenizer_params = tokenizer_params or {}
 
-        # for predefined tokenizer implementations from HuggingFace
-        # if tokenizer is not None:
-        #     self.tokenizer = tokenizer
-        # else:
         # for customized tokenizers
-        self.tokenizer = BertWordPieceTokenizer()
-        self.tokenizer.enable_padding(length=128)
-        self.tokenizer.enable_truncation(max_length=128)
+        self.tokenizer = tokenizer_map.get(tokenizer)(**self.tokenizer_params)
+
+        # truncation and padding constrain length to sentence_length
+        self.tokenizer.enable_padding(length=sentence_length)
+        self.tokenizer.enable_truncation(max_length=sentence_length)
         self.vocab_size = vocab_size
         self.min_frequency = min_frequency
         self.special_tokens = special_tokens or [
@@ -62,10 +59,12 @@ class TokenizerStep(BaseStep):
         ]
 
     def train(self, files: List[Text]):
+        # showing progress does not work in logging mode apparently
         self.tokenizer.train(files=files,
                              vocab_size=self.vocab_size,
                              min_frequency=self.min_frequency,
-                             special_tokens=self.special_tokens)
+                             special_tokens=self.special_tokens,
+                             show_progress=False)
 
     def save(self, output_dir: Text):
         path_utils.create_dir_if_not_exists(output_dir)
