dca15db5574140a29e731f994f0e813f0323fe14
Nicholas Junge <nicholas@maiot.io>
2021-03-03 15:44:49 +0100

    Remove costly first IO, train tokenizer on demand

--- zenml/core/components/tokenizer/executor.py
+++ zenml/core/components/tokenizer/executor.py
@@ -21,8 +21,7 @@ from tfx import types
 from tfx.dsl.components.base import base_executor
 from tfx.types import artifact_utils
 from zenml.core.standards.standard_keys import StepKeys
-from zenml.core.steps.trainer.nlp_tokenizers.hf_tokenizer import \
-    TokenizerStep
+from zenml.core.steps.tokenizer.hf_tokenizer import TokenizerStep
 from zenml.core.components.split_gen.executor import WriteSplit
 from zenml.utils import source_utils
 from zenml.core.steps.split.utils import get_categorical_value
@@ -31,22 +30,13 @@ from tfx.types.artifact_utils import get_split_uri
 from tfx.utils import io_utils
 
 
-def encode_sentence(ex: tf.train.Example, tokenizer: Any, text_feature: Text):
-    sentence = get_categorical_value(ex, text_feature)
+def encode_sentence(ex: tf.train.Example, tokenizer_step: TokenizerStep):
+    sentence = get_categorical_value(ex, tokenizer_step.text_feature)
 
-    encoded = tokenizer.encode(sentence)
+    encoded = tokenizer_step.encode(sentence, output_format="dict")
 
-    id_list = tf.train.Feature(
-        int64_list=tf.train.Int64List(value=encoded.ids))
-    token_list = tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=[
-            tf.compat.as_bytes(" ".join(encoded.tokens))]))
-    attention_list = tf.train.Feature(
-        int64_list=tf.train.Int64List(value=encoded.attention_mask))
-
-    new_feature = {"input_ids": id_list,
-                   "tokens": token_list,
-                   "attention_mask": attention_list}
+    new_feature = {k: tf.train.Feature(
+        int64_list=tf.train.Int64List(value=v)) for k, v in encoded.items()}
 
     new_feature.update({
         f: ex.features.feature[f] for f in ex.features.feature
@@ -74,13 +64,14 @@ class TokenizerExecutor(base_executor.BaseExecutor):
         tokenizer_location = artifact_utils.get_single_uri(
             output_dict["tokenizer"])
 
-        split_uris, split_names = [], []
+        split_uris, split_names, all_files = [], [], []
         for artifact in input_dict["examples"]:
             for split in artifact_utils.decode_split_names(
                     artifact.split_names):
                 split_names.append(split)
                 uri = os.path.join(artifact.uri, split)
                 split_uris.append((split, uri))
+                all_files += path_utils.list_dir(uri)
 
         # Get output split path
         output_examples = artifact_utils.get_single_instance(
@@ -88,51 +79,24 @@ class TokenizerExecutor(base_executor.BaseExecutor):
         output_examples.split_names = artifact_utils.encode_split_names(
             split_names)
 
-        text_cache = artifact_utils.get_single_instance(
-            output_dict["text_cache"])
+        # if vocab is already loaded, skip training
+        if tokenizer_step.has_vocab():
+            tokenizer_step.train_from_iterator(files=all_files)
 
-        text_feature = tokenizer_step.text_feature
+            tokenizer_step.save(output_dir=tokenizer_location)
 
         with self._make_beam_pipeline() as p:
-            # The outer loop will for now only run once
             for split, uri in split_uris:
                 input_uri = io_utils.all_files_pattern(uri)
-                output_uri = os.path.join(
-                    text_cache.uri, "sentences_{}".format(split))
 
-                # write text feature to files
                 _ = (p
                      | 'ReadData.' + split >> beam.io.ReadFromTFRecord(
-                            file_pattern=input_uri)
-                     | "ParseTFExFromString." + split >> beam.Map(
-                            tf.train.Example.FromString)
-                     | "ExtractTextFeature." + split >> beam.Map(
-                            get_categorical_value, cat_col=text_feature)
-                     | "WriteToTextCache." + split >> beam.io.WriteToText(
-                            output_uri,
-                            file_name_suffix=".txt"))
-
-        # get the previously created text files
-        text_files = path_utils.list_dir(text_cache.uri)
-
-        tokenizer_step.train(files=text_files)
-
-        tokenizer_step.save(output_dir=tokenizer_location)
-
-        # make a second pipeline for now
-        with self._make_beam_pipeline() as p:
-            for split, uri in split_uris:
-                input_uri = io_utils.all_files_pattern(uri)
-
-                _ = (p
-                     | 'ReadData2.' + split >> beam.io.ReadFromTFRecord(
                         file_pattern=input_uri)
-                     | "ParseTFExFromString2." + split >> beam.Map(
+                     | "ParseTFExFromString." + split >> beam.Map(
                             tf.train.Example.FromString)
                      | "AddTokens." + split >> beam.Map(
                             encode_sentence,
-                            tokenizer=tokenizer_step.tokenizer,
-                            text_feature=tokenizer_step.text_feature)
+                            tokenizer_step=tokenizer_step)
                      | 'Serialize2.' + split >> beam.Map(
                             lambda x: x.SerializeToString())
                      | 'WriteSplit2_' + split >> WriteSplit(
