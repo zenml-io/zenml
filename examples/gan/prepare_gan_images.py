de165774543dcdfec96e7dd0969490f2dc17ee51
Nicholas Junge <nicholas@maiot.io>
2021-03-05 12:38:09 +0100

    Change to positional arguments

--- examples/gan/prepare_gan_images.py
+++ examples/gan/prepare_gan_images.py
@@ -7,10 +7,10 @@ from shutil import copyfile
 parser = argparse.ArgumentParser(
     description="Process images and create metadata JSON file.")
 
-parser.add_argument("--input_dir", type=str, default=".",
+parser.add_argument("input_dir", type=str, default=".",
                     help="directory containing the images.")
 
-parser.add_argument("--output_dir", type=str, default=".",
+parser.add_argument("output_dir", type=str, default=".",
                     help="output_directory for the processed images.")
 
 parser.add_argument("--max_imgs", type=int, default=1 << 30,
