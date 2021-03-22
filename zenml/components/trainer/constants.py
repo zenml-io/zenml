d88275d1f1c436ea76b1db90fd0b9e8b6e9a35eb
Baris Can Durak <bariscandurak@hotmail.com>
2021-03-22 15:18:14 +0100

    defining constants

--- zenml/components/trainer/constants.py
+++ zenml/components/trainer/constants.py
@@ -1,2 +1,16 @@
+# ZenML constants
 TEST_RESULTS = 'test_results'
+TRAIN_SPLITS = 'train_splits'
+TEST_SPLITS = 'test_splits'
+EVAL_SPLITS = 'eval_splits'
 
+# TFX constants
+RUN_FN = 'run_fn'
+CUSTOM_CONFIG = 'custom_config'
+EXAMPLES = 'examples'
+SCHEMA = 'schema'
+BASE_MODEL = 'base_model'
+TRANSFORM_GRAPH = 'transform_graph'
+HYPERPARAMETERS = 'hyperparameters'
+MODEL = 'model'
+MODEL_RUN = 'model_run'
