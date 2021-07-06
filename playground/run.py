c68cc2a535748da8762639bbc3e07052542ddc5b
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-06 17:33:16 +0200

    starting the parameterization

--- playground/run.py
+++ playground/run.py
@@ -1,8 +1,10 @@
-from playground.split_pipeline import SplitPipeline
 from playground.client import Client
+from playground.split_pipeline import SplitPipeline
 
 client = Client()
 
-split_pipeline = SplitPipeline()
+split_pipeline = SplitPipeline(split_map={'train': 0.7, 'test': 0.3})
 
 split_pipeline.run()
+
+# client.run(split_pipeline)
--- playground/split_pipeline.py
+++ playground/split_pipeline.py
@@ -3,12 +3,11 @@ from playground.split_step import SplitStep
 
 
 class SplitPipeline(BasePipeline):
-    def __init__(self):
+    def __init__(self,
+                 split_map):
         super(SplitPipeline, self).__init__()
 
-        self.split_step = SplitStep(split_map={})
+        self.split_step = SplitStep(split_map=split_map)
 
     def connect(self, datasource):
         split_step = self.split_step(datasource)
-        preprocessing = self.preprocessing(
-            input_data=splits.outputs.output_data)
