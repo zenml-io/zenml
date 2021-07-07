cc6bca44406eff0a08c9fd4027de130fa700780e
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-07 16:58:59 +0200

    added a new step to test

---
+++ playground/preprocesser_step.py
@@ -0,0 +1,15 @@
+from playground.base_step import BaseStep
+from playground.artifacts import DataArtifact, Input, Output
+
+
+class PreprocesserStep(BaseStep):
+    def __init__(self, param: float):
+        self.param = param
+
+        super(PreprocesserStep, self).__init__()
+
+    def process(self,
+                input_data: Input[DataArtifact],
+                output_data: Output[DataArtifact]):
+
+        self.param = 0.3
