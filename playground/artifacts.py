156dc4ca9e8f6f08a97cb197920e0fd375504332
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-07 16:43:53 +0200

    using input and output artifacts

--- playground/artifacts.py
+++ playground/artifacts.py
@@ -1,9 +1,25 @@
 import pandas as pd
+from tfx.dsl.component.experimental.annotations import InputArtifact, \
+    OutputArtifact
 
 from playground.base_artifact import BaseArtifact
 
 
+# Input and output wrappers
+
+class Input(InputArtifact):
+    pass
+
+
+class Output(OutputArtifact):
+    pass
+
+
+# Artifact types
+
 class DataArtifact(BaseArtifact):
+    TYPE_NAME = 'data_artifact'
+
     def read(self):
         return pd.read_csv(self.uri)
 
