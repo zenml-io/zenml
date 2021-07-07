fbe913abb8c0fbd1f30430b1cedd191afe68d7b0
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-07 16:45:14 +0200

    adding inputs outputs to the base step and infering them from the signature

--- playground/base_step.py
+++ playground/base_step.py
@@ -1,50 +1,83 @@
 import inspect
 from abc import abstractmethod
 
+from playground.artifacts import Input, Output
+
 
 class BaseStep:
     def __init__(self):
-        pass
-
-    def __call__(self, **kwargs):
-        # infer the input artifacts
-        self.inputs = inspect.func(self.process)
-        # infer the output artifacts
-        self.outputs = self.outputs or self.output()
-        return self
+        self.__inputs = {}
+        self.__outputs = {}
 
     @abstractmethod
     def process(self):
         pass
 
-# class DistributedBaseStep:
-#     def __init__(self):
-#         pass
-#
-#     def __call__(self, **kwargs):
-#         self.inputs = inspect.func(self.process)
-#         self.outputs = self.outputs or self.output()
-#         return self
-#
-#     def process(self):
-#         with beam.pipeline() as p:
-#             (p | ReadParDo | ProcessParDo | WriteParDo)
-#
-#     def ReadParDo(self):
-#         with self._make_beam_pipeline():
-#             return p | beam.io.ReadFromArrow(self.data.uri)
-#
-#     def ProcessParDo(self):
-#         pass
-#
-#     def WriteParDo(self):
-#         pass
-#
-#     def process(self,
-#                 data: DataArtifact,
-#                 schema: SchemaArtifact,
-#                 statistics: StatisticsArtifact
-#                 ) -> PandasDataFrameArtifact:
-#         t = PandasDataFrameArtifact()
-#
-#         return t
+    @property
+    def inputs(self):
+        return self.__inputs
+
+    @property
+    def outputs(self):
+        return self.__outputs
+
+    @inputs.setter
+    def inputs(self, inputs):
+        raise PermissionError('The attribute inputs is used internally by '
+                              'ZenML. Please avoid making changes to it.')
+
+    @outputs.setter
+    def outputs(self, outputs):
+        raise PermissionError('The attribute outputs is used internally by '
+                              'ZenML. Please avoid making changes to it.')
+
+    @inputs.deleter
+    def inputs(self):
+        self.__inputs = dict()
+
+    @outputs.deleter
+    def outputs(self):
+        self.__outputs = dict()
+
+    def __call__(self, **kwargs):
+        fullspec = inspect.getfullargspec(self.process)
+        # TODO: check whether it is implemented as a static or class method
+        # TODO: implement a way to interpret the params
+        for arg, arg_type in fullspec.annotations.items():
+            if isinstance(arg_type, Input):
+                self.__inputs.update({arg: arg_type.type()})
+            if isinstance(arg_type, Output):
+                self.__outputs.update({arg: arg_type.type()})
+        return self
+
+    # class DistributedBaseStep:
+    #     def __init__(self):
+    #         pass
+    #
+    #     def __call__(self, **kwargs):
+    #         self.inputs = inspect.func(self.process)
+    #         self.outputs = self.outputs or self.output()
+    #         return self
+    #
+    #     def process(self):
+    #         with beam.pipeline() as p:
+    #             (p | ReadParDo | ProcessParDo | WriteParDo)
+    #
+    #     def ReadParDo(self):
+    #         with self._make_beam_pipeline():
+    #             return p | beam.io.ReadFromArrow(self.data.uri)
+    #
+    #     def ProcessParDo(self):
+    #         pass
+    #
+    #     def WriteParDo(self):
+    #         pass
+    #
+    #     def process(self,
+    #                 data: DataArtifact,
+    #                 schema: SchemaArtifact,
+    #                 statistics: StatisticsArtifact
+    #                 ) -> PandasDataFrameArtifact:
+    #         t = PandasDataFrameArtifact()
+    #
+    #         return t
