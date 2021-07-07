480ab96bab11888654210f7634ae032c9b4acf6a
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-07 16:59:45 +0200

    extending the example

--- playground/run.py
+++ playground/run.py
@@ -3,8 +3,13 @@ from playground.split_pipeline import SplitPipeline
 
 client = Client()
 
-split_pipeline = SplitPipeline(split_map={'train': 0.7, 'test': 0.3})
+# Parameters
+split_map = {'train': 0.7, 'test': 0.3}
+param = 1
 
+# Pipeline
+split_pipeline = SplitPipeline(split_map=split_map,
+                               param=param)
 split_pipeline.run()
 
-# client.run(split_pipeline)
+
--- playground/split_pipeline.py
+++ playground/split_pipeline.py
@@ -1,13 +1,19 @@
 from playground.base_pipeline import BasePipeline
+from playground.preprocesser_step import PreprocesserStep
 from playground.split_step import SplitStep
 
 
 class SplitPipeline(BasePipeline):
     def __init__(self,
-                 split_map):
+                 split_map,
+                 param):
         super(SplitPipeline, self).__init__()
 
         self.split_step = SplitStep(split_map=split_map)
+        self.preprocesser_step = PreprocesserStep(param=param)
 
     def connect(self, datasource):
-        split_step = self.split_step(datasource)
+        self.split_step(input_data=datasource)
+        # TODO: Make a dot dict
+        self.preprocesser_step(
+            input_data=self.split_step.outputs['output_data'])
