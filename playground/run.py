7a557b49d05557ead091b70eb267a960e5e67f95
Baris Can Durak <bariscandurak@hotmail.com>
2021-07-19 15:36:59 +0200

    added a mlmd store

--- playground/run.py
+++ playground/run.py
@@ -1,7 +1,9 @@
-from playground.client import Client
 from playground.split_pipeline import SplitPipeline
+from zenml.metadata import SQLiteMetadataStore
 
-client = Client()
+uri = '/home/baris/Maiot/zenml/local_test/new_zenml/db'
+store = SQLiteMetadataStore(uri)
+datasource = None
 
 # Parameters
 param = 1
@@ -10,4 +12,4 @@ split_map = {'train': 0.7, 'test': 0.3}
 # Pipeline
 split_pipeline = SplitPipeline(split_map=split_map,
                                param=param)
-split_pipeline.run()
+split_pipeline.run(datasource)
