dd201f7323dd62b8b0845044d29422e0ce16b4ad
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-04 18:25:46 +0200

    added two options for pipeline definition

--- playground/pipelines/split_pipeline.py
+++ playground/pipelines/split_pipeline.py
@@ -1,12 +1,29 @@
-from playground.annotations import Step, Datasource
-from playground.steps.split_step import SplitStep
-from playground.steps.preprocesser_step import PreprocesserStep
+# Option 1
+
 from playground.datasources.csv_datasource import CSVDatasource
+from playground.steps.preprocesser_step import FunctionPreprocesserStep
+from playground.steps.split_step import FunctionSplitStep
+from playground.utils.annotations import Step, Datasource
+from playground.utils.pipeline_utils import pipeline
 
 
 @pipeline
-def SplitPipeline(datasource: Datasource[CSVDatasource],
-                  split_step: Step[SplitStep],
-                  preprocesser_step: Step[PreprocesserStep]):
+def FunctionSplitPipeline(datasource: Datasource[CSVDatasource],
+                          split_step: Step[FunctionSplitStep],
+                          preprocesser_step: Step[FunctionPreprocesserStep]):
     split_step(input_data=datasource)
     preprocesser_step(input_data=split_step.outputs.output_data)
+
+
+# Option 2
+
+from playground.pipelines.base_pipeline import BasePipeline
+
+
+class ClassSplitPipeline(BasePipeline):
+    def connect(self,
+                datasource: Datasource[CSVDatasource],
+                split_step: Step[FunctionSplitStep],
+                preprocesser_step: Step[FunctionPreprocesserStep]):
+        split_step(input_data=datasource.outputs.result)
+        preprocesser_step(input_data=split_step.outputs.output_data)
