4ce4024da314823bd5addad813bf3b49d598f410
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-04 18:27:52 +0200

    modifying the pipeline configuration

--- playground/run.py
+++ playground/run.py
@@ -1,19 +1,20 @@
-from playground.preprocesser_step import PreprocesserStep
-from playground.split_pipeline import SplitPipeline
-from playground.split_step import SplitStep
+from playground.pipelines.split_pipeline import ClassSplitPipeline, FunctionSplitPipeline
+from playground.steps.split_step import ClassSplitStep, FunctionSplitStep
+from playground.steps.preprocesser_step import ClassPreprocesserStep, FunctionPreprocesserStep
+from playground.datasources.csv_datasource import CSVDatasource
 
 # Parameters
 param = 1.0
 split_map = 0.6
 
-placeholder_datasource = None
-
 # Steps
-split_step = SplitStep(split_map=split_map)
-preprocesser_step = PreprocesserStep(param=param)
+datasource = CSVDatasource("local_test/data/data.csv")
+split_step = ClassSplitStep(split_map=split_map)
+preprocesser_step = ClassPreprocesserStep(param=param)
 
 # Pipeline
-split_pipeline = SplitPipeline(split_step=split_step,
-                               preprocesser_step=preprocesser_step)
+split_pipeline = ClassSplitPipeline(datasource=datasource,
+                                    split_step=split_step,
+                                    preprocesser_step=preprocesser_step)
 
-split_pipeline.run(placeholder_datasource)
+split_pipeline.run()
