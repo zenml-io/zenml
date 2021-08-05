0463f7010370cc5e211a99f295393ded024f2ead
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-05 14:43:49 +0200

    fix the read and write functions

--- playground/artifacts/data_artifacts.py
+++ playground/artifacts/data_artifacts.py
@@ -1,15 +1,18 @@
+import os
+
 import pandas as pd
 
 from playground.artifacts.base_artifact import BaseArtifact
 
-
 # Artifact types
+DEFAULT_FILENAME = "data.csv"
+
 
 class CSVArtifact(BaseArtifact):
     TYPE_NAME = 'csv_artifact'
 
     def read(self):
-        return pd.read_csv(self.uri)
+        return pd.read_csv(os.path.join(self.uri, DEFAULT_FILENAME))
 
     def write(self, df: pd.DataFrame):
-        df.to_csv(self.uri)
+        df.to_csv(os.path.join(self.uri, DEFAULT_FILENAME))
