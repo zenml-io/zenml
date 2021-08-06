0e82b702df77d78d3535990bd319ee16661bb87e
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-06 13:25:11 +0200

    example moved to a single script

--- playground/run.py
+++ playground/run.py
@@ -1,20 +1,42 @@
-from playground.pipelines.split_pipeline import ClassSplitPipeline, FunctionSplitPipeline
-from playground.steps.split_step import ClassSplitStep, FunctionSplitStep
-from playground.steps.preprocesser_step import ClassPreprocesserStep, FunctionPreprocesserStep
+from playground.artifacts.data_artifacts import CSVArtifact
 from playground.datasources.csv_datasource import CSVDatasource
+from playground.pipelines.base_pipeline import Step, Datasource
+from playground.pipelines.simple_pipeline import SimplePipeline
+from playground.steps.base_step import Input, Output, Param
+from playground.steps.simple_step import SimpleStep
 
-# Parameters
-param = 1.0
-split_map = 0.6
 
-# Steps
-datasource = CSVDatasource("local_test/data/data.csv")
-split_step = ClassSplitStep(split_map=split_map)
-preprocesser_step = ClassPreprocesserStep(param=param)
+@SimpleStep
+def SplitStep(input_data: Input[CSVArtifact],
+              output_data: Output[CSVArtifact],
+              split_map: Param[float]):
+    data = input_data.read()
+    split_map = None
+    output_data.write(data)
+
+
+@SimpleStep
+def PreprocesserStep(input_data: Input[CSVArtifact],
+                     output_data: Output[CSVArtifact],
+                     param: Param[float]):
+    data = input_data.read()
+    param = None
+    output_data.write(data)
+
+
+@SimplePipeline
+def SplitPipeline(datasource: Datasource[CSVDatasource],
+                  split_step: Step[SplitStep],
+                  preprocesser_step: Step[PreprocesserStep]):
+    split_step(input_data=datasource)
+    preprocesser_step(input_data=split_step.outputs.output_data)
+
 
 # Pipeline
-split_pipeline = ClassSplitPipeline(datasource=datasource,
-                                    split_step=split_step,
-                                    preprocesser_step=preprocesser_step)
+split_pipeline = SplitPipeline(
+    datasource=CSVDatasource("local_test/data/data.csv"),
+    split_step=SplitStep(split_map=0.6),
+    preprocesser_step=PreprocesserStep(param=1.0)
+)
 
 split_pipeline.run()
