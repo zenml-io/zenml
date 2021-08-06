cb8f4eafb9cde971a94b1bcbb0e9b519764eb680
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-06 13:25:57 +0200

    annotations moved to base classes

--- playground/utils/annotations.py
+++ playground/utils/annotations.py
@@ -19,10 +19,6 @@ from typing import Type
 
 from six import with_metaclass
 
-from playground.artifacts.base_artifact import BaseArtifact
-from playground.datasources.base_datasource import BaseDatasource
-from playground.steps.base_step import BaseStep
-
 
 class GenericMeta(type):
     def __getitem__(cls: Type["GenericType"],
@@ -57,24 +53,3 @@ class GenericType(with_metaclass(GenericMeta, object)):
             raise ValueError(f"Generic type `{class_name}[T]` expects the "
                              f"single parameter T to be one of "
                              f"{cls.VALID_TYPES}.")
-
-
-Datasource = type("Datasource",
-                  (GenericType,),
-                  {"VALID_TYPES": [BaseDatasource]})
-
-Step = type("Step",
-            (GenericType,),
-            {"VALID_TYPES": [BaseStep]})
-
-Input = type("Input",
-             (GenericType,),
-             {"VALID_TYPES": [BaseArtifact]})
-
-Output = type("Output",
-              (GenericType,),
-              {"VALID_TYPES": [BaseArtifact]})
-
-Param = type("Param",
-             (GenericType,),
-             {"VALID_TYPES": [int, float, str, bytes]})
