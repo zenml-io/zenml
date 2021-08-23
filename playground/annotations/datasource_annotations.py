c53336f2d2569a3758b79d832e241857b1ff278a
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-23 16:11:25 +0200

    removed the ds annotations

--- playground/annotations/datasource_annotations.py
+++
@@ -1,6 +0,0 @@
-from playground.annotations.base_annotations import GenericType
-from playground.datasources.base_datasource import BaseDatasource
-
-Datasource = type("Datasource",
-                  (GenericType,),
-                  {"VALID_TYPES": [BaseDatasource]})
