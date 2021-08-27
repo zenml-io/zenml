d7709653b32d0e3c2a0e1940d07d54e50fa5300b
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-27 16:38:09 +0200

    playing around with the run.py

--- playground/run.py
+++ playground/run.py
@@ -1,43 +1,42 @@
-from playground.annotations import Input, Output, Param, Step
+from playground.annotations import Input, Output, Param, Step, External
 from playground.artifacts import CSVArtifact
 from playground.pipelines.simple_pipeline import SimplePipeline
 from playground.steps.simple_step import SimpleStep
 
 
 # A basic Pipeline, which runs all steps locally
-
-@SimpleStep
-def SplitStep(input_data: Input[CSVArtifact],
-              output_data: Output[CSVArtifact],
-              split_map: Param[float]):
-    data = input_data.read()
-    split_map = None
-    output_data.write(data)
-
-
-@SimpleStep
-def PreprocesserStep(input_data: Input[CSVArtifact],
-                     output_data: Output[CSVArtifact],
-                     param: Param[float]):
-    data = input_data.read()
-    param = None
-    output_data.write(data)
-
-
-@SimplePipeline
-def SplitPipeline(split_step: Step[SplitStep],
-                  preprocesser_step: Step[PreprocesserStep]):
-    split_step()
-    preprocesser_step(input_data=split_step.outputs.output_data)
-
-
-# Pipeline
-split_pipeline = SplitPipeline(
-    split_step=SplitStep(split_map=0.6),
-    preprocesser_step=PreprocesserStep(param=1.0)
-)
-
-
+#
+# @SimpleStep
+# def SplitStep(input_data: Input[CSVArtifact],
+#               output_data: Output[CSVArtifact],
+#               split_map: Param[float]):
+#     data = input_data.read()
+#     split_map = None
+#     output_data.write(data)
+#
+#
+# @SimpleStep
+# def PreprocesserStep(input_data: Input[CSVArtifact],
+#                      output_data: Output[CSVArtifact],
+#                      param: Param[float]):
+#     data = input_data.read()
+#     param = None
+#     output_data.write(data)
+#
+#
+# @SimplePipeline
+# def SplitPipeline(split_step: Step[SplitStep],
+#                   preprocesser_step: Step[PreprocesserStep]):
+#     split_step()
+#     preprocesser_step(input_data=split_step.outputs.output_data)
+#
+#
+# # Pipeline
+# split_pipeline = SplitPipeline(
+#     split_step=SplitStep(split_map=0.6),
+#     preprocesser_step=PreprocesserStep(param=1.0)
+# )
+#
 # split_pipeline.run()
 
 # A distributed Pipeline, which runs on Beam
@@ -69,9 +68,9 @@ def DistPreprocesserStep(input_data: Input[CSVArtifact],
 
 
 @SimplePipeline
-def DistSplitPipeline(input_artifact: Input[CSVArtifact],
-                      split_step: Step[SplitStep],
-                      preprocesser_step: Step[PreprocesserStep]):
+def DistSplitPipeline(input_artifact: External[CSVArtifact],
+                      split_step: Step[DistSplitStep],
+                      preprocesser_step: Step[DistPreprocesserStep]):
     split_step(input_data=input_artifact)
     preprocesser_step(input_data=split_step.outputs.output_data)
 
@@ -80,8 +79,8 @@ def DistSplitPipeline(input_artifact: Input[CSVArtifact],
 dist_split_pipeline = DistSplitPipeline(
     # TODO: implement the with backend
     input_artifact=CSVArtifact(),
-    split_step=SplitStep(split_map=0.6).with_backend({"some_params"}),
-    preprocesser_step=PreprocesserStep(param=1.0).with_backend({"some_params"})
+    split_step=DistSplitStep(split_map=0.6).with_backend({"some_params"}),
+    preprocesser_step=DistPreprocesserStep(param=1.0).with_backend({"some_params"})
 )
 
 dist_split_pipeline.run()
