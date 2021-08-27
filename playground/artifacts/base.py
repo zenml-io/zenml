8ee516526e81a336e848061c3e5317ca2a77f986
Baris Can Durak <bariscandurak@hotmail.com>
2021-08-27 14:16:15 +0200

    removed the read and write abstract functions

--- playground/artifacts/base.py
+++ playground/artifacts/base.py
@@ -1,16 +1,6 @@
-from abc import abstractmethod
-
 from tfx.types import Artifact
 
 
 class BaseArtifact(Artifact):
     TYPE_NAME = 'BaseArtifact'
     PROPERTIES = {}
-
-    @abstractmethod
-    def read(self):
-        pass
-
-    @abstractmethod
-    def write(self):
-        pass
