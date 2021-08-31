1a5902b08933d751faab64d20004388bd6a5150d
Hamza Tahir <htahir111@gmail.com>
2021-08-31 16:42:47 +0200

    added format with excluded

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place zenml tests --exclude=__init__.py
-isort zenml tests
-black zenml tests
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place zenml tests --exclude=__init__.py,zenml/legacy/*
+isort zenml tests --skip  zenml/legacy/
+black zenml tests --exclude zenml/legacy/
