c1cb06beb40f19e0668a46f3f643a3d887919655
baris <baris@maiot.io>
2021-09-03 11:30:39 +0200

    checkpoint for the new design

--- run.py
+++ run.py
@@ -1,98 +1,48 @@
-from playground.annotations import External, Input, Output, Param, Step
-from playground.artifacts import CSVArtifact
-from playground.pipelines.simple_pipeline import SimplePipeline
-from playground.steps.simple_step import SimpleStep
+import pandas as pd
 
-# A basic Pipeline, which runs all steps locally
-#
-# @SimpleStep
-# def SplitStep(input_data: Input[CSVArtifact],
-#               output_data: Output[CSVArtifact],
-#               split_map: Param[float]):
-#     data = input_data.read()
-#     split_map = None
-#     output_data.write(data)
-#
-#
-# @SimpleStep
-# def PreprocesserStep(input_data: Input[CSVArtifact],
-#                      output_data: Output[CSVArtifact],
-#                      param: Param[float]):
-#     data = input_data.read()
-#     param = None
-#     output_data.write(data)
-#
-#
-# @SimplePipeline
-# def SplitPipeline(split_step: Step[SplitStep],
-#                   preprocesser_step: Step[PreprocesserStep]):
-#     split_step()
-#     preprocesser_step(input_data=split_step.outputs.output_data)
-#
-#
-# # Pipeline
-# split_pipeline = SplitPipeline(
-#     split_step=SplitStep(split_map=0.6),
-#     preprocesser_step=PreprocesserStep(param=1.0)
-# )
-#
-# split_pipeline.run()
-
-# A distributed Pipeline, which runs on Beam
+from zenml import pipelines
+from zenml import steps
+from zenml.annotations import External, Input, Step
+from zenml.artifacts import TextArtifact
+from zenml.distributed.beam import BeamOutput
 
 
-@SimpleStep
-def DistSplitStep(
-    input_data: Input[CSVArtifact],
-    output_data: Output[CSVArtifact],
-    split_map: Param[float],
-):
+@steps.SimpleStep
+def DistSplitStep(input_data: Input[TextArtifact]
+                  ) -> BeamOutput[TextArtifact]:
     import apache_beam as beam
 
-    with beam.Pipeline() as p:
-        result = (
-            p
-            | "ReadData" >> input_data.read()
-            | "Split" >> beam.Map(lambda x: x)
-            | "WriteData" >> output_data.write()
-        )
+    with beam.Pipeline() as pipeline:
+        data = input_data.read_with_beam(pipeline)
+        result = data | beam.Map(lambda x: x)
+
+    return result
 
 
-@SimpleStep
-def DistPreprocesserStep(
-    input_data: Input[CSVArtifact],
-    output_data: Output[CSVArtifact],
-    param: Param[float],
+@steps.SimpleStep
+def InMemPreprocesserStep(input_data: Input[TextArtifact]) -> pd.DataFrame:
+    data = input_data.read_with_pandas()
+    return data
+
+
+@pipelines.SimplePipeline
+def SplitPipeline(
+        input_artifact: External[TextArtifact],
+        split_step: Step[DistSplitStep],
+        preprocesser_step: Step[InMemPreprocesserStep],
 ):
-    import apache_beam as beam
-
-    with beam.Pipeline() as p:
-        result = (
-            p
-            | "ReadData" >> input_data.read()
-            | "Split" >> beam.Map(lambda x: x)
-            | "WriteData" >> output_data.write()
-        )
-
-
-@SimplePipeline
-def DistSplitPipeline(
-    input_artifact: External[CSVArtifact],
-    split_step: Step[DistSplitStep],
-    preprocesser_step: Step[DistPreprocesserStep],
-):
-    split_step(input_data=input_artifact)
-    preprocesser_step(input_data=split_step.outputs.output_data)
+    split_data = split_step(input_data=input_artifact)
+    processed_data = preprocesser_step(input_data=split_data)
 
 
 # Pipeline
-dist_split_pipeline = DistSplitPipeline(
-    # TODO: implement the with backend
-    input_artifact=CSVArtifact(),
-    split_step=DistSplitStep(split_map=0.6).with_backend({"some_params"}),
-    preprocesser_step=DistPreprocesserStep(param=1.0).with_backend(
-        {"some_params"}
-    ),
+test_artifact = TextArtifact()
+test_artifact.uri = "/home/baris/zenml/zenml/zenml/local_test/data/data.csv"
+
+dist_split_pipeline = SplitPipeline(
+    input_artifact=test_artifact,
+    split_step=DistSplitStep(),
+    preprocesser_step=InMemPreprocesserStep()
 )
 
 dist_split_pipeline.run()
