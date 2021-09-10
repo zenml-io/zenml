d5cfc633938dc4b365c39bd5bbb94716fdb43667
baris <baris@maiot.io>
2021-09-10 14:31:18 +0200

    fixing the imports

--- zenml/annotations/__init__.py
+++ zenml/annotations/__init__.py
@@ -1,3 +1,4 @@
-from zenml.annotations.artifact_annotations import External, Input, Output
+from zenml.annotations.artifact_annotations import External
+from zenml.annotations.artifact_annotations import Input, Output
 from zenml.annotations.misc_annotations import Param
 from zenml.annotations.step_annotations import Step
