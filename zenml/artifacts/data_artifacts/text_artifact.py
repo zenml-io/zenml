f3b42ad2a602226ec101c540cd3a66f4ecc9719b
baris <baris@maiot.io>
2021-09-10 14:38:08 +0200

    implementing the text artifact with writer factories

--- zenml/artifacts/data_artifacts/text_artifact.py
+++ zenml/artifacts/data_artifacts/text_artifact.py
@@ -1,6 +1,8 @@
 import os
 
-from zenml.artifacts.data_artifacts.base import BaseDataArtifact
+from zenml.annotations.artifact_annotations import BeamOutput, PandasOutput
+from zenml.artifacts.data_artifacts.base_data_artifact import BaseDataArtifact
+from zenml.artifacts.utils import WriterFactory
 
 DEFAULT_FILENAME = "data.txt"
 
@@ -17,10 +19,49 @@ def parse_line(x):
     return x
 
 
+def write_with_pandas(artifact, df):
+    """
+
+    Args:
+      artifact:
+      df:
+
+    Returns:
+
+    """
+    df.to_csv(os.path.join(artifact.uri, DEFAULT_FILENAME))
+
+
+def write_with_beam(artifact, pipeline):
+    """
+
+    Args:
+      artifact:
+      pipeline:
+
+    Returns:
+
+    """
+    import apache_beam as beam
+
+    out_path = os.path.join(artifact.uri, DEFAULT_FILENAME)
+    return pipeline | beam.io.WriteToText(out_path)
+
+
 class TextArtifact(BaseDataArtifact):
     """ """
 
     TYPE_NAME = "text_artifact"
+
+    WRITER_FACTORY = WriterFactory()
+    WRITER_FACTORY.register_type(PandasOutput, write_with_pandas)
+    WRITER_FACTORY.register_type(BeamOutput, write_with_beam)
+
+    def read_with_pandas(self):
+        """ """
+        import pandas as pd
+
+        return pd.read_csv(os.path.join(self.uri, DEFAULT_FILENAME))
 
     def read_with_beam(self, pipeline):
         """
@@ -31,7 +72,6 @@ class TextArtifact(BaseDataArtifact):
         Returns:
 
         """
-
         import apache_beam as beam
         from tfx.dsl.io import fileio
 
@@ -45,44 +85,7 @@ class TextArtifact(BaseDataArtifact):
 
         txt_files = [os.path.join(self.uri, file) for file in txt_files]
 
-        return (
-            pipeline
-            | "ReadText"
-            >> beam.io.ReadFromText(
-                file_pattern=txt_files[0],
-                # TODO: testing first file
-                skip_header_lines=1,
-            )
-            | "ParsedLines" >> beam.Map(parse_line)
-        )
-
-    def write_with_beam(self, pipeline):
-        """
-
-        Args:
-          pipeline:
-
-        Returns:
-
-        """
-        import apache_beam as beam
-
-        out_path = os.path.join(self.uri, DEFAULT_FILENAME)
-        return pipeline | beam.io.WriteToText(out_path)
-
-    def read_with_pandas(self):
-        """ """
-        import pandas as pd
-
-        return pd.read_csv(os.path.join(self.uri, DEFAULT_FILENAME))
-
-    def write_with_pandas(self, df):
-        """
-
-        Args:
-          df:
-
-        Returns:
-
-        """
-        df.to_csv(os.path.join(self.uri, DEFAULT_FILENAME))
+        return (pipeline
+                | "ReadText" >> beam.io.ReadFromText(file_pattern=txt_files[0],
+                                                     skip_header_lines=1)
+                | "ParsedLines" >> beam.Map(parse_line))
