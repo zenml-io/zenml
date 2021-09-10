a64b243dc3efae20e0a230c52001cd7d5abfd6b5
baris <baris@maiot.io>
2021-09-10 14:31:36 +0200

    added new output artifact annotations

--- zenml/annotations/artifact_annotations.py
+++ zenml/annotations/artifact_annotations.py
@@ -1,8 +1,18 @@
 from zenml.annotations.base_annotations import GenericType
-from zenml.artifacts import BaseArtifact
+from zenml.artifacts.base_artifact import BaseArtifact
+
+# General Artifact Annotations
 
 Input = type("Input", (GenericType,), {"VALID_TYPES": [BaseArtifact]})
 
 Output = type("Output", (GenericType,), {"VALID_TYPES": [BaseArtifact]})
 
 External = type("Input", (GenericType,), {"VALID_TYPES": [BaseArtifact]})
+
+# Specialized Artifact Annotations
+
+BeamOutput = type("BeamOutput", (Output,), {"VALID_TYPES": [BaseArtifact]})
+
+PandasOutput = type("PandasOutput", (Output,), {"VALID_TYPES": [BaseArtifact]})
+
+JSONOutput = type("JSONOutput", (Output,), {"VALID_TYPES": [BaseArtifact]})
