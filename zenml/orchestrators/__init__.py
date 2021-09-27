77503b28618ef80fd6643576615280eba1c459cb
Hamza Tahir <htahir111@gmail.com>
2021-09-27 07:10:30 +0200

    Airflow integration checked :bug:

--- zenml/orchestrators/__init__.py
+++ zenml/orchestrators/__init__.py
@@ -1,1 +1,25 @@
-from zenml.orchestrators.airflow.airflow_orchestrator import AirflowOrchestrator  # noqa
+#  Copyright (c) ZenML GmbH 2020. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+# TODO [MEDIUM]: Create a standard way of doing this.
+from zenml.logger import get_logger
+
+logger = get_logger(__name__)
+
+try:
+    from zenml.orchestrators.airflow.airflow_orchestrator import (  # noqa
+        AirflowOrchestrator,
+    )
+except ImportError:
+    logger.debug("Airflow not installed.")
