7d75b126b86336f4dc0d349c5b74f88f695088e3
bcdurak <bariscandurak@hotmail.com>
2021-09-28 15:34:54 +0200

    checkpoint for the run.py

--- run.py
+++ run.py
@@ -1,46 +1,49 @@
 import os
 
-import pandas as pd
-
 from zenml import pipeline, step
-from zenml.annotations import Input, Param, Step
-from zenml.annotations.artifact_annotations import BeamOutput, PandasOutput
+from zenml.annotations import Input, Param, Step, Output
 from zenml.artifacts.data_artifacts.text_artifact import TextArtifact
 
 
-@step
-def DataIngestionStep(uri: Param[str]) -> PandasOutput[TextArtifact]:
-    return pd.read_csv(uri)
+@step(name="data_ingest")
+def DataIngestionStep(uri: Param[str],
+                      output_artifact: Output[TextArtifact]):
+    import pandas as pd
+    df = pd.read_csv(uri)
+    output_artifact.write_with_pandas(df)
 
 
-@step
-def DistSplitStep(
-        text_artifact: Input[TextArtifact],
-) -> BeamOutput[TextArtifact]:
+@step(name="split")
+def DistSplitStep(input_artifact: Input[TextArtifact],
+                  output_artifact: Output[TextArtifact]):
     import apache_beam as beam
 
-    pipeline = beam.Pipeline()
-    data = text_artifact.read_with_beam(pipeline)
-    result = data | beam.Map(lambda x: x)
-
-    return (result, pipeline)
-
-
-@step
-def InMemPreprocesserStep(text_artifact: Input[TextArtifact]) -> pd.DataFrame:
-    data = text_artifact.read_with_pandas()
-    return data
+    with beam.Pipeline() as p:
+        _ = (p
+             | input_artifact.read_with_beam()
+             | output_artifact.write_with_beam())
 
 
-@pipeline
+@step(name="preprocessing")
+def InMemPreprocesserStep(input_artifact: Input[TextArtifact],
+                          output_artifact: Output[TextArtifact]):
+    data = input_artifact.read_with_pandas()
+    output_artifact.write_with_pandas(data)
+
+
+@pipeline(name="my_pipeline")
 def SplitPipeline(
         data_step: Step[DataIngestionStep],
         split_step: Step[DistSplitStep],
         preprocesser_step: Step[InMemPreprocesserStep],
 ):
-    text_artifact = data_step()
-    split_artifact = split_step(text_artifact=text_artifact)
-    _ = preprocesser_step(text_artifact=split_artifact)
+    split_step.connect_inputs(
+        text_artifact=data_step.outputs.output_artifact,
+    )
+
+    preprocesser_step.connect_inputs(
+        text_artifact=split_step.outputs.output_artifact,
+    )
 
 
 # Pipeline
