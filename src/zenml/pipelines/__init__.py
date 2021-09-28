46c739d5a7f9da6a9acfcc71c6a014ff0862ad57
bcdurak <bariscandurak@hotmail.com>
2021-09-28 15:42:27 +0200

    fixed the init files

--- src/zenml/pipelines/__init__.py
+++ src/zenml/pipelines/__init__.py
@@ -1,4 +1,2 @@
 from zenml.pipelines.base_pipeline import BasePipeline  # noqa
-from zenml.pipelines.simple_pipeline import SimplePipeline  # noqa
-
-# from zenml.pipelines.training_pipeline import TrainingPipeline
+from zenml.pipelines.pipeline import pipeline  # noqa
--- src/zenml/steps/__init__.py
+++ src/zenml/steps/__init__.py
@@ -1,2 +1,2 @@
 from zenml.steps.base_step import BaseStep  # noqa
-from zenml.steps.simple_step import SimpleStep  # noqa
+from zenml.steps.step import step  # noqa
