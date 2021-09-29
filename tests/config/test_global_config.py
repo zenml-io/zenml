e75eaee07d407d50be4863baba40009250e0869e
Hamza Tahir <htahir111@gmail.com>
2021-09-29 08:06:18 +0200

    GC tests complete

--- tests/config/test_global_config.py
+++ tests/config/test_global_config.py
@@ -12,6 +12,44 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+import os
 
-def test_me():
-    """A simple test to check a functionality"""
+import click
+
+from zenml import constants
+from zenml.config.constants import GLOBAL_CONFIG_NAME
+from zenml.config.global_config import GlobalConfig
+from zenml.utils import path_utils, yaml_utils
+
+APP_DIR = click.get_app_dir(constants.APP_NAME)
+
+
+def test_global_config_file_creation():
+    """A simple test to check whether the global config is created."""
+    GlobalConfig()
+
+    # Raw config should now exist
+    assert path_utils.file_exists(os.path.join(APP_DIR, GLOBAL_CONFIG_NAME))
+
+
+def test_global_config_persistence():
+    """A simple test to check whether the persistence logic works."""
+    gc = GlobalConfig()
+
+    # Track old one
+    old_analytics_opt_in = gc.analytics_opt_in
+
+    # Toggle it
+    gc.analytics_opt_in = not old_analytics_opt_in
+
+    # Initialize new config
+    gc = GlobalConfig()
+
+    # It still should be equal to the old value, as we have not saved
+    assert old_analytics_opt_in == gc.analytics_opt_in
+
+    # Get raw config
+    raw_config = yaml_utils.read_json(
+        os.path.join(APP_DIR, GLOBAL_CONFIG_NAME)
+    )
+    assert raw_config["analytics_opt_in"] == old_analytics_opt_in
