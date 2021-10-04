649aac642d0aba2e1368f615a882982a6a71ce3d
bcdurak <bariscandurak@hotmail.com>
2021-10-04 16:56:33 +0200

    beam materializer reads headers now

--- src/zenml/materializers/beam_materializer.py
+++ src/zenml/materializers/beam_materializer.py
@@ -13,12 +13,17 @@
 #  permissions and limitations under the License.
 
 import os
+from pathlib import Path
 
 import apache_beam as beam
 
+from zenml.logger import get_logger
 from zenml.materializers.base_materializer import BaseMaterializer
+from zenml.utils import path_utils
 
-DEFAULT_FILENAME = "data"
+logger = get_logger(__name__)
+
+DEFAULT_FILENAME = "data.csv"
 
 
 class BeamMaterializer(BaseMaterializer):
@@ -26,15 +31,43 @@ class BeamMaterializer(BaseMaterializer):
 
     TYPE_NAME = "beam"
 
-    def read_text(self, pipeline):
+    def read_text(self, pipeline, read_header: bool = True):
         """Read from text"""
-        return pipeline | "ReadText" >> beam.io.ReadFromText(
-            file_pattern=os.path.join(self.artifact.uri, "*")
+        pipeline = pipeline | "ReadText" >> beam.io.ReadFromText(
+            file_pattern=os.path.join(self.artifact.uri, "*"),
+            skip_header_lines=True,
         )
 
-    def write_text(self, pipeline, shard_name_template="''"):
+        if read_header:
+            wildcard_qualifier = "*"
+            file_pattern = os.path.join(self.artifact.uri, wildcard_qualifier)
+
+            csv_files = path_utils.list_dir(self.artifact.uri)
+            if not csv_files:
+                raise RuntimeError(
+                    'Split pattern {} does not match any files.'.format(
+                        file_pattern))
+
+            # weed out bad file exts with this logic
+            allowed_file_exts = [".csv", ".txt"]  # ".dat"
+            csv_files = [uri for uri in csv_files
+                         if Path(uri).suffix in allowed_file_exts]
+
+            logger.info(f'Matched {len(csv_files)}: {csv_files}')
+
+            # Always use header from file
+            logger.info(f'Using header from file: {csv_files[0]}.')
+            header = ",".join(path_utils.load_csv_header(csv_files[0]))
+
+            return header, pipeline
+        else:
+            return pipeline
+
+    def write_text(self, pipeline, shard_name_template=None, header=None):
         """Write from text"""
         return pipeline | beam.io.WriteToText(
             os.path.join(self.artifact.uri, DEFAULT_FILENAME),
             shard_name_template=shard_name_template,
+            header=header,
+            num_shards=3,
         )
