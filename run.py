9f10598aaaccc7458fda013229554ebcdeeb9950
bcdurak <bariscandurak@hotmail.com>
2021-10-04 16:55:53 +0200

    artifacts with the proper read and write functions

--- run.py
+++ run.py
@@ -14,55 +14,58 @@
 
 import os
 
+import apache_beam as beam
+import pandas as pd
+
 from zenml import pipeline
 from zenml.annotations import Input, Output, Step
-from zenml.artifacts.data_artifacts.json_artifact import JSONArtifact
-from zenml.artifacts.data_artifacts.text_artifact import TextArtifact
+from zenml.artifacts.data_artifact import DataArtifact
 from zenml.steps import step
 
 
 @step(name="SimplestStepEver")
-def SimplestStepEver(basic_param_1: int, basic_param_2: str) -> int:
+def SimplestStepEver(
+        basic_param_1: int,
+        basic_param_2: str,
+) -> int:
     return basic_param_1 + int(basic_param_2)
 
 
 @step(name="data_ingest")
 def DataIngestionStep(
-    input_random_number: Input[JSONArtifact],
-    output_artifact: Output[TextArtifact],
-    uri: str,
+        input_random_number: Input[int],
+        output_artifact: Output[DataArtifact],
+        uri: str,
 ):
-    import pandas as pd
-
     df = pd.read_csv(uri)
-    output_artifact.materializers.pandas.write(df)
+    output_artifact.materializers.pandas.write_dataframe(df)
 
 
 @step(name="split")
 def DistSplitStep(
-    input_artifact: Input[TextArtifact], output_artifact: Output[TextArtifact]
+        input_artifact: Input[DataArtifact],
+        output_artifact: Output[DataArtifact]
 ):
-    import apache_beam as beam
-
     with beam.Pipeline() as p:
-        data = input_artifact.materializers.beam.read(p)
-        output_artifact.materializers.beam.write(data)
+        header, data = input_artifact.materializers.beam.read_text(p)
+        output_artifact.materializers.beam.write_text(data, header=header)
 
 
 @step(name="preprocessing")
 def InMemPreprocesserStep(
-    input_artifact: Input[TextArtifact], output_artifact: Output[TextArtifact]
+        input_artifact: Input[DataArtifact],
+        output_artifact: Output[DataArtifact]
 ):
-    data = input_artifact.materializers.pandas.read()
-    output_artifact.materializers.pandas.write(data)
+    data = input_artifact.materializers.pandas.read_dataframe()
+    output_artifact.materializers.pandas.write_dataframe(data)
 
 
 @pipeline(name="my_pipeline")
 def SplitPipeline(
-    simple_step: Step[SimplestStepEver],
-    data_step: Step[DataIngestionStep],
-    split_step: Step[DistSplitStep],
-    preprocesser_step: Step[InMemPreprocesserStep],
+        simple_step: Step[SimplestStepEver],
+        data_step: Step[DataIngestionStep],
+        split_step: Step[DistSplitStep],
+        preprocesser_step: Step[InMemPreprocesserStep],
 ):
     data_step(input_random_number=simple_step.outputs["return_output"])
     split_step(input_artifact=data_step.outputs["output_artifact"])
