44d576ecc348444341214e633bedf2b00772d0db
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-06 16:24:38 +0200

    Add materializer factory docstrings

--- src/zenml/materializers/materializer_factory.py
+++ src/zenml/materializers/materializer_factory.py
@@ -12,44 +12,47 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from typing import Text
+from typing import TYPE_CHECKING, ClassVar, Dict, Text, Type
+
+if TYPE_CHECKING:
+    from zenml.artifacts.base_artifact import BaseArtifact
+    from zenml.materializers.base_materializer import BaseMaterializer
 
 
 class MaterializerFactory(object):
     """A factory class which is used by the ZenML artifacts to keep track
     of different materializers"""
 
-    materializer_types = {}
+    materializer_types: ClassVar[Dict[str, Type["BaseMaterializer"]]] = {}
 
-    def __init__(self, artifact):
+    def __init__(self, artifact: "BaseArtifact"):
         """Initialization with an empty factory"""
         self.artifact = artifact
 
-    def __getattr__(self, key: Text):
+    def __getattr__(self, key: Text) -> "BaseMaterializer":
         """Get a single materializers based on the key.
 
         Args:
             key: Indicates the materializer name
 
         Returns:
-            The corresponding writer function within the factory.
+            Instance of a `BaseMaterializer` subclass initialized with the artifact of this factory.
         """
         if key in self.materializer_types:
             return self.materializer_types[key](self.artifact)
         else:
             return object.__getattribute__(self, key)(self.artifact)
 
-    def get_types(self):
-        """Get the materializer dictionary"""
+    def get_types(self) -> Dict[str, Type["BaseMaterializer"]]:
+        """Get all registered materializers."""
         return self.materializer_types
 
     @classmethod
-    def register_type(cls, key, type_):
-        """Register a new materializer in the factory
+    def register_type(cls, key: str, type_: Type["BaseMaterializer"]):
+        """Registers a new materializer.
 
         Args:
-            key: str, which indicates the materializer used within the step
-
-            type_: a ZenML materializer object
+            key: Name with which the materializer can be accessed.
+            type_: A BaseMaterializer subclass.
         """
         cls.materializer_types[key] = type_
