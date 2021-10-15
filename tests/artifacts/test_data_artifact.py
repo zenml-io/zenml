affc256a549ab06dbf1c6232d5bf31bfef096e8f
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 14:35:59 +0200

    add test for split_names_property

--- tests/artifacts/test_data_artifact.py
+++ tests/artifacts/test_data_artifact.py
@@ -13,6 +13,7 @@
 #  permissions and limitations under the License.
 
 import pytest
+from tfx.types.artifact import Property
 
 from zenml.artifacts import data_artifact
 
@@ -21,3 +22,21 @@ from zenml.artifacts import data_artifact
 def data_artifact_fixture():
     """Fixture for creating a model_artifact instance"""
     return data_artifact.DataArtifact()
+
+
+def test_data_artifact_class_has_two_constants(data_artifact_fixture):
+    """Check two constants are defined on DataArtifact class"""
+    assert data_artifact_fixture.TYPE_NAME == "data_artifact"
+    assert isinstance(data_artifact_fixture.PROPERTIES, dict)
+
+
+def test_properties_dict_has_a_split_names_property(data_artifact_fixture):
+    """Check the properties dict has a split_names property"""
+    assert "split_names" in data_artifact_fixture.PROPERTIES
+
+
+def test_split_names_property_is_a_property_type(data_artifact_fixture):
+    """Check the split_names property is a property type"""
+    assert isinstance(
+        data_artifact_fixture.PROPERTIES["split_names"], Property
+    )
