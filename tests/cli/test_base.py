b36b4312b4e62f50e1ecf6570521b9587aec5f9d
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 17:18:44 +0200

    add test for when repo is already initialized

--- tests/cli/test_base.py
+++ tests/cli/test_base.py
@@ -19,6 +19,7 @@ from zenml.cli.base import clean, init
 
 
 @pytest.mark.xfail()
+# TODO: [HIGH] fix failing test
 def test_init_fails_when_repo_path_is_not_git_repo_already(tmp_path):
     """Check that init command raises an InvalidGitRepositoryError
     when executed outside a valid Git repository"""
@@ -30,6 +31,7 @@ def test_init_fails_when_repo_path_is_not_git_repo_already(tmp_path):
 
 
 @pytest.mark.xfail()
+# TODO: [HIGH] fix failing test
 def test_init(tmp_path):
     """Check that init command works as expected inside temporary directory"""
     runner = CliRunner()
@@ -38,6 +40,22 @@ def test_init(tmp_path):
     assert result.exit_code == 0
     assert f"Initializing at {tmp_path}" in result.output
     assert f"ZenML repo initialized at {tmp_path}" in result.output
+
+
+@pytest.mark.xfail()
+# TODO: [HIGH] fix failing test
+def test_assertion_error_raised_when_trying_to_init_when_already_initialized(
+    tmp_path,
+):
+    """Check that an assertion error is raised when trying to initialize
+    a repo that is already initialized"""
+    runner = CliRunner()
+    # initialize the zenml repository
+    runner.invoke(init, ["--repo_path", str(tmp_path)])
+    # run the command a second time in the same directory
+    result = runner.invoke(init, ["--repo_path", str(tmp_path)])
+    assert result.exit_code == 0
+    assert f"{tmp_path} is already initialized!" in result.output
 
 
 def test_clean_only_returns_a_message():
