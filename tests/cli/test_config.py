7b83d6c8a2ed78275abc745fdb0f4fb740bca4a0
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 18:23:11 +0200

    add initial test for analytics opt-in checks

---
+++ tests/cli/test_config.py
@@ -0,0 +1,28 @@
+#  Copyright (c) ZenML GmbH 2020. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+from click.testing import CliRunner
+
+from zenml.cli.config import opt_in
+
+
+def test_analytics_opt_in_amends_global_config():
+    """Check to make sure that analytics opt-in amends global config"""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(opt_in)
+        assert result.exit_code == 0
+        assert "Opted in to analytics." in result.output
+        # TODO: [MEDIUM] Check that the config was updated
+        # find the global config file + read the value
