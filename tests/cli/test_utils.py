c49460b3207b4a47d862a4fe4d5d269fd1166364
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 18:47:07 +0200

    add test for title function

---
+++ tests/cli/test_utils.py
@@ -0,0 +1,35 @@
+#  Copyright (c) ZenML GmbH 2020. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import click
+from click.testing import CliRunner
+from hypothesis import given
+from hypothesis.strategies import text
+
+from zenml.cli.utils import title
+
+
+@given(sample_text=text())
+def test_title_formats_a_string_properly(sample_text):
+    """Check that title function capitalizes text and adds newline"""
+
+    @click.command()
+    @click.argument("text")
+    def title_trial(text):
+        """wrapper function to run title"""
+        title(text)
+
+    runner = CliRunner()
+    result = runner.invoke(title_trial, [sample_text])
+    assert result.output == sample_text.upper() + "\n"
