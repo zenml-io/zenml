6ac06c245d034a48c1e6092311125045fc651347
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 14:05:27 +0200

    add final base artifact test

--- tests/artifacts/test_base_artifact.py
+++ tests/artifacts/test_base_artifact.py
@@ -12,6 +12,33 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+import pytest
 
-def test_me():
-    """A simple test to check a functionality"""
+from zenml.artifacts import base_artifact
+from zenml.materializers.materializer_factory import MaterializerFactory
+from zenml.utils.exceptions import ArtifactInterfaceError
+
+
+@pytest.fixture()
+def artifact():
+    """Fixture for a base_artifact"""
+    return base_artifact.BaseArtifact()
+
+
+def test_baseartifact_class_has_two_constants(artifact):
+    """Check two constants are defined on BaseArtifact class"""
+    assert artifact.TYPE_NAME == "BaseArtifact"
+    assert isinstance(artifact.PROPERTIES, dict)
+
+
+def test_reading_materializers_property_returns_a_materializer_factory(
+    artifact,
+):
+    """Test a MaterializerFactor is returned when reading from base_artifact"""
+    assert isinstance(artifact.materializers, MaterializerFactory)
+
+
+def test_baseartifact_materializer_cannot_be_set(artifact):
+    """Check that setting the materializer isn't possible"""
+    with pytest.raises(ArtifactInterfaceError):
+        artifact.materializers = None
