fef496296d0ef4a857c78e20b8421c48d42ad617
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-15 15:57:17 +0200

    add basic artifact_annotation tests

--- tests/annotations/test_artifact_annotations.py
+++ tests/annotations/test_artifact_annotations.py
@@ -12,8 +12,34 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from zenml.annotations import artifact_annotations  # noqa
+from zenml.annotations import artifact_annotations, base_annotations
 
 
-def test_me():
-    """A simple test to check a functionality"""
+def test_input_is_subclass_of_base_annotation():
+    """Check Input is a subclass of BaseAnnotation"""
+    assert issubclass(
+        artifact_annotations.Input, base_annotations.BaseAnnotation
+    )
+
+
+def test_output_is_subclass_of_base_annotation():
+    """Check Output is a subclass of BaseAnnotation"""
+    assert issubclass(
+        artifact_annotations.Output, base_annotations.BaseAnnotation
+    )
+
+
+def test_input_and_output_have_valid_types_property():
+    """Check that Input and Output have VALID_TYPES properties"""
+    assert hasattr(artifact_annotations.Input, "VALID_TYPES")
+    assert hasattr(artifact_annotations.Output, "VALID_TYPES")
+
+
+def test_input_is_named_input():
+    """Check that the Input annotation is named Input"""
+    assert artifact_annotations.Input.__name__ == "Input"
+
+
+def test_output_is_named_output():
+    """Check that the Output annotation is named Output"""
+    assert artifact_annotations.Output.__name__ == "Output"
