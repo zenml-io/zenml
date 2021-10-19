f4d49414747e85fe58ac40de3f0b5347d7dbe2ac
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-19 11:54:54 +0200

    update based on pre-commit

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -27,9 +27,7 @@ from zenml.utils.analytics_utils import INITIALIZE, track
 @click.option("--repo_path", type=click.Path(exists=True))
 @click.option("--analytics_opt_in", "-a", type=click.BOOL)
 @track(event=INITIALIZE)
-def init(
-    repo_path: str, analytics_opt_in: bool = True
-):
+def init(repo_path: str, analytics_opt_in: bool = True):
     """Initialize ZenML on given path.
 
     Args:
