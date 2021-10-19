49cbe7ae4358acac4e74774d54e21ae3797d158b
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-19 16:21:18 +0200

    implement initial make_pass_decorator version

--- src/zenml/cli/example.py
+++ src/zenml/cli/example.py
@@ -25,15 +25,17 @@ from zenml.constants import APP_NAME, GIT_REPO_URL
 from zenml.utils import path_utils
 
 
-async def clone_zenml_repository(repo_dir: str) -> None:
-    """Clone the zenml repository to a specific directory
-    and checkout branch corresponding to the version
-    of ZenML installed"""
-    config_directory_files = os.listdir(repo_dir)
+class GitExamplesHandler(object):
+    def __init__(self) -> None:
+        repo_dir = click.get_app_dir(APP_NAME)
+        config_directory_files = os.listdir(repo_dir)
 
-    if APP_NAME not in config_directory_files:
-        installed_version = zenml_version_installed
-        Repo.clone_from(GIT_REPO_URL, repo_dir, branch=installed_version)
+        if APP_NAME not in config_directory_files:
+            installed_version = zenml_version_installed
+            Repo.clone_from(GIT_REPO_URL, repo_dir, branch=installed_version)
+
+
+pass_git_examples_handler = click.make_pass_decorator(GitExamplesHandler)
 
 
 def get_examples_dir() -> str:
@@ -41,10 +43,9 @@ def get_examples_dir() -> str:
     return os.path.join(click.get_app_dir(APP_NAME), APP_NAME, "examples")
 
 
-async def get_all_examples() -> List[str]:
+@pass_git_examples_handler
+def get_all_examples() -> List[str]:
     """Get all the examples"""
-    await clone_zenml_repository(click.get_app_dir(APP_NAME))
-
     return [
         name
         for name in sorted(os.listdir(get_examples_dir()))
