7e284ccca5da77394cc434e22de09ed27bc36cea
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-21 09:17:06 +0200

    add reset for analytics status

--- tests/cli/test_config.py
+++ tests/cli/test_config.py
@@ -12,35 +12,51 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+import os
 
 from click import get_app_dir
 from click.testing import CliRunner
 
 from zenml.cli.config import opt_in, opt_out
 from zenml.config.constants import GLOBAL_CONFIG_NAME
+from zenml.config.global_config import GlobalConfig
 from zenml.constants import APP_NAME
 from zenml.utils.yaml_utils import read_json
 
 
 def read_global_config():
     """Read the global config file"""
-    config_file = get_app_dir(APP_NAME) + "/" + GLOBAL_CONFIG_NAME
-    read_json(config_file)
+    config_file = os.path.join(get_app_dir(APP_NAME), GLOBAL_CONFIG_NAME)
+    return read_json(str(config_file))
+
+
+def get_analytics_opt_in_status():
+    """Get the analytics opt-in status"""
+    return read_global_config()["analytics_opt_in"]
+
+
+def set_analytics_opt_in_status(status: bool):
+    """Set the analytics opt-in status"""
+    gc = GlobalConfig()
+    gc.analytics_opt_in = status
+    gc.update()
 
 
 def test_analytics_opt_in_amends_global_config():
     """Check to make sure that analytics opt-in amends global config"""
+    pre_test_status = get_analytics_opt_in_status()
     runner = CliRunner()
     result = runner.invoke(opt_in)
     assert result.exit_code == 0
-    # TODO: [LOW] create a test environment to test this
-    # assert read_global_config()["analytics_opt_in"]
+    assert read_global_config()["analytics_opt_in"]
+    set_analytics_opt_in_status(pre_test_status)
 
 
 def test_analytics_opt_out_amends_global_config():
     """Check to make sure that analytics opt-out amends global config"""
+    pre_test_status = get_analytics_opt_in_status()
     runner = CliRunner()
     result = runner.invoke(opt_out)
     assert result.exit_code == 0
-    # TODO: [LOW] create a test environment to test this
-    # assert not read_global_config()["analytics_opt_in"]
+    assert not read_global_config()["analytics_opt_in"]
+    set_analytics_opt_in_status(pre_test_status)
