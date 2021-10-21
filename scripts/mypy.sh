c5d4f24fec073a96cb568c39fa4e316a607ef146
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-21 11:58:56 +0200

    Update mypy script

--- scripts/mypy.sh
+++ scripts/mypy.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+# Usage:
+# `mypy.sh [--install-types]`
+# * --install-types: Installs missing type stubs before running mypy
+
 set -e
 set -x
 
-SRC=${1:-"src/zenml"}
+if [ "$1" = "--install-types" ]
+then
+  MYPY_ARGS="--install-types --non-interactive"
+fi
+
+SRC="src/zenml"
 
 export ZENML_DEBUG=1
-mypy --python-version=3.6 src/zenml
+mypy $MYPY_ARGS "$SRC"
