78df88947d5e8e44ed7f0bebaacb97394e24e6fa
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-21 13:36:03 +0200

    Rename type checking script, add type checking in main/publish github workflows

--- scripts/type_checker.sh
+++ scripts/type_checker.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Usage:
-# `mypy.sh [--install-types]`
+# `type_checker.sh [--install-types]`
 # * --install-types: Installs missing type stubs before running mypy
 
 set -e
 set -x
+
+export ZENML_DEBUG=1
+SRC="src/zenml"
 
 if [ "$1" = "--install-types" ]
 then
   MYPY_ARGS="--install-types --non-interactive"
 fi
 
-SRC="src/zenml"
-
-export ZENML_DEBUG=1
 mypy $MYPY_ARGS "$SRC"
