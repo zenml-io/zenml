d211e4372e7e5ab1c11af70729244bc267abe8f8
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-22 12:04:02 +0200

    add basic tests for example

--- tests/cli/test_example.py
+++ tests/cli/test_example.py
@@ -11,3 +11,37 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+
+import pytest
+from click.testing import CliRunner
+
+from zenml.cli.example import info, list, pull
+
+ZERO_FIVE_RELEASE_EXAMPLES = ["airflow", "legacy", "quickstart"]
+
+
+@pytest.mark.parametrize("example", ZERO_FIVE_RELEASE_EXAMPLES)
+def test_list_returns_three_examples_for_0_5_release(example: str) -> None:
+    """Check the examples returned from zenml example list"""
+    runner = CliRunner()
+    runner.invoke(pull, ["--force-redownload", "0.5.0"])
+    with runner.isolated_filesystem():
+        result = runner.invoke(list)
+        assert result.exit_code == 0
+        assert example in result.output
+
+
+@pytest.mark.parametrize("example", ZERO_FIVE_RELEASE_EXAMPLES)
+def test_info_returns_zero_exit_code(example: str) -> None:
+    """Check info command exits without errors"""
+    runner = CliRunner()
+    result = runner.invoke(info, [example])
+    assert result.exit_code == 0
+
+
+def test_pull_command_returns_zero_exit_code() -> None:
+    """Check pull command exits without errors"""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(pull)
+        assert result.exit_code == 0
