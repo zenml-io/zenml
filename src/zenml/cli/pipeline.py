d6b0f6d467ca015cfafdfb127f264fcb716961cf
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-22 12:11:42 +0200

    Cleanup cli pipeline run

--- src/zenml/cli/pipeline.py
+++ src/zenml/cli/pipeline.py
@@ -13,64 +13,24 @@
 # #  permissions and limitations under the License.
 """CLI to interact with pipelines."""
 
+import os
+import sys
+import types
+from typing import Any
 
 import click
 
 from zenml.cli.cli import cli
+from zenml.exceptions import PipelineConfigurationError
+from zenml.logger import get_logger
+from zenml.utils import yaml_utils
+
+logger = get_logger(__name__)
 
 
 @cli.group()
 def pipeline() -> None:
     """Pipeline group"""
-
-
-# @pipeline.command('compare')
-# @pass_repo
-# def compare_training_runs(repo: Repository):
-#     """Compares pipelines in repo"""
-#     click.echo('Comparing training pipelines in repo: Starting app..')
-#     repo.compare_training_runs()
-#
-#
-# @pipeline.command('list')
-# @pass_repo
-# def list_pipelines(repo: Repository):
-#     """Lists pipelines in the current repository."""
-#     try:
-#         pipelines = repo.get_pipelines()
-#
-#         names = [p.name for p in pipelines]
-#         types = [p.PIPELINE_TYPE for p in pipelines]
-#         statuses = [p.get_status() for p in pipelines]
-#         cache_enabled = [p.enable_cache for p in pipelines]
-#         filenames = [p.file_name for p in pipelines]
-#
-#         headers = ["name", "type", "cache enabled", "status", "file name"]
-#
-#         click.echo(tabulate(zip(names, types, cache_enabled,
-#                                 statuses, filenames),
-#                             headers=headers))
-#     except Exception as e:
-#         error(e)
-#
-#
-# @pipeline.command('get')
-# @click.argument('pipeline_name')
-# @pass_repo
-# def get_pipeline_by_name(repo: Repository, pipeline_name: str):
-#     """
-#     Gets pipeline from current repository by matching a name against a
-#     pipeline name in the repository.
-#     """
-#     try:
-#         p = repo.get_pipeline_by_name(pipeline_name)
-#     except Exception as e:
-#         error(e)
-#         return
-#
-#     pretty_print(p)
-#
-#
 
 
 @pipeline.command("run", help="Run a pipeline with the given configuration.")
@@ -89,44 +49,35 @@ def run_pipeline(python_file: str, config_path: str) -> None:
         python_file: Path to the python file that defines the pipeline.
         config_path: Path to configuration YAML file.
     """
-    import importlib
-    import os
-    import sys
-
-    from zenml.utils import yaml_utils
-
-    python_file = os.path.abspath(python_file)
-
-    # Add directory of python file to PYTHONPATH so we can import it
-    sys.path.append(os.path.dirname(python_file))
-
-    module_name = os.path.splitext(os.path.basename(python_file))[0]
-    pipeline_module = importlib.import_module(module_name)
-
+    module = _import_python_file(python_file)
     config = yaml_utils.read_yaml(config_path)
 
     pipeline_name = config["name"]
-    pipeline_class = getattr(pipeline_module, pipeline_name)
+    pipeline_class = _get_module_attribute(module, pipeline_name)
 
     steps = {}
     for step_name, step_config in config["steps"].items():
-        step_class = getattr(pipeline_module, step_config["source"])
+        step_class = _get_module_attribute(module, step_config["source"])
         step_instance = step_class()
         materializers_config = step_config.get("materializers", None)
-        if isinstance(materializers_config, str):
-            # Single materializer
-            materializer = getattr(pipeline_module, materializers_config)
-            step_instance = step_instance.with_return_materializers(
-                materializer
-            )
-        elif isinstance(materializers_config, dict):
-            materializers = {}
-            for (
-                output_name,
-                materializer_source,
-            ) in materializers_config.items():
-                materializers[output_name] = getattr(
-                    pipeline_module, materializer_source
+        if materializers_config:
+            # We need to differentiate whether it's a single materializer
+            # or a dictionary mapping output names to materializers
+            if isinstance(materializers_config, str):
+                materializers = _get_module_attribute(
+                    module, materializers_config
+                )
+            elif isinstance(materializers_config, dict):
+                materializers = {
+                    output_name: _get_module_attribute(module, source)
+                    for output_name, source in materializers_config.items()
+                }
+            else:
+                raise PipelineConfigurationError(
+                    f"Only `str` and `dict` values are allowed for "
+                    f"'materializers' attribute of a step configuration. You "
+                    f"tried to pass in `{materializers_config}` (type: "
+                    f"`{type(materializers_config).__name__}`)."
                 )
             step_instance = step_instance.with_return_materializers(
                 materializers
@@ -137,4 +88,47 @@ def run_pipeline(python_file: str, config_path: str) -> None:
     pipeline_instance = pipeline_class(**steps).with_config(
         config_path, overwrite_step_parameters=True
     )
+    logger.debug("Finished setting up pipeline '%s' from CLI", pipeline_name)
     pipeline_instance.run()
+
+
+def _get_module_attribute(module: types.ModuleType, attribute_name: str) -> Any:
+    """Gets an attribute from a module.
+
+    Args:
+        module: The module to load the attribute from.
+        attribute_name: Name of the attribute to load.
+
+    Returns:
+        The attribute value.
+
+    Raises:
+        PipelineConfigurationError: If the module does not have an attribute
+            with the given name.
+    """
+    try:
+        return getattr(module, attribute_name)
+    except AttributeError:
+        raise PipelineConfigurationError(
+            f"Unable to load '{attribute_name}' from"
+            f" file '{module.__file__}'"
+        ) from None
+
+
+def _import_python_file(file_path: str) -> types.ModuleType:
+    """Imports a python file.
+
+    Args:
+        file_path: Path to python file that should be imported.
+
+    Returns:
+        The imported module.
+    """
+    import importlib
+
+    # Add directory of python file to PYTHONPATH so we can import it
+    file_path = os.path.abspath(file_path)
+    sys.path.append(os.path.dirname(file_path))
+
+    module_name = os.path.splitext(os.path.basename(file_path))[0]
+    return importlib.import_module(module_name)
