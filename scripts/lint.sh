e9557cd776f716f5a2216be4071a1a6645f5f9ca
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-26 17:12:39 +0200

    Temporary fix for airflow python3.8 mypy issue

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,10 +5,10 @@ set -x
 SRC=${1:-"src/zenml tests"}
 SRC_NO_TESTS=${1:-"src/zenml"}
 
-mypy $SRC_NO_TESTS
 export ZENML_DEBUG=1
 flake8 $SRC
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py,legacy/* --check
 isort $SRC scripts --check-only
 black $SRC  --check
 interrogate $SRC_NO_TESTS -c pyproject.toml
+mypy $SRC_NO_TESTS
