c284cd35763b66375cd7f18c72443b5713b31928
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-26 14:50:57 +0200

    add component_factory tests

--- tests/core/test_component_factory.py
+++ tests/core/test_component_factory.py
@@ -12,6 +12,37 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+import pytest
 
-def test_me():
-    """A simple test to check a functionality"""
+from zenml.core.component_factory import (
+    ComponentFactory,
+    artifact_store_factory,
+    metadata_store_factory,
+    orchestrator_store_factory,
+)
+
+COMPONENT_FACTORIES = [
+    artifact_store_factory,
+    metadata_store_factory,
+    orchestrator_store_factory,
+]
+
+
+@pytest.mark.parametrize("component_factory", COMPONENT_FACTORIES)
+def test_factories_are_type_component_factory(
+    component_factory: ComponentFactory,
+) -> None:
+    """Checks that factories are component factory types"""
+    assert isinstance(component_factory, ComponentFactory)
+    assert component_factory.name is not None
+    assert isinstance(component_factory.name, str)
+    assert isinstance(component_factory.components, dict)
+
+
+@pytest.mark.parametrize("component_factory", COMPONENT_FACTORIES)
+def test_get_components_returns_components(
+    component_factory: ComponentFactory,
+) -> None:
+    """Checks that get_components method returns a dictionary"""
+    components = component_factory.get_components()
+    assert isinstance(components, dict)
