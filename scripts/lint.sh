58ef525ce63aa7847256b56bc88283277a713353
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-10-26 11:44:52 +0200

    Increase interrogate threshold, disable interrogate for tests

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 
 SRC=${1:-"src/zenml tests"}
+SRC_NO_TESTS=${1:-"src/zenml"}
 
 # mypy src/zenml
 export ZENML_DEBUG=1
@@ -10,4 +11,4 @@ flake8 $SRC
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py,legacy/* --check
 isort $SRC scripts --check-only
 black $SRC  --check
-interrogate $SRC -c pyproject.toml
+interrogate $SRC_NO_TESTS -c pyproject.toml
--- scripts/type_checker.sh
+++ scripts/type_checker.sh
@@ -14,5 +14,4 @@ then
   MYPY_ARGS="--install-types --non-interactive"
 fi
 
-# for now remove this until we figure out the problems with it
 mypy $MYPY_ARGS "$SRC"
