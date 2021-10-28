33398dbdfab6e4ee67ad69d178fd690c09f22315
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-28 18:36:25 +0200

    add another mapping_utils test

--- tests/core/test_mapping_utils.py
+++ tests/core/test_mapping_utils.py
@@ -17,7 +17,7 @@ from uuid import UUID
 from hypothesis import given
 from hypothesis.strategies import integers, text
 
-from zenml.core.mapping_utils import UUIDSourceTuple
+from zenml.core.mapping_utils import UUIDSourceTuple, get_key_from_uuid
 
 
 @given(
@@ -38,3 +38,26 @@ def test_uuidsourcetuple_instance_is_instance_of_pydantic_base_model(
     assert isinstance(uuid_st_instance, UUIDSourceTuple)
     assert uuid_st_instance.uuid == UUID(str(sample_uuid))
     assert uuid_st_instance.source == sample_source
+
+
+@given(
+    sample_uuid=integers(
+        min_value=11111111111111111111111111111111,
+        max_value=99999999999999999999999999999999,
+    ),
+    sample_source=text(min_size=1),
+)
+def test_get_key_from_uuid_returns_key_pointing_to_uuid(
+    sample_uuid: int, sample_source: str
+) -> None:
+    """Check to make sure that get_key_from_uuid returns the key
+    pointing to a certain uuid in a mapping"""
+    uuid_st_instance = UUIDSourceTuple(
+        uuid=UUID(str(sample_uuid)), source=sample_source
+    )
+    mapping_dict = {
+        sample_source: uuid_st_instance,
+    }
+    assert (
+        get_key_from_uuid(uuid_st_instance.uuid, mapping_dict) == sample_source
+    )
