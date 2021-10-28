7a2e28d63a488937f95e2a5a74f570d84b3fc52c
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-10-28 14:25:37 +0200

    add test of metadata listing

--- tests/cli/test_config.py
+++ tests/cli/test_config.py
@@ -14,14 +14,24 @@
 
 import os
 
+import pytest
 from click import get_app_dir
 from click.testing import CliRunner
 
-from zenml.cli.config import opt_in, opt_out
+from zenml.cli.config import (
+    list_metadata_stores,
+    opt_in,
+    opt_out,
+    register_metadata_store,
+    set_logging_verbosity,
+)
 from zenml.config.constants import GLOBAL_CONFIG_NAME
 from zenml.config.global_config import GlobalConfig
-from zenml.constants import APP_NAME
+from zenml.constants import APP_NAME, ZENML_LOGGING_VERBOSITY
+from zenml.metadata.sqlite_metadata_wrapper import SQLiteMetadataStore
 from zenml.utils.yaml_utils import read_json
+
+NOT_LOGGING_LEVELS = ["abc", "my_cat_is_called_aria", "pipeline123"]
 
 
 def read_global_config():
@@ -63,7 +73,42 @@ def test_analytics_opt_out_amends_global_config():
     set_analytics_opt_in_status(pre_test_status)
 
 
-# test logging verbosity raises KeyError when it's not a real level
+@pytest.mark.parametrize("not_a_level", NOT_LOGGING_LEVELS)
+def test_set_logging_verbosity_stops_when_not_real_level(
+    not_a_level: str,
+) -> None:
+    """Check that set_logging_verbosity doesn't run when no real level"""
+    pre_test_logging_status = ZENML_LOGGING_VERBOSITY
+    runner = CliRunner()
+    result = runner.invoke(set_logging_verbosity, [not_a_level])
+    os.environ["ZENML_LOGGING_VERBOSITY"] = pre_test_logging_status
+    assert result.exit_code == 2
+
+
+@pytest.mark.xfail()
+def test_metadata_register_actually_registers_new_metadata_store(
+    tmp_path,
+) -> None:
+    """Test that the metadata register command actually registers a metadata store"""
+    # TODO: [Medium]
+    runner = CliRunner()
+    test_metadata_dir = os.path.join(tmp_path, "metadata.db")
+
+    result = runner.invoke(
+        register_metadata_store,
+        ["test_store", SQLiteMetadataStore(uri=test_metadata_dir)],
+    )
+    assert result.exit_code == 0
+
+
+def test_metadata_list_lists_default_local_metadata_store() -> None:
+    """Test that the metadata list command lists the default local metadata store"""
+    # TODO: [HIGH] add a fixture that spins up a test env each time
+    runner = CliRunner()
+    result = runner.invoke(list_metadata_stores)
+    assert result.exit_code == 0
+    assert "local_metadata_store" in result.output
+
 
 # test metadata register command actually registers a new metadata store
 # test metadata list actually lists newly created metadata stores
