e4bb129154b68b324d58e1c6a98f88adcb75e426
bcdurak <bariscandurak@hotmail.com>
2021-10-29 17:24:54 +0200

    reverted some changes

--- src/zenml/artifact_stores/gcp_artifact_store.py
+++ src/zenml/artifact_stores/gcp_artifact_store.py
@@ -12,39 +12,25 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 from zenml.logger import get_logger
-from zenml.exceptions import IntegrationNotFoundError
+
 logger = get_logger(__name__)
 
-print("IMPORTING..........")
+from pydantic import validator
 
-try:
-    # integration check
-    integrated = True
-except IntegrationNotFoundError:
-    integrated = False
-    logger.warning("some warning")
-
-if integrated:
-    from pydantic import validator
-
-    from zenml.artifact_stores.base_artifact_store import BaseArtifactStore
-    from zenml.core.component_factory import artifact_store_factory
-    from zenml.enums import ArtifactStoreTypes
+from zenml.artifact_stores.base_artifact_store import BaseArtifactStore
+from zenml.core.component_factory import artifact_store_factory
+from zenml.enums import ArtifactStoreTypes
 
 
-    @artifact_store_factory.register(ArtifactStoreTypes.gcp)
-    class GCPArtifactStore(BaseArtifactStore):
-        """Artifact Store for Google Cloud Storage based artifacts."""
+@artifact_store_factory.register(ArtifactStoreTypes.gcp)
+class GCPArtifactStore(BaseArtifactStore):
+    """Artifact Store for Google Cloud Storage based artifacts."""
 
-        @validator("path")
-        def must_be_gcs_path(cls, v: str) -> str:
-            """Validates that the path is a valid gcs path."""
-            if not v.startswith("gs://"):
-                raise ValueError(
-                    "Must be a valid gcs path, i.e., starting with `gs://`"
-                )
-            return v
-
-else:
-    GCPArtifactStore = None
-
+    @validator("path")
+    def must_be_gcs_path(cls, v: str) -> str:
+        """Validates that the path is a valid gcs path."""
+        if not v.startswith("gs://"):
+            raise ValueError(
+                "Must be a valid gcs path, i.e., starting with `gs://`"
+            )
+        return v
