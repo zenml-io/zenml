0a1c754466587d719793e6892a46e5e32603cf1c
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-11-01 10:10:55 +0100

    fix broken test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -19,4 +19,4 @@ from zenml.cli import cli
 
 def test_cli_command_defines_a_cli_group() -> None:
     """Check that cli command defines a CLI group when invoked"""
-    assert isinstance(cli.commands, Group)
+    assert isinstance(cli, Group)
