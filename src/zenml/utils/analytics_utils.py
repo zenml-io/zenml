e04ffd340e208e477a8d9499c7fd2c292190be48
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-11-05 08:47:24 +0100

    add paperspace env check

--- src/zenml/utils/analytics_utils.py
+++ src/zenml/utils/analytics_utils.py
@@ -78,6 +78,13 @@ def in_google_colab() -> bool:
     return False
 
 
+def in_paperspace_gradient() -> bool:
+    """Returns: True if running in a Paperspace Gradient env, else False"""
+    if "PAPERSPACE_NOTEBOOK_REPO_ID" in os.environ:
+        return True
+    return False
+
+
 def get_system_info() -> Dict[str, Any]:
     """Returns system info as a dict.
 
