7a8c9c1733bc652c82822e8bef6fcb83c619ce97
Hamza Tahir <htahir111@gmail.com>
2021-11-12 10:27:09 +0100

    added deploy scripts

---
+++ scripts/deploy-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+set -x
+set -e
+
+DOCS_PATH=${1:-"docs/sphinx_docs/_build/html"}
+
+
+cd $DOCS_PATH && html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true; cd ../
+aws s3 sync $DOCS_PATH s3://$AWS_BUCKET --region eu-central-1 --delete
+aws s3 website s3://$AWS_BUCKET --index-document index.html --error-document 404.html  --region eu-central-1
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
