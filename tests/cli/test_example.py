b2c7ee834d914a1bcabb4527aaa86229e58c78ef
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-11-19 11:33:19 +0100

    add temp_cloned_repo fixture

--- tests/cli/test_example.py
+++ tests/cli/test_example.py
@@ -15,8 +15,11 @@
 from datetime import datetime
 from typing import List
 
+import pytest
+
 from zenml.cli.example import ExamplesRepo, GitExamplesHandler
 from zenml.logger import get_logger
+from zenml.utils import path_utils
 
 logger = get_logger(__name__)
 
@@ -41,6 +44,14 @@ class MockRepo:
         self.tags = tags
 
 
+@pytest.fixture(scope="session")
+def cloned_repo_path(tmp_path) -> None:
+    """Returns the path of a temporary cloned repository"""
+    examples_repo_dir = GitExamplesHandler().examples_repo.examples_dir
+    path_utils.copy_dir(examples_repo_dir, tmp_path)
+    return tmp_path
+
+
 def test_check_if_latest_release_works(monkeypatch):
     """Tests to see that latest_release gets the latest_release"""
     mock_tags = [
@@ -55,7 +66,7 @@ def test_check_if_latest_release_works(monkeypatch):
     assert examples_repo.latest_release == "0.5.2"
 
 
-def test_pull(monkeypatch, mocker) -> None:
+def test_pull(monkeypatch, mocker, cloned_repo_path) -> None:
     """Check what happens when (valid) desired version for a force redownload
     is higher than the latest version stored in the global config"""
     git_examples_handler = GitExamplesHandler()
@@ -67,9 +78,142 @@ def test_pull(monkeypatch, mocker) -> None:
     mocker.patch.object(mock_repo.checkout, return_value=None)
     monkeypatch.setattr(git_examples_handler, "examples_repo", mock_repo)
 
-    # make a fixture that gives us the path to the repository that is cloned
-    # (copy repository to a temp path + then return that path)
-    # monkey patch the cloning_path + the cloning + is_cloned
+
+# make a fixture that gives us the path to the repository that is cloned
+# (copy repository to a temp path + then return that path)
+# monkey patch the cloning_path + the cloning + is_cloned
+
+
+# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
+# def test_list_returns_three_examples_for_0_5_release(
+#     example: str, monkey_patch_clone_repo
+# ) -> None:
+#     """Check the examples returned from zenml example list"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+#         result = runner.invoke(list)
+#         assert result.exit_code == 0
+#         assert example in result.output
+
+
+# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
+# def test_info_returns_zero_exit_code(
+#     example: str, monkey_patch_clone_repo
+# ) -> None:
+#     """Check info command exits without errors"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+#         result = runner.invoke(mock_info, [example])
+#         assert result.exit_code == 0
+
+
+# def test_pull_earlier_version_returns_zero_exit_code(
+#     monkey_patch_clone_repo,
+# ) -> None:
+#     """Check pull of earlier version exits without errors"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.1"])
+#         result = runner.invoke(pull, ["-f", "-v", "0.3.8"])
+#         assert result.exit_code == 0
+
+
+# def test_pull_of_higher_version_than_currently_in_global_config_store(
+#     monkey_patch_clone_repo,
+# ) -> None:
+#     """Check what happens when (valid) desired version for a force redownload
+#     is higher than the latest version stored in the global config"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+#         result = runner.invoke(pull, ["-f", "-v", "0.5.1"])
+#         assert result.exit_code == 0
+
+
+# @pytest.mark.parametrize("bad_version", BAD_VERSIONS)
+# def test_pull_of_bad_version_when_valid_version_already_exists(
+#     bad_version: str, monkey_patch_clone_repo
+# ) -> None:
+#     """When user has valid version present in global config, attempts to force
+#     redownload invalid versions should fail"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.1"])
+#         result = runner.invoke(mock_pull, ["-f", "-v", bad_version])
+#         assert result.exit_code != 0
+
+
+# def test_pull_without_any_flags_should_exit_without_errors(
+#     monkey_patch_clone_repo,
+# ) -> None:
+#     """Check pull command exits without errors"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         result1 = runner.invoke(mock_pull)
+#         assert result1.exit_code == 0
+
+
+# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
+# def test_info_echos_out_readme_content(
+#     example: str, monkey_patch_clone_repo
+# ) -> None:
+#     """Check that info subcommand displays readme content"""
+#     # TODO [LOW]: make test handle rich markdown output
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         # setup the test
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+
+#         # get path variables
+#         repo_dir = click.get_app_dir(APP_NAME)
+#         examples_dir = os.path.join(repo_dir, EXAMPLES_GITHUB_REPO, "examples")
+#         readme_path = os.path.join(examples_dir, example, "README.md")
+
+#         result = runner.invoke(mock_info, [example])
+#         assert result.exit_code == 0
+#         assert example in result.output
+#         with open(readme_path) as f:
+#             for line in f.read().splitlines():
+#                 assert line in result.output
+#         examples_dir = os.path.join(os.getcwd(), EXAMPLES_GITHUB_REPO)
+#         assert example in os.listdir(examples_dir)
+
+
+# @pytest.mark.parametrize("bad_example", NOT_ZERO_FIVE_RELEASE_EXAMPLES)
+# def test_info_fails_gracefully_when_bad_example_given(
+#     tmp_path: str, bad_example: str, monkey_patch_clone_repo
+# ) -> None:
+#     """Check info command fails gracefully when bad example given"""
+#     runner = CliRunner()
+#     with runner.isolated_filesystem(tmp_path):
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+#         result = runner.invoke(mock_info, [bad_example])
+#         assert (
+#             f"Example {bad_example} is not one of the available options."
+#             in result.output
+#         )
+#         assert bad_example not in os.listdir(tmp_path)
+
+
+# @pytest.mark.parametrize("bad_example", NOT_ZERO_FIVE_RELEASE_EXAMPLES)
+# def test_info_fails_gracefully_when_no_readme_present(
+#     tmp_path: str, bad_example: str, monkey_patch_clone_repo
+# ) -> None:
+#     """Check info command fails gracefully when bad example given"""
+#     # get path variables
+#     repo_dir = click.get_app_dir(APP_NAME)
+#     examples_dir = os.path.join(repo_dir, EXAMPLES_GITHUB_REPO, "examples")
+
+#     runner = CliRunner()
+#     with runner.isolated_filesystem(tmp_path):
+#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
+#         fake_example_path = os.path.join(examples_dir, bad_example)
+#         os.mkdir(fake_example_path)
+#         result = runner.invoke(mock_info, [bad_example])
+#         assert "No README.md file found" in result.output
+#         os.rmdir(fake_example_path)
 
 
 # class MockGitExamplesHandler(GitExamplesHandler):
@@ -240,135 +384,3 @@ def test_pull(monkeypatch, mocker) -> None:
 #         "clone_when_examples_already_cloned",
 #         MockGitExamplesHandler.clone_when_examples_already_cloned,
 #     )
-
-
-# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
-# def test_list_returns_three_examples_for_0_5_release(
-#     example: str, monkey_patch_clone_repo
-# ) -> None:
-#     """Check the examples returned from zenml example list"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-#         result = runner.invoke(list)
-#         assert result.exit_code == 0
-#         assert example in result.output
-
-
-# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
-# def test_info_returns_zero_exit_code(
-#     example: str, monkey_patch_clone_repo
-# ) -> None:
-#     """Check info command exits without errors"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-#         result = runner.invoke(mock_info, [example])
-#         assert result.exit_code == 0
-
-
-# def test_pull_earlier_version_returns_zero_exit_code(
-#     monkey_patch_clone_repo,
-# ) -> None:
-#     """Check pull of earlier version exits without errors"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.1"])
-#         result = runner.invoke(pull, ["-f", "-v", "0.3.8"])
-#         assert result.exit_code == 0
-
-
-# def test_pull_of_higher_version_than_currently_in_global_config_store(
-#     monkey_patch_clone_repo,
-# ) -> None:
-#     """Check what happens when (valid) desired version for a force redownload
-#     is higher than the latest version stored in the global config"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-#         result = runner.invoke(pull, ["-f", "-v", "0.5.1"])
-#         assert result.exit_code == 0
-
-
-# @pytest.mark.parametrize("bad_version", BAD_VERSIONS)
-# def test_pull_of_bad_version_when_valid_version_already_exists(
-#     bad_version: str, monkey_patch_clone_repo
-# ) -> None:
-#     """When user has valid version present in global config, attempts to force
-#     redownload invalid versions should fail"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.1"])
-#         result = runner.invoke(mock_pull, ["-f", "-v", bad_version])
-#         assert result.exit_code != 0
-
-
-# def test_pull_without_any_flags_should_exit_without_errors(
-#     monkey_patch_clone_repo,
-# ) -> None:
-#     """Check pull command exits without errors"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         result1 = runner.invoke(mock_pull)
-#         assert result1.exit_code == 0
-
-
-# @pytest.mark.parametrize("example", ZERO_FIVE_ZERO_RELEASE_EXAMPLES)
-# def test_info_echos_out_readme_content(
-#     example: str, monkey_patch_clone_repo
-# ) -> None:
-#     """Check that info subcommand displays readme content"""
-#     # TODO [LOW]: make test handle rich markdown output
-#     runner = CliRunner()
-#     with runner.isolated_filesystem():
-#         # setup the test
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-
-#         # get path variables
-#         repo_dir = click.get_app_dir(APP_NAME)
-#         examples_dir = os.path.join(repo_dir, EXAMPLES_GITHUB_REPO, "examples")
-#         readme_path = os.path.join(examples_dir, example, "README.md")
-
-#         result = runner.invoke(mock_info, [example])
-#         assert result.exit_code == 0
-#         assert example in result.output
-#         with open(readme_path) as f:
-#             for line in f.read().splitlines():
-#                 assert line in result.output
-#         examples_dir = os.path.join(os.getcwd(), EXAMPLES_GITHUB_REPO)
-#         assert example in os.listdir(examples_dir)
-
-
-# @pytest.mark.parametrize("bad_example", NOT_ZERO_FIVE_RELEASE_EXAMPLES)
-# def test_info_fails_gracefully_when_bad_example_given(
-#     tmp_path: str, bad_example: str, monkey_patch_clone_repo
-# ) -> None:
-#     """Check info command fails gracefully when bad example given"""
-#     runner = CliRunner()
-#     with runner.isolated_filesystem(tmp_path):
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-#         result = runner.invoke(mock_info, [bad_example])
-#         assert (
-#             f"Example {bad_example} is not one of the available options."
-#             in result.output
-#         )
-#         assert bad_example not in os.listdir(tmp_path)
-
-
-# @pytest.mark.parametrize("bad_example", NOT_ZERO_FIVE_RELEASE_EXAMPLES)
-# def test_info_fails_gracefully_when_no_readme_present(
-#     tmp_path: str, bad_example: str, monkey_patch_clone_repo
-# ) -> None:
-#     """Check info command fails gracefully when bad example given"""
-#     # get path variables
-#     repo_dir = click.get_app_dir(APP_NAME)
-#     examples_dir = os.path.join(repo_dir, EXAMPLES_GITHUB_REPO, "examples")
-
-#     runner = CliRunner()
-#     with runner.isolated_filesystem(tmp_path):
-#         runner.invoke(mock_pull, ["-f", "-v", "0.5.0"])
-#         fake_example_path = os.path.join(examples_dir, bad_example)
-#         os.mkdir(fake_example_path)
-#         result = runner.invoke(mock_info, [bad_example])
-#         assert "No README.md file found" in result.output
-#         os.rmdir(fake_example_path)
