e1c2cf534fe5d1cec1f7ea74a0b057a96acba3c8
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-11-28 16:51:25 +0100

    add step init docstring

--- src/zenml/steps/__init__.py
+++ src/zenml/steps/__init__.py
@@ -11,7 +11,20 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+"""
+A step is a single piece or stage of a ZenML pipeline. Think of each step as
+being one of the nodes of a Directed Acyclic Graph (or DAG). Steps are responsible for one aspect of processing
+or interacting with the data / artifacts in the pipeline.
 
+ZenML currently
+implements a basic step interface, but there will be other more customized
+interfaces (layered in a hierarchy) for specialized
+implementations. Conceptually, a Step is a discrete and independent part of a
+pipeline that is responsible for one particular aspect of data manipulation
+inside a ZenML pipeline.
+
+Steps can be subclassed from the `BaseStep` class, or used via our `@step` decorator.
+"""
 
 from zenml.steps.base_step import BaseStep  # noqa
 from zenml.steps.step_decorator import step  # noqa
