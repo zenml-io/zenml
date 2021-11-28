beb3362e3194502610b9029f31b7ff609396ac21
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-11-28 12:09:11 +0100

    add beam docstring

--- src/zenml/integrations/beam/__init__.py
+++ src/zenml/integrations/beam/__init__.py
@@ -11,6 +11,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+"""
+
+"""
 from zenml.integrations.constants import BEAM
 from zenml.integrations.integration import Integration
 from zenml.utils.source_utils import import_class_by_path
