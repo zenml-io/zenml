6d1b0e5651042362b0e0eeed519c19b3e9114ff8
Hamza Tahir <htahir111@gmail.com>
2021-12-13 18:05:18 +0100

    Linting fixed

--- src/zenml/artifacts/base_artifact.py
+++ src/zenml/artifacts/base_artifact.py
@@ -13,8 +13,9 @@
 #  permissions and limitations under the License.
 from typing import Dict
 
-from tfx.types.artifact import Artifact, Property, PropertyType
 from ml_metadata.proto import metadata_store_pb2
+from tfx.types.artifact import Artifact, Property, PropertyType
+
 from zenml.artifacts.constants import (
     DATATYPE_PROPERTY_KEY,
     MATERIALIZER_PROPERTY_KEY,
@@ -49,28 +50,36 @@ class BaseArtifact(Artifact):
 
     @classmethod
     def set_zenml_artifact_type(cls):
+        """Set the type of the artifact."""
         type_name = cls.TYPE_NAME
         if not (type_name and isinstance(type_name, str)):
             raise ValueError(
                 (
-                    'The Artifact subclass %s must override the TYPE_NAME attribute '
-                    'with a string type name identifier (got %r instead).') %
-                (cls, type_name))
+                    "The Artifact subclass %s must override the TYPE_NAME attribute "
+                    "with a string type name identifier (got %r instead)."
+                )
+                % (cls, type_name)
+            )
         artifact_type = metadata_store_pb2.ArtifactType()
         artifact_type.name = type_name
         if cls.PROPERTIES:
             # Perform validation on PROPERTIES dictionary.
             if not isinstance(cls.PROPERTIES, dict):
                 raise ValueError(
-                    'Artifact subclass %s.PROPERTIES is not a dictionary.' % cls)
+                    "Artifact subclass %s.PROPERTIES is not a dictionary." % cls
+                )
             for key, value in cls.PROPERTIES.items():
-                if not (isinstance(key,
-                                   (str, bytes)) and isinstance(value,
-                                                                Property)):
+                if not (
+                    isinstance(key, (str, bytes))
+                    and isinstance(value, Property)
+                ):
                     raise ValueError(
                         (
-                            'Artifact subclass %s.PROPERTIES dictionary must have keys of '
-                            'type string and values of type artifact.Property.') % cls)
+                            "Artifact subclass %s.PROPERTIES dictionary must have keys of "
+                            "type string and values of type artifact.Property."
+                        )
+                        % cls
+                    )
 
             # Populate ML Metadata artifact properties dictionary.
             for key, value in cls.PROPERTIES.items():
