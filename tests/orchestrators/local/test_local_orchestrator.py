8c33725f60ebb802a835bab560110c0a0cc93275
bcdurak <bariscandurak@hotmail.com>
2021-12-16 16:54:51 +0100

    removed or renames old files, added licenses, removed commented out tests

--- tests/orchestrators/local/test_local_orchestrator.py
+++ tests/orchestrators/local/test_local_orchestrator.py
@@ -11,7 +11,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-
-
-def test_me():
-    """A simple test to check a functionality"""
