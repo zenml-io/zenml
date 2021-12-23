4355f1854f1d4f9a892f1e4a8c147f80d7bb3ed7
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-12-23 15:50:19 +0100

    add drift detection run starter

--- examples/drift_detection/run.py
+++ examples/drift_detection/run.py
@@ -1,5 +1,4 @@
 #  Copyright (c) ZenML GmbH 2021. All Rights Reserved.
-#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at:
@@ -15,12 +14,66 @@
 import pandas as pd
 from sklearn import datasets
 
-from evidently.model_profile import Profile
-from evidently.profile_sections import DataDriftProfileSection
+from evidently.dashboard import Dashboard
+from evidently.tabs import DataDriftTab
 
-iris = datasets.load_iris()
-iris_frame = pd.DataFrame(iris.data, columns=iris.feature_names)
+from zenml.pipelines import pipeline
+from zenml.steps import Output, step
+from zenml.logger import get_logger
 
-iris_data_drift_profile = Profile(sections=[DataDriftProfileSection()])
-iris_data_drift_profile.calculate(iris_frame, iris_frame, column_mapping=None)
-print(iris_data_drift_profile.json())
+logger = get_logger(__name__)
+
+
+@step
+def data_loader() -> Output(dataset=pd.DataFrame):
+    """Load the breast cancer dataset."""
+    return datasets.load_breast_cancer()
+
+
+@step
+def full_split(input: pd.DataFrame) -> Output(
+    dataset=pd.DataFrame,
+):
+    """Loads the breast cancer dataset as a Pandas dataframe."""
+    return pd.DataFrame(input.data, columns=input.feature_names)
+
+
+@step
+def partial_split(input: pd.DataFrame) -> Output(
+    dataset=pd.DataFrame,
+):
+    """Loads part of the breast cancer dataset as a Pandas dataframe."""
+    return pd.DataFrame(input.data, columns=input.feature_names)[:100]
+
+
+@step()
+def drift_detector(full_data: pd.DataFrame, partial_data: pd.DataFrame) -> None:
+    """Detects a drift in the pipeline."""
+    breast_cancer_data_drift_report = Dashboard(tabs=[DataDriftTab()])
+    breast_cancer_data_drift_report.calculate(full_data, partial_data, column_mapping=None)
+    breast_cancer_data_drift_report.save("reports/breast_cancer_drift_report.html")
+    # logger.info("Drift detected")
+
+
+@pipeline
+def drift_detection_pipeline(
+    data_loader,
+    full_data,
+    partial_data,
+    drift_detector,
+):
+    """Links all the steps together in a pipeline"""
+    data_loader = data_loader()
+    full_data = full_data(data_loader)
+    partial_data = partial_data(data_loader)
+    drift_detector(full_data, partial_data)
+
+
+pipeline = drift_detection_pipeline(
+    data_loader=data_loader(),
+    full_data=full_split(),
+    partial_data=partial_split(),
+    drift_detector=drift_detector(),
+)
+
+pipeline.run()
