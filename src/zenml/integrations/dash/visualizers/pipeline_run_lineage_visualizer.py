9105f2cb3c424d03e7bc45e784e8a2b23c300ca1
Michael Schuster <michael.schuster.ffb@googlemail.com>
2021-12-23 14:31:34 +0100

    Remove unused import

--- src/zenml/integrations/dash/visualizers/pipeline_run_lineage_visualizer.py
+++ src/zenml/integrations/dash/visualizers/pipeline_run_lineage_visualizer.py
@@ -12,7 +12,6 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from abc import abstractmethod
 from typing import Any, Collection, Dict, List, Union
 
 import dash
