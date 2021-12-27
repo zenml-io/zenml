65b44e477d1a7c18f616f6cbb095c7220d12a175
Alexej Penner <thealexejpenner@gmail.com>
2021-12-27 17:32:37 +0100

    Fixed linting issue with Path

--- src/zenml/cli/example.py
+++ src/zenml/cli/example.py
@@ -120,7 +120,7 @@ class LocalExample:
             os.chdir(self.path)
             try:
                 # TODO [ENG-271]: Catch errors that might be thrown in subprocess
-                declare(self.path)
+                declare(str(self.path))
                 if force:
                     subprocess.check_call(
                         [
