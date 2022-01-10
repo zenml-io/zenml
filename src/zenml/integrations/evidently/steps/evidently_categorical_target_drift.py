5d979824e9f62390739e84e12c1e730967fc0ea9
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-01-10 11:46:35 +0100

    add categorical target drift step

---
+++ src/zenml/integrations/evidently/steps/evidently_categorical_target_drift.py
@@ -0,0 +1,70 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+from typing import Optional, cast
+
+from evidently.model_profile import Profile  # type: ignore
+from evidently.pipeline.column_mapping import ColumnMapping  # type: ignore
+from evidently.profile_sections import (  # type: ignore
+    CatTargetDriftProfileSection,
+)
+
+from zenml.artifacts import DataArtifact
+from zenml.steps import StepContext
+from zenml.steps.step_interfaces.base_drift_detection_step import (
+    BaseDriftDetectionConfig,
+    BaseDriftDetectionStep,
+)
+
+
+class EvidentlyCategoricalTargetDriftDetectionConfig(BaseDriftDetectionConfig):
+    """Config class for the Evidently categorical target drift detection step.
+
+    column_mapping: properties of the dataframe's columns used for categorical
+    target drift detection
+    """
+
+    column_mapping: Optional[ColumnMapping]
+
+
+class EvidentlyCategoricalTargetDriftDetectionStep(BaseDriftDetectionStep):
+    """Simple step implementation which implements Evidently's functionality for
+    detecting categorical target data drift."""
+
+    def entrypoint(  # type: ignore[override]
+        self,
+        reference_dataset: DataArtifact,
+        comparison_dataset: DataArtifact,
+        config: EvidentlyCategoricalTargetDriftDetectionConfig,
+        context: StepContext,
+    ) -> dict:  # type: ignore[type-arg]
+        """Main entrypoint for the Evidently categorical target drift detection
+        step.
+
+        Args:
+            reference_dataset: a Pandas dataframe
+            comparison_dataset: a Pandas dataframe of new data you wish to
+                compare against the reference data
+            config: the configuration for the step
+            context: the context of the step
+
+        Returns:
+            a dict containing the results of the drift detection
+        """
+        data_drift_profile = Profile(sections=[CatTargetDriftProfileSection()])
+        data_drift_profile.calculate(
+            reference_dataset,
+            comparison_dataset,
+            column_mapping=config.column_mapping or None,
+        )
+        return cast(dict, data_drift_profile.object())  # type: ignore[type-arg]
