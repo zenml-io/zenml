49fbd81c933ca7778ad6f1c6eba78c9bfbeb9d59
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-01-10 17:29:17 +0100

    remove leftover comment

--- src/zenml/steps/utils.py
+++ src/zenml/steps/utils.py
@@ -411,7 +411,6 @@ class _FunctionExecutor(BaseExecutor):
         if return_type is not None:
             if isinstance(return_type, Output):
                 # Resolve named (and multi-) outputs.
-                # breakpoint()
                 if sum(1 for dummy_var in return_type.items()) == 1:
                     return_values = [return_values]
                 for i, output_tuple in enumerate(return_type.items()):
