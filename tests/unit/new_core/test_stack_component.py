37efb23f3b6e249d2721175fb76f8578025cb942
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-01-12 16:27:50 +0100

    Implement stack component tests

---
+++ tests/unit/new_core/test_stack_component.py
@@ -0,0 +1,87 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+from contextlib import ExitStack as does_not_raise
+
+import pytest
+
+from zenml.enums import StackComponentFlavor, StackComponentType
+from zenml.new_core.stack_component import StackComponent
+
+
+class MockComponentFlavor(StackComponentFlavor):
+    MOCK = "mock"
+
+
+class MockComponent(StackComponent):
+    name = "MockComponent"
+    supports_local_execution = False
+    supports_remote_execution = False
+    some_public_attribute_name = "Aria"
+    _some_private_attribute_name = "Also Aria"
+
+    @property
+    def type(self) -> StackComponentType:
+        return StackComponentType.ORCHESTRATOR
+
+    @property
+    def flavor(self) -> MockComponentFlavor:
+        return MockComponentFlavor.MOCK
+
+
+def test_stack_component_default_method_implementations():
+    """Tests the return values for default implementations of some
+    StackComponent methods."""
+    component = MockComponent()
+
+    assert component.validator is None
+    assert component.log_file is None
+    assert component.runtime_options == {}
+    assert component.requirements == []
+
+    assert component.is_provisioned is False
+    assert component.is_running is False
+
+    with pytest.raises(NotImplementedError):
+        component.provision()
+
+    with pytest.raises(NotImplementedError):
+        component.deprovision()
+
+    with pytest.raises(NotImplementedError):
+        component.resume()
+
+    with pytest.raises(NotImplementedError):
+        component.suspend()
+
+
+def test_stack_component_dict_only_contains_public_attributes():
+    """Tests that the `dict()` method which is used to serialize stack
+    components does not include private attributes."""
+    component = MockComponent()
+    assert component._some_private_attribute_name == "Also Aria"
+
+    expected_dict_keys = {"some_public_attribute_name", "name", "uuid"}
+    assert component.dict().keys() == expected_dict_keys
+
+
+def test_stack_component_public_attributes_are_immutable():
+    """Tests that stack component public attributes are immutable but private
+    attribute can be modified."""
+    component = MockComponent()
+
+    with pytest.raises(TypeError):
+        component.some_public_attribute_name = "Not Aria"
+
+    with does_not_raise():
+        component._some_private_attribute_name = "Woof"
