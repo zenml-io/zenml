464ec9dba8010ee41cb4f23031c62640f431bce4
Alexej Penner <thealexejpenner@gmail.com>
2022-01-12 14:18:49 +0100

    Work in Progress on separte github action for releases

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,7 +1,26 @@
 #!/bin/sh -e
 set -x
 
-SRC=${1:-"src/zenml/"}
+SRC="src/zenml/"
+VERSION=""
+
+
+while :; do
+  case "${1-}" in
+  -s | --source)
+    SRC="${2-}"
+    shift
+    ;;
+  -v | --version)
+    VERSION="${2-}"
+    shift
+    ;;
+  -?*) die "Unknown option: $1" ;;
+  *) break ;;
+  esac
+  shift
+done
+
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
@@ -11,6 +30,11 @@ rm -rf docs/sphinx_docs/api_reference || true
 # only show documented members of a module
 export SPHINX_APIDOC_OPTIONS=members,ignore-module-all
 
-sphinx-apidoc --force --separate --no-toc --module-first --templatedir docs/sphinx_docs/_templates -o docs/sphinx_docs/api_reference $SRC
+if [ -n "$VERSION" ]; then
+  sphinx-apidoc --force --separate --no-toc --module-first --templatedir docs/sphinx_docs/_templates -V $VERSION -o docs/sphinx_docs/api_reference $SRC
+else
+  sphinx-apidoc --force --separate --no-toc --module-first --templatedir docs/sphinx_docs/_templates -o docs/sphinx_docs/api_reference $SRC
+fi
+
 cd docs/sphinx_docs/
 make html
