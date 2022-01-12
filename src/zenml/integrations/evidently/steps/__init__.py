539d0d942c68cd61fe98abc3ac14b024321818c3
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-01-12 13:09:06 +0100

    refactor out classes

--- src/zenml/integrations/evidently/steps/__init__.py
+++ src/zenml/integrations/evidently/steps/__init__.py
@@ -11,3 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+
+from zenml.integrations.evidently.steps.evidently_profile import (
+    EvidentlyProfileConfig,
+    EvidentlyProfileStep,
+)
