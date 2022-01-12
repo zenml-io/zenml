43585d8e6785ed7c604b19396eee1acaec8295aa
James W. Browne <james@zenml.io>
2022-01-12 14:58:50 +0100

    Alexej/eng 300 bugfix windows integration tests (#296)
    * Specified shell as executor and removed python3 specification
    
    * Use shell=True in subprocess calls on Windows
    
    This sets the value for the `shell=` parameter in subprocess calls to
    click's belief whether or not the system is Windows. Setting it to True
    alltogether completely breaks subprocess calls to paths with whitespace
    on MacOS. This commit also moves the `if force` check closer to where
    it takes effect in order to reduce duplicated code. Also adds back in
    integration tests to github actions.
    
    * Add integration tests to actions on PRs
    
    * Make bash the default shell for github actions
    
    * Specify shell executable via CLA/env variable
    
    * adjust github env var syntax for bash
    
    * Add shell executable variable to integration tests
    
    * Make main and PR actions same
    
    * Fix ml_flow integration test from main
    
    * Add back bash shell and hotfix makedirs
    
    * Prefix mlflow backend path with file:
    
    
    Co-authored-by: Alexej Penner <thealexejpenner@gmail.com>

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -12,11 +12,12 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 import shutil
+from os import environ
 from pathlib import Path
 
 import pytest
 
-from zenml.cli import EXAMPLES_RUN_SCRIPT, LocalExample
+from zenml.cli import EXAMPLES_RUN_SCRIPT, SHELL_EXECUTABLE, LocalExample
 from zenml.core.repo import Repository
 from zenml.enums import ExecutionStatus
 
@@ -28,10 +29,24 @@ MLFLOW = "mlflow"
 
 @pytest.fixture
 def examples_dir(clean_repo):
+    # TODO [high]: tests should store zenml artifacts in a new temp directory
     examples_path = Path(clean_repo.path) / "zenml_examples"
     source_path = Path(clean_repo.original_cwd) / "examples"
     shutil.copytree(source_path, examples_path)
     yield examples_path
+
+
+def example_runner(examples_dir):
+    """Get the executable that runs examples.
+
+    By default returns the path to an executable .sh file in the
+    repository, but can also prefix that with the path to a shell
+    / interpreter when the file is not executable on its own. The
+    latter option is needed for windows compatibility.
+    """
+    return (
+        [environ[SHELL_EXECUTABLE]] if SHELL_EXECUTABLE in environ else []
+    ) + [str(examples_dir / EXAMPLES_RUN_SCRIPT)]
 
 
 def test_run_quickstart(examples_dir: Path):
@@ -43,8 +58,7 @@ def test_run_quickstart(examples_dir: Path):
     """
     local_example = LocalExample(examples_dir / QUICKSTART, name=QUICKSTART)
 
-    bash_script_location = examples_dir / EXAMPLES_RUN_SCRIPT
-    local_example.run_example(bash_file=str(bash_script_location), force=True)
+    local_example.run_example(example_runner(examples_dir), force=True)
 
     # Verify the example run was successful
     repo = Repository(path=str(local_example.path))
@@ -69,9 +83,7 @@ def test_run_not_so_quickstart(examples_dir: Path):
     local_example = LocalExample(
         examples_dir / NOT_SO_QUICKSTART, name=NOT_SO_QUICKSTART
     )
-
-    bash_script_location = examples_dir / EXAMPLES_RUN_SCRIPT
-    local_example.run_example(bash_file=str(bash_script_location), force=True)
+    local_example.run_example(example_runner(examples_dir), force=True)
 
     # Verify the example run was successful
     repo = Repository(path=str(local_example.path))
@@ -95,9 +107,7 @@ def test_run_caching(examples_dir: Path):
         bash script.
     """
     local_example = LocalExample(examples_dir / CACHING, name=CACHING)
-
-    bash_script_location = examples_dir / EXAMPLES_RUN_SCRIPT
-    local_example.run_example(bash_file=str(bash_script_location), force=True)
+    local_example.run_example(example_runner(examples_dir), force=True)
 
     # Verify the example run was successful
     repo = Repository(path=str(local_example.path))
@@ -130,9 +140,7 @@ def test_run_mlflow(examples_dir: Path):
         bash script.
     """
     local_example = LocalExample(examples_dir / MLFLOW, name=MLFLOW)
-
-    bash_script_location = examples_dir / EXAMPLES_RUN_SCRIPT
-    local_example.run_example(bash_file=str(bash_script_location), force=True)
+    local_example.run_example(example_runner(examples_dir), force=True)
 
     # Verify the example run was successful
     repo = Repository(path=str(local_example.path))
