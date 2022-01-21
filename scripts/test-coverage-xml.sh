efddcf5e352ee8917b1052b05d778ae977eb5886
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-01-21 19:30:52 +0100

    add extra line

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -12,6 +12,7 @@ TEST_SRC="tests/"${1:-""}
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 export EVIDENTLY_DISABLE_TELEMETRY=1
+
 if [ -n "$1" ]; then
     coverage run -m pytest $TEST_SRC --color=yes
 else
