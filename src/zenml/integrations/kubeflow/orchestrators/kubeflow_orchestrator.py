3326672ffe708d777fee7c02ff93c3ecafbf97db
Hamza Tahir <htahir111@gmail.com>
2022-01-24 18:22:42 +0100

    Kubeflow orchestrator conformed to BaseOrchestrator runtimeconfiguration update

--- src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
+++ src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
@@ -143,7 +143,10 @@ class KubeflowOrchestrator(BaseOrchestrator):
             push_docker_image(image_name)
 
     def run_pipeline(
-        self, pipeline: "BasePipeline", stack: "Stack", run_name: str
+        self,
+        pipeline: "BasePipeline",
+        stack: "Stack",
+        runtime_configuration: "RuntimeConfiguration",
     ) -> Any:
         """Runs a pipeline on Kubeflow Pipelines."""
         from zenml.integrations.kubeflow.docker_utils import get_image_digest
@@ -163,19 +166,25 @@ class KubeflowOrchestrator(BaseOrchestrator):
         runner.run(tfx_pipeline)
 
         self._upload_and_run_pipeline(
+            pipeline_name=pipeline.name,
             pipeline_file_path=pipeline_file_path,
-            run_name=run_name,
+            runtime_configuration=runtime_configuration,
             enable_cache=pipeline.enable_cache,
         )
 
     def _upload_and_run_pipeline(
-        self, pipeline_file_path: str, run_name: str, enable_cache: bool
+        self,
+        pipeline_name: str,
+        pipeline_file_path: str,
+        runtime_configuration: "RuntimeConfiguration",
+        enable_cache: bool,
     ) -> None:
         """Tries to upload and run a KFP pipeline.
 
         Args:
+            pipeline_name: Name of the pipeline.
             pipeline_file_path: Path to the pipeline definition file.
-            run_name: A name for the pipeline run that will be started.
+            runtime_configuration: Runtime configuration of the pipeline run.
             enable_cache: Whether caching is enabled for this pipeline run.
         """
         try:
@@ -190,12 +199,22 @@ class KubeflowOrchestrator(BaseOrchestrator):
 
             # upload the pipeline to Kubeflow and start it
             client = kfp.Client()
-            result = client.create_run_from_pipeline_package(
-                pipeline_file_path,
-                arguments={},
-                run_name=run_name,
-                enable_caching=enable_cache,
-            )
+            if runtime_configuration.schedule:
+                # TODO [HIGH]: Find duplicate experiments.
+                experiment = client.create_experiment(pipeline_name)
+                result = client.create_recurring_run(
+                    experiment_id=experiment.id,
+                    job_name=runtime_configuration.run_name,
+                    pipeline_package_path=pipeline_file_path,
+                    enable_caching=enable_cache,
+                )
+            else:
+                result = client.create_run_from_pipeline_package(
+                    pipeline_file_path,
+                    arguments={},
+                    run_name=runtime_configuration.run_name,
+                    enable_caching=enable_cache,
+                )
             logger.info("Started pipeline run with ID '%s'.", result.run_id)
         except urllib3.exceptions.HTTPError as error:
             logger.warning(
