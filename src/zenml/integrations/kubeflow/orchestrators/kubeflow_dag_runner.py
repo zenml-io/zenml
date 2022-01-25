6e53d908b927c3764712d17bfbb63c52ab791328
bcdurak <bariscandurak@hotmail.com>
2022-01-25 14:20:15 +0100

    minor docstring change

--- src/zenml/integrations/kubeflow/orchestrators/kubeflow_dag_runner.py
+++ src/zenml/integrations/kubeflow/orchestrators/kubeflow_dag_runner.py
@@ -26,7 +26,7 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """The below code is copied from the TFX source repo with minor changes.
-All credits goes to  the TFX team for the core implementation"""
+All credits go to the TFX team for the core implementation"""
 
 import collections
 import copy
