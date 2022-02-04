66aaa7b22e1a9fa4c18772938c6592cf93c2b2aa
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-02-04 10:13:42 +0100

    Try pinning more versions

--- examples/not_so_quickstart/setup.sh
+++ examples/not_so_quickstart/setup.sh
@@ -6,10 +6,12 @@ pre_run () {
   zenml integration install sklearn
   zenml integration install tensorflow
   zenml integration install pytorch
+  pip install click==8.0.3
 }
 
 pre_run_forced () {
   zenml integration install sklearn -f
   zenml integration install tensorflow -f
   zenml integration install pytorch -f
+  pip install --no-input click==8.0.3
 }
