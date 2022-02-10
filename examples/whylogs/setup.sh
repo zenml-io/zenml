893bdf24525fdcd49ad798a1162ff8a841725746
Alexej Penner <thealexejpenner@gmail.com>
2022-02-10 17:19:52 +0100

    A total shot in the dark to see if Windows works like this

--- examples/whylogs/setup.sh
+++ examples/whylogs/setup.sh
@@ -5,9 +5,11 @@ set -Eeo pipefail
 pre_run () {
   zenml integration install whylogs
   zenml integration install sklearn
+  pip install --upgrade --no-deps --force-reinstall scipy
 }
 
 pre_run_forced () {
   zenml integration install whylogs -f
   zenml integration install sklearn -f
+  pip install --upgrade --no-deps --force-reinstall scipy
 }
