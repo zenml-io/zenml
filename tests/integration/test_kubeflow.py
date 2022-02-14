491cc589df1e700a9fc0e4e4b40cdf2c576731c9
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-02-14 13:28:38 +0100

    Implement running remaining example tests on KFP

--- tests/integration/test_kubeflow.py
+++ tests/integration/test_kubeflow.py
@@ -15,6 +15,7 @@ import os
 import shutil
 from collections import namedtuple
 from pathlib import Path
+from typing import Dict
 
 import pytest
 
@@ -65,6 +66,94 @@ def caching_example_validation(repository: Repository):
     assert not second_run.steps[3].is_cached
 
 
+def drift_detection_example_validation(repository: Repository):
+    pipeline = repository.get_pipeline("drift_detection_pipeline")
+    assert pipeline
+
+    run = pipeline.runs[-1]
+    assert run.status == ExecutionStatus.COMPLETED
+
+    # Final step should have output a data drift report
+    drift_detection_step = run.get_step("drift_detector")
+    output = drift_detection_step.outputs["profile"].read()
+    assert isinstance(output, Dict)
+    assert output.get("data_drift") is not None
+
+
+def mlflow_tracking_example_validation(repository: Repository):
+    pipeline = repository.get_pipeline("mlflow_example_pipeline")
+    assert pipeline
+
+    first_run, second_run = pipeline.runs[-2:]
+
+    assert first_run.status == ExecutionStatus.COMPLETED
+    assert second_run.status == ExecutionStatus.COMPLETED
+
+    import mlflow
+    from mlflow.tracking import MlflowClient
+
+    from zenml.integrations.mlflow.mlflow_environment import MLFlowEnvironment
+
+    # Create and activate the global MLflow environment
+    MLFlowEnvironment(repo_root=repository.root).activate()
+
+    # fetch the MLflow experiment created for the pipeline runs
+    mlflow_experiment = mlflow.get_experiment_by_name(pipeline.name)
+    assert mlflow_experiment is not None
+
+    # fetch all MLflow runs created for the pipeline
+    mlflow_runs = mlflow.search_runs(
+        experiment_ids=[mlflow_experiment.experiment_id], output_format="list"
+    )
+    assert len(mlflow_runs) == 2
+
+    # fetch the MLflow run created for the first pipeline run
+    mlflow_runs = mlflow.search_runs(
+        experiment_ids=[mlflow_experiment.experiment_id],
+        filter_string=f'tags.mlflow.runName = "{first_run.name}"',
+        output_format="list",
+    )
+    assert len(mlflow_runs) == 1
+    first_mlflow_run = mlflow_runs[0]
+
+    # fetch the MLflow run created for the second pipeline run
+    mlflow_runs = mlflow.search_runs(
+        experiment_ids=[mlflow_experiment.experiment_id],
+        filter_string=f'tags.mlflow.runName = "{second_run.name}"',
+        output_format="list",
+    )
+    assert len(mlflow_runs) == 1
+    second_mlflow_run = mlflow_runs[0]
+
+    client = MlflowClient()
+    # fetch the MLflow artifacts logged during the first pipeline run
+    artifacts = client.list_artifacts(first_mlflow_run.info.run_id)
+    assert len(artifacts) == 3
+
+    # fetch the MLflow artifacts logged during the second pipeline run
+    artifacts = client.list_artifacts(second_mlflow_run.info.run_id)
+    assert len(artifacts) == 3
+
+
+def whylogs_example_validation(repository: Repository):
+    pipeline = repository.get_pipeline("data_profiling_pipeline")
+    assert pipeline
+
+    run = pipeline.runs[-1]
+    assert run.status == ExecutionStatus.COMPLETED
+
+    from whylogs import DatasetProfile
+
+    profiles = [
+        run.get_step("data_loader").outputs["profile"].read(),
+        run.get_step("train_data_profiler").output.read(),
+        run.get_step("test_data_profiler").output.read(),
+    ]
+
+    for profile in profiles:
+        assert isinstance(profile, DatasetProfile)
+
+
 ExampleIntegrationTestConfiguration = namedtuple(
     "ExampleIntegrationTestConfiguration", ["name", "validation_function"]
 )
@@ -99,6 +188,17 @@ examples = [
             pipeline_name="mnist_pipeline"
         ),
     ),
+    # ExampleIntegrationTestConfiguration(
+    #     name="drift_detection",
+    #     validation_function=drift_detection_example_validation,
+    # ),
+    # ExampleIntegrationTestConfiguration(
+    #     name="mlflow_tracking",
+    #     validation_function=mlflow_tracking_example_validation,
+    # ),
+    # ExampleIntegrationTestConfiguration(
+    #     name="whylogs", validation_function=whylogs_example_validation
+    # ),
 ]
 
 
@@ -157,6 +257,7 @@ def shared_kubeflow_repo(base_repo, tmp_path_factory, module_mocker):
 
     yield repo
 
+    kubeflow_stack.deprovision()
     os.chdir(str(base_repo.root))
     shutil.rmtree(tmp_path)
 
@@ -208,132 +309,3 @@ def test_run_example_on_kfp(
     )
     _wait_for_kubeflow_pipeline()
     example.validation_function(Repository(root=local_example.path))
-
-
-# def test_run_kubeflow(examples_dir: Path):
-#     """Testing the functionality of the kubeflow example
-#
-#     Args:
-#         examples_dir: Temporary folder containing all examples including the run_examples
-#         bash script.
-#     """
-#     local_example = LocalExample(examples_dir / KUBEFLOW, name=KUBEFLOW)
-#     shutil.copytree(examples_dir.parent / ".zen", local_example.path / ".zen")
-#     local_example.run_example(
-#         example_runner(examples_dir), force=False, prevent_stack_setup=True
-#     )
-#     _wait_for_kubeflow_pipeline()
-#
-#     # Verify the example run was successful
-#     repo = Repository(local_example.path)
-#     pipeline = repo.get_pipelines()[0]
-#     assert pipeline.name == "mnist_pipeline"
-#
-#     pipeline_run = pipeline.runs[-1]
-#
-#     assert pipeline_run.status == ExecutionStatus.COMPLETED
-#
-#     for step in pipeline_run.steps:
-#         assert step.status == ExecutionStatus.COMPLETED
-#
-#
-# def test_run_custom_materializer(examples_dir: Path):
-#     """Testing the functionality of the custom materializer example.
-#
-#     Args:
-#         examples_dir: Temporary folder containing all examples including the
-#                       run_examples bash script.
-#     """
-#     local_example = LocalExample(
-#         examples_dir / CUSTOM_MATERIALIZER, name=CUSTOM_MATERIALIZER
-#     )
-#     shutil.copytree(examples_dir.parent / ".zen", local_example.path / ".zen")
-#     local_example.run_example(
-#         example_runner(examples_dir), force=False, prevent_stack_setup=True
-#     )
-#     _wait_for_kubeflow_pipeline()
-#
-#     # Verify the example run was successful
-#     repo = Repository(local_example.path)
-#     pipeline = repo.get_pipelines()[0]
-#     first_run = pipeline.runs[-1]
-#
-#     # Both runs should be completed
-#     assert first_run.status == ExecutionStatus.COMPLETED
-
-
-# def test_run_mlflow(examples_dir: Path):
-#     """Testing the functionality of the quickstart example
-#
-#     Args:
-#         examples_dir: Temporary folder containing all examples including the run_examples
-#         bash script.
-#     """
-#     local_example = LocalExample(examples_dir / MLFLOW, name=MLFLOW)
-#     shutil.copytree(examples_dir.parent / ".zen", local_example.path / ".zen")
-#     local_example.run_example(
-#         example_runner(examples_dir), force=False, prevent_stack_setup=True
-#     )
-#     _wait_for_kubeflow_pipeline()
-#
-#     # Verify the example run was successful
-#     repo = Repository(local_example.path)
-#     pipeline = repo.get_pipelines()[0]
-#     assert pipeline.name == "mlflow_example_pipeline"
-#
-#     first_run = pipeline.runs[-2]
-#     second_run = pipeline.runs[-1]
-#
-#     # Both runs should be completed
-#     assert first_run.status == ExecutionStatus.COMPLETED
-#     assert second_run.status == ExecutionStatus.COMPLETED
-#
-#     for step in first_run.steps:
-#         assert step.status == ExecutionStatus.COMPLETED
-#     for step in second_run.steps:
-#         assert step.status == ExecutionStatus.COMPLETED
-#
-#     import mlflow
-#     from mlflow.tracking import MlflowClient
-#
-#     from zenml.integrations.mlflow.mlflow_environment import MLFlowEnvironment
-#
-#     # Create and activate the global MLflow environment
-#     MLFlowEnvironment(local_example.path).activate()
-#
-#     # fetch the MLflow experiment created for the pipeline runs
-#     mlflow_experiment = mlflow.get_experiment_by_name(pipeline.name)
-#     assert mlflow_experiment is not None
-#
-#     # fetch all MLflow runs created for the pipeline
-#     mlflow_runs = mlflow.search_runs(
-#         experiment_ids=[mlflow_experiment.experiment_id], output_format="list"
-#     )
-#     assert len(mlflow_runs) == 2
-#
-#     # fetch the MLflow run created for the first pipeline run
-#     mlflow_runs = mlflow.search_runs(
-#         experiment_ids=[mlflow_experiment.experiment_id],
-#         filter_string=f'tags.mlflow.runName = "{first_run.name}"',
-#         output_format="list",
-#     )
-#     assert len(mlflow_runs) == 1
-#     first_mlflow_run = mlflow_runs[0]
-#
-#     # fetch the MLflow run created for the second pipeline run
-#     mlflow_runs = mlflow.search_runs(
-#         experiment_ids=[mlflow_experiment.experiment_id],
-#         filter_string=f'tags.mlflow.runName = "{second_run.name}"',
-#         output_format="list",
-#     )
-#     assert len(mlflow_runs) == 1
-#     second_mlflow_run = mlflow_runs[0]
-#
-#     client = MlflowClient()
-#     # fetch the MLflow artifacts logged during the first pipeline run
-#     artifacts = client.list_artifacts(first_mlflow_run.info.run_id)
-#     assert len(artifacts) == 3
-#
-#     # fetch the MLflow artifacts logged during the second pipeline run
-#     artifacts = client.list_artifacts(second_mlflow_run.info.run_id)
-#     assert len(artifacts) == 3
