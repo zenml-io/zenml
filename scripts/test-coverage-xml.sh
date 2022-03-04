8674de2b70585857096e3f46ad20ae05b0a4c793
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-03-04 14:08:16 +0100

    parallel tests only for unit tests

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -17,10 +17,10 @@ export EVIDENTLY_DISABLE_TELEMETRY=1
 # all available CPU cores.
 # The '-vv' flag enables pytest-clarity output when tests fail.
 if [ -n "$1" ]; then
-    coverage run -m pytest $TEST_SRC --color=yes -n auto -vv
+    coverage run -m pytest $TEST_SRC --color=yes -vv
 else
     coverage run -m pytest tests/unit --color=yes -vv -n auto
-    coverage run -m pytest tests/integration --use-virtualenv --color=yes -vv -n auto
+    coverage run -m pytest tests/integration --use-virtualenv --color=yes -vv
 fi
 coverage combine
 coverage report --show-missing
