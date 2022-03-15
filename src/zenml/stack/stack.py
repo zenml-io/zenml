b52d9ecffe91142338eb4c12ad5d4ef660dece69
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-03-15 16:57:58 +0100

    add secrets manager to stack

--- src/zenml/stack/stack.py
+++ src/zenml/stack/stack.py
@@ -42,6 +42,7 @@ if TYPE_CHECKING:
     from zenml.metadata_stores import BaseMetadataStore
     from zenml.orchestrators import BaseOrchestrator
     from zenml.pipelines import BasePipeline
+    from zenml.secrets_manager import BaseSecretsManager
     from zenml.stack import StackComponent
     from zenml.step_operators import BaseStepOperator
 
@@ -67,6 +68,7 @@ class Stack:
         metadata_store: "BaseMetadataStore",
         artifact_store: "BaseArtifactStore",
         container_registry: Optional["BaseContainerRegistry"] = None,
+        secrets_manager: Optional["BaseSecretsManager"] = None,
         step_operator: Optional["BaseStepOperator"] = None,
     ):
         """Initializes and validates a stack instance.
@@ -80,6 +82,7 @@ class Stack:
         self._artifact_store = artifact_store
         self._container_registry = container_registry
         self._step_operator = step_operator
+        self._secrets_manager = secrets_manager
 
         self.validate()
 
@@ -104,6 +107,7 @@ class Stack:
         from zenml.container_registries import BaseContainerRegistry
         from zenml.metadata_stores import BaseMetadataStore
         from zenml.orchestrators import BaseOrchestrator
+        from zenml.secrets_manager import BaseSecretsManager
         from zenml.step_operators import BaseStepOperator
 
         def _raise_type_error(
@@ -136,6 +140,12 @@ class Stack:
         ):
             _raise_type_error(container_registry, BaseContainerRegistry)
 
+        secrets_manager = components.get(StackComponentType.SECRETS_MANAGER)
+        if secrets_manager is not None and not isinstance(
+            secrets_manager, BaseSecretsManager
+        ):
+            _raise_type_error(secrets_manager, BaseSecretsManager)
+
         step_operator = components.get(StackComponentType.STEP_OPERATOR)
         if step_operator is not None and not isinstance(
             step_operator, BaseStepOperator
@@ -148,6 +158,7 @@ class Stack:
             metadata_store=metadata_store,
             artifact_store=artifact_store,
             container_registry=container_registry,
+            secrets_manager=secrets_manager,
             step_operator=step_operator,
         )
 
@@ -194,6 +205,7 @@ class Stack:
                 self.metadata_store,
                 self.artifact_store,
                 self.container_registry,
+                self.secrets_manager,
                 self.step_operator,
             ]
             if component is not None
@@ -223,6 +235,11 @@ class Stack:
     def container_registry(self) -> Optional["BaseContainerRegistry"]:
         """The container registry of the stack."""
         return self._container_registry
+
+    @property
+    def secrets_manager(self) -> Optional["BaseSecretsManager"]:
+        """The secrets manager of the stack."""
+        return self._secrets_manager
 
     @property
     def step_operator(self) -> Optional["BaseStepOperator"]:
