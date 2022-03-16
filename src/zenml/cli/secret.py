ef2e24967077cc7c945df36aff8b8eb9d81b282c
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-03-16 15:47:26 +0100

    update with alexej's complex fix

--- src/zenml/cli/secret.py
+++ src/zenml/cli/secret.py
@@ -12,6 +12,8 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+from typing import List
+
 import click
 
 from zenml.cli.cli import cli
@@ -19,13 +21,17 @@ from zenml.cli.utils import confirmation, error
 from zenml.console import console
 from zenml.enums import StackComponentType
 from zenml.repository import Repository
-from zenml.stack.stack_component import StackComponent
+from zenml.secrets_manager.base_secrets_manager import BaseSecretsManager
 
 
 # Secrets
 @cli.group()
-def secret() -> None:
+@click.pass_context
+def secret(ctx) -> None:
     """Secrets for storing key-value pairs for use in authentication."""
+    ctx.obj = Repository().active_stack.components.get(
+        StackComponentType.SECRETS_MANAGER, None
+    )
 
 
 @secret.command("register")
@@ -38,14 +44,14 @@ def secret() -> None:
     required=True,
     type=str,
 )
+@click.pass_obj
 def register_secret(
+    secrets_manager: "BaseSecretsManager",
     name: str,
     secret_value: str,
 ) -> None:
     """Create a secret."""
-    secrets_manager = Repository().active_stack.components.get(
-        StackComponentType.SECRETS_MANAGER, None
-    )
+    # TODO [MEDIUM] Implement interactive mode for registering secrets
     with console.status(f"Creating secret `{name}`..."):
         try:
             secrets_manager.register_secret(name, secret_value)
@@ -56,18 +62,10 @@ def register_secret(
 
 @secret.command("get")
 @click.argument("name", type=str)
-def get_secret(
-    secrets_manager: StackComponentType.SECRETS_MANAGER, name: str
-) -> None:
+@click.pass_obj
+def get_secret(secrets_manager: "BaseSecretsManager", name: str) -> None:
     """Get a secret, given its name."""
-    secrets_manager = Repository().active_stack.components.get(
-        StackComponentType.SECRETS_MANAGER, None
-    )
     with console.status(f"Getting secret `{name}`..."):
-        active_stack = Repository().active_stack
-        secrets_manager = active_stack.components.get(
-            StackComponent.SECRETS_MANAGER
-        )
         try:
             secret_value = secrets_manager.get_secret_by_key(name)
             console.print(f"Secret for `{name.upper()}` is `{secret_value}`.")
@@ -75,15 +73,24 @@ def get_secret(
             error(e)
 
 
+@secret.command("list")
+@click.pass_obj
+def list_secrets(secrets_manager: "BaseSecretsManager") -> List[str]:
+    """Get a list of all the keys to secrets in the store."""
+    with console.status("Getting secret keys..."):
+        try:
+            secret_keys = secrets_manager.get_all_secret_keys()
+            # TODO: [HIGH] implement as a table?
+            console.print(secret_keys)
+        except KeyError as e:
+            error(e)
+
+
 @secret.command("delete")
 @click.argument("name", type=str)
-def delete_secret(
-    secrets_manager: StackComponentType.SECRETS_MANAGER, name: str
-) -> None:
+@click.pass_obj
+def delete_secret(secrets_manager: "BaseSecretsManager", name: str) -> None:
     """Delete a secret, given its name."""
-    secrets_manager = Repository().active_stack.components.get(
-        StackComponentType.SECRETS_MANAGER, None
-    )
     confirmation_response = confirmation(
         f"This will delete the secret associated with `{name}`. "
         "Are you sure you want to proceed?"
@@ -92,10 +99,6 @@ def delete_secret(
         console.print("Aborting secret deletion...")
     else:
         with console.status(f"Deleting secret `{name}`..."):
-            active_stack = Repository().active_stack
-            secrets_manager = active_stack.components.get(
-                StackComponent.SECRETS_MANAGER
-            )
             try:
                 secrets_manager.delete_secret_by_key(name)
                 console.print(f"Deleted secret for `{name.upper()}`.")
@@ -113,20 +116,14 @@ def delete_secret(
     required=True,
     type=str,
 )
+@click.pass_obj
 def update_secret(
-    secrets_manager: StackComponentType.SECRETS_MANAGER,
+    secrets_manager: "BaseSecretsManager",
     name: str,
     secret_value: str,
 ) -> None:
     """Update a secret."""
-    secrets_manager = Repository().active_stack.components.get(
-        StackComponentType.SECRETS_MANAGER, None
-    )
     with console.status(f"Updating secret `{name}`..."):
-        active_stack = Repository().active_stack
-        secrets_manager = active_stack.components.get(
-            StackComponent.SECRETS_MANAGER
-        )
         try:
             secrets_manager.update_secret_by_key(name, secret_value)
             console.print(f"Secret `{name.upper()}` updated.")
