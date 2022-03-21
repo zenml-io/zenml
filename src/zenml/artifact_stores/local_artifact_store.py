cf81ca3dc1e6b9290ea49364445e430813fee8f9
bcdurak <bariscandurak@hotmail.com>
2022-03-21 10:50:54 +0100

    converting the local version

--- src/zenml/artifact_stores/local_artifact_store.py
+++ src/zenml/artifact_stores/local_artifact_store.py
@@ -1,4 +1,18 @@
-#  Copyright (c) ZenML GmbH 2021. All Rights Reserved.
+# Copyright 2020 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,11 +26,17 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from pydantic import validator
+import glob
+import os
+import shutil
+from typing import Any, Callable, Iterable, List, Optional, Set, Tuple, Union
+
+from tfx.dsl.io.fileio import NotFoundError
 
 from zenml.artifact_stores import BaseArtifactStore
 from zenml.enums import ArtifactStoreFlavor
-from zenml.io import fileio
+
+PathType = Union[bytes, str]
 
 
 class LocalArtifactStore(BaseArtifactStore):
@@ -26,16 +46,109 @@ class LocalArtifactStore(BaseArtifactStore):
     supports_remote_execution = False
 
     @property
-    def flavor(self) -> ArtifactStoreFlavor:
+    def flavor(self) -> str:
         """The artifact store flavor."""
-        return ArtifactStoreFlavor.LOCAL
+        return ArtifactStoreFlavor.LOCAL.value
 
-    @validator("path")
-    def ensure_path_is_local(cls, path: str) -> str:
-        """Ensures that the artifact store path is local."""
-        if fileio.is_remote(path):
-            raise ValueError(
-                f"Path '{path}' specified for LocalArtifactStore is not a "
-                f"local path."
+    @property
+    def supported_schemes(self) -> Set[str]:
+        return {""}
+
+    @staticmethod
+    def open(name: PathType, mode: str = "r") -> Any:
+        try:
+            return open(name, mode=mode)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def copyfile(src: PathType, dst: PathType, overwrite: bool = False) -> None:
+        if not overwrite and os.path.exists(dst):
+            raise OSError(
+                (
+                    "Destination file %r already exists and argument "
+                    "`overwrite` is false."
+                )
+                % dst
             )
-        return path
+        try:
+            shutil.copyfile(src, dst)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def exists(path: PathType) -> bool:
+        return os.path.exists(path)
+
+    @staticmethod
+    def glob(pattern: PathType) -> List[PathType]:
+        return glob.glob(pattern)
+
+    @staticmethod
+    def isdir(path: PathType) -> bool:
+        return os.path.isdir(path)
+
+    @staticmethod
+    def listdir(path: PathType) -> List[PathType]:
+        try:
+            return os.listdir(path)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def makedirs(path: PathType) -> None:
+        os.makedirs(path, exist_ok=True)
+
+    @staticmethod
+    def mkdir(path: PathType) -> None:
+        try:
+            os.mkdir(path)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def remove(path: PathType) -> None:
+        try:
+            os.remove(path)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def rename(src: PathType, dst: PathType, overwrite: bool = False) -> None:
+        if not overwrite and os.path.exists(dst):
+            raise OSError(
+                (
+                    "Destination path %r already exists and argument "
+                    "`overwrite` is false."
+                )
+                % dst
+            )
+        try:
+            os.rename(src, dst)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def rmtree(path: PathType) -> None:
+        try:
+            shutil.rmtree(path)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def stat(path: PathType) -> Any:
+        try:
+            return os.stat(path)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
+
+    @staticmethod
+    def walk(
+        top: PathType,
+        topdown: bool = True,
+        onerror: Optional[Callable[..., None]] = None,
+    ) -> Iterable[Tuple[PathType, List[PathType], List[PathType]]]:
+        try:
+            yield from os.walk(top, topdown=topdown, onerror=onerror)
+        except FileNotFoundError as e:
+            raise NotFoundError() from e
