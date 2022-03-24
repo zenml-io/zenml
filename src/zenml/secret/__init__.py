c0985da3642eda9fa311a03f3cac12052b6a8d0b
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-03-24 12:02:28 +0100

    update main docstring definition

--- src/zenml/secret/__init__.py
+++ src/zenml/secret/__init__.py
@@ -13,7 +13,14 @@
 #  permissions and limitations under the License.
 """
 A ZenML Secret is a grouping of key-value pairs. These are accessed and
-administered via the ZenML Secret Manager (a stack component)
+administered via the ZenML Secret Manager (a stack component).
+
+Secrets are distinguished by having different schemas. An AWS SecretSchema, for
+example, has key-value pairs for `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`
+as well as an optional `AWS_SESSION_TOKEN`. If you don't specify a schema at the
+point of registration, ZenML will set the schema as `ArbitrarySecretSchema`, a
+kind of default schema where things that aren't attached to a grouping can be
+stored.
 """
 from zenml.secret.base_secret import BaseSecretSchema
 from zenml.secret.secret_schema_class_registry import SecretSchemaClassRegistry
