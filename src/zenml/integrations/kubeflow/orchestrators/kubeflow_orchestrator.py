bb2be9ea6fe80a333d43f795585cc13bbe3fb838
Safoine El Khabich <safoine.ext@zenml.io>
2022-03-31 19:27:14 +0000

    Fix The error raising when secrets manager isn't set with the active stack

--- src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
+++ src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
@@ -508,14 +508,18 @@ class KubeflowOrchestrator(BaseOrchestrator):
 
         Raises:
             ProvisioningError: If the stack has no secrets manager."""
-
+        environment_vars: Dict[str, str] = {}
         secret_manager = Repository().active_stack.secrets_manager
-        if not secret_manager:
+        if secrets and not secret_manager:
             raise ProvisioningError(
                 "Unable to provision local Kubeflow Pipelines deployment: "
                 "Missing secrets manager in current stack."
             )
-        environment_vars: Dict[str, str] = {}
-        for secret in secrets:
-            environment_vars.update(secret_manager.get_secret(secret))
+        else:
+            for secret in secrets:
+                if secret_manager:
+                    secret_schema = secret_manager.get_secret(secret)
+                    environment_vars.update(secret_schema.content)
+                else:
+                    environment_vars.update({})
         return environment_vars
