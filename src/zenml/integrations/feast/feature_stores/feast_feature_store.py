32e374cd800de8c1f6a6c632c31a76faf38b9b3a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-04-07 20:49:49 +0200

    update attribute

--- src/zenml/integrations/feast/feature_stores/feast_feature_store.py
+++ src/zenml/integrations/feast/feature_stores/feast_feature_store.py
@@ -36,7 +36,7 @@ class FeastFeatureStore(BaseFeatureStore):
 
     online_host: str = "localhost"
     online_port: int = 6379
-    feast_repo: str = ""
+    feast_repo: str
 
     def get_historical_features(
         self,
