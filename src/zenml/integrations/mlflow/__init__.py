98bf7dd964d6984a56187b9ad313c1795561fe83
wjayesh <jayeshsharma.w@live.in>
2022-04-11 21:38:04 +0530

    add model deployers to the mlflow integration

--- src/zenml/integrations/mlflow/__init__.py
+++ src/zenml/integrations/mlflow/__init__.py
@@ -36,6 +36,7 @@ class MlflowIntegration(Integration):
         from zenml.integrations.mlflow.mlflow_environment import (
             MLFlowEnvironment,
         )
+        from zenml.integrations.mlflow import model_deployers
 
         # Create and activate the global MLflow environment
         MLFlowEnvironment().activate()
