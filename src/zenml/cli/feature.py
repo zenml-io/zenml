d51a902b337a5b3a1f658210b019ef2ab1413d8e
Hamza Tahir <htahir111@gmail.com>
2022-04-11 17:01:07 +0200

    Fixed import issues with BaseFeatureStore

--- src/zenml/cli/feature.py
+++ src/zenml/cli/feature.py
@@ -1,3 +1,4 @@
+# type: ignore
 #  Copyright (c) ZenML GmbH 2020. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,15 +13,14 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+# TODO [HIGH]: Generalize these commands to all feature stores.
 
 import click
 
 from zenml.cli.cli import cli
 from zenml.cli.utils import declare, error
 from zenml.enums import StackComponentType
-from zenml.integrations.feast.feature_stores.feast_feature_store import (
-    FeastFeatureStore,
-)
+from zenml.feature_stores.base_feature_store import BaseFeatureStore
 from zenml.repository import Repository
 
 
@@ -41,7 +41,7 @@ def feature(ctx: click.Context) -> None:
 
 @feature.command("get-data-sources")
 @click.pass_obj
-def get_data_sources(feature_store: "FeastFeatureStore") -> None:
+def get_data_sources(feature_store: "BaseFeatureStore") -> None:
     """Get all data sources from the feature store."""
     data_sources = feature_store.get_data_sources()
     declare(f"Data sources: {data_sources}")
@@ -49,7 +49,7 @@ def get_data_sources(feature_store: "FeastFeatureStore") -> None:
 
 @feature.command("get-entities")
 @click.pass_obj
-def get_entities(feature_store: "FeastFeatureStore") -> None:
+def get_entities(feature_store: "BaseFeatureStore") -> None:
     """Get all entities from the feature store."""
     entities = feature_store.get_entities()
     declare(f"Entities: {entities}")
@@ -57,7 +57,7 @@ def get_entities(feature_store: "FeastFeatureStore") -> None:
 
 @feature.command("get-feature-services")
 @click.pass_obj
-def get_feature_services(feature_store: "FeastFeatureStore") -> None:
+def get_feature_services(feature_store: "BaseFeatureStore") -> None:
     """Get all feature services from the feature store."""
     feature_services = feature_store.get_feature_services()
     declare(f"Feature services: {feature_services}")
@@ -65,7 +65,7 @@ def get_feature_services(feature_store: "FeastFeatureStore") -> None:
 
 @feature.command("get-feature-views")
 @click.pass_obj
-def get_feature_views(feature_store: "FeastFeatureStore") -> None:
+def get_feature_views(feature_store: "BaseFeatureStore") -> None:
     """Get all feature views from the feature store."""
     feature_views = feature_store.get_feature_views()
     declare(f"Feature views: {feature_views}")
@@ -73,7 +73,7 @@ def get_feature_views(feature_store: "FeastFeatureStore") -> None:
 
 @feature.command("get-project")
 @click.pass_obj
-def get_project(feature_store: "FeastFeatureStore") -> None:
+def get_project(feature_store: "BaseFeatureStore") -> None:
     """Get the current project name from the feature store."""
     project = feature_store.get_project()
     declare(f"Project name: {project}")
@@ -81,7 +81,7 @@ def get_project(feature_store: "FeastFeatureStore") -> None:
 
 @feature.command("get-feast-version")
 @click.pass_obj
-def get_feast_version(feature_store: "FeastFeatureStore") -> None:
+def get_feast_version(feature_store: "BaseFeatureStore") -> None:
     """Get the current Feast version being used."""
     version = feature_store.get_feast_version()
     declare(f"Feast version: {version}")
