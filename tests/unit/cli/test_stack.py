f4c30639e35983b67b85b5cb3144424dcfbd32e8
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-04-19 11:24:14 +0200

    add tests for updating stacks

--- tests/unit/cli/test_stack.py
+++ tests/unit/cli/test_stack.py
@@ -17,6 +17,11 @@ from click.testing import CliRunner
 
 from zenml.artifact_stores import LocalArtifactStore
 from zenml.cli.stack import describe_stack, update_stack
+from zenml.orchestrators import LocalOrchestrator
+from zenml.secrets_managers.local.local_secrets_manager import (
+    LocalSecretsManager,
+)
+from zenml.stack import Stack
 
 NOT_STACKS = ["abc", "my_other_cat_is_called_blupus", "stack123"]
 
@@ -41,14 +46,68 @@ def test_stack_describe_fails_for_bad_input(
     assert result.exit_code == 1
 
 
-def test_updating_active_stack_fails(clean_repo) -> None:
-    """Test stack update of active stack fails."""
+def test_updating_active_stack_succeeds(clean_repo) -> None:
+    """Test stack update of active stack succeeds."""
     new_artifact_store = LocalArtifactStore(name="arias_store", path="/meow")
     clean_repo.register_stack_component(new_artifact_store)
 
     runner = CliRunner()
-    result = runner.invoke(
-        update_stack, ["default", "-a", "artifact_store_name"]
+    result = runner.invoke(update_stack, ["default", "-a", "arias_store"])
+    assert result.exit_code == 0
+    assert clean_repo.active_stack.artifact_store == new_artifact_store
+
+
+def test_updating_non_active_stack_succeeds(clean_repo) -> None:
+    """Test stack update of pre-existing component of non-active stack succeeds."""
+    new_orchestrator = LocalOrchestrator(name="arias_orchestrator")
+    clean_repo.register_stack_component(new_orchestrator)
+
+    registered_stack = clean_repo.active_stack
+    new_stack = Stack(
+        name="arias_new_stack",
+        orchestrator=registered_stack.orchestrator,
+        metadata_store=registered_stack.metadata_store,
+        artifact_store=registered_stack.artifact_store,
     )
+    clean_repo.register_stack(new_stack)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        update_stack, ["arias_new_stack", "-o", "arias_orchestrator"]
+    )
+    assert result.exit_code == 0
+    assert (
+        clean_repo.get_stack("arias_new_stack").orchestrator == new_orchestrator
+    )
+
+
+def test_adding_to_stack_succeeds(clean_repo) -> None:
+    """Test stack update by adding a new component to a stack
+    succeeds."""
+    local_secrets_manager = LocalSecretsManager(name="arias_secrets_manager")
+    clean_repo.register_stack_component(local_secrets_manager)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        update_stack, ["default", "-x", "arias_secrets_manager"]
+    )
+
+    assert result.exit_code == 0
+    assert clean_repo.get_stack("default").secrets_manager is not None
+    assert (
+        clean_repo.get_stack("default").secrets_manager == local_secrets_manager
+    )
+
+
+def test_updating_nonexistent_stack_fails(clean_repo) -> None:
+    """Test stack update of nonexistent stack fails."""
+    local_secrets_manager = LocalSecretsManager(name="arias_secrets_manager")
+    clean_repo.register_stack_component(local_secrets_manager)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        update_stack, ["not_a_stack", "-x", "arias_secrets_manager"]
+    )
+
     assert result.exit_code == 1
-    assert clean_repo.active_stack.artifact_store != new_artifact_store
+    assert clean_repo.get_stack("default").secrets_manager is None
