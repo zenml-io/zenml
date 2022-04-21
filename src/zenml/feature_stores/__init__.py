b714795ee081df9bcc64969165acd696e582ddaf
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-04-21 10:58:40 +0200

    Remove todo from docstring

--- src/zenml/feature_stores/__init__.py
+++ src/zenml/feature_stores/__init__.py
@@ -12,7 +12,6 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """
-# TODO [ENG-793]: Write docstring.
 ## Feature Store
 
 Feature stores allow data teams to serve data via an offline store and an online
