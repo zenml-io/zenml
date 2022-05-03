8b705518f65878850375c0fc72f896534fecd23d
Hamza Tahir <hamza@zenml.io>
2022-05-03 16:32:07 +0200

    Simplified pytorch materializer

--- src/zenml/integrations/pytorch/materializers/pytorch_materializer.py
+++ src/zenml/integrations/pytorch/materializers/pytorch_materializer.py
@@ -13,13 +13,12 @@
 #  permissions and limitations under the License.
 
 import os
-from typing import Any, Type, Union
+from typing import Any, Type
 
 import torch
 from torch.nn import Module  # type: ignore[attr-defined]
 
 from zenml.artifacts import ModelArtifact
-from zenml.integrations.pytorch.materializers.pytorch_types import TorchDict
 from zenml.io import fileio
 from zenml.materializers.base_materializer import BaseMaterializer
 
@@ -28,13 +27,15 @@ CHECKPOINT_FILENAME = "checkpoint.pt"
 
 
 class PyTorchMaterializer(BaseMaterializer):
-    """Materializer to read/write Pytorch models."""
+    """Materializer to read/write Pytorch models. Inspired by the guide:
+    https://pytorch.org/tutorials/beginner/saving_loading_models.html"""
 
-    ASSOCIATED_TYPES = (Module, TorchDict)
+    ASSOCIATED_TYPES = (Module,)
     ASSOCIATED_ARTIFACT_TYPES = (ModelArtifact,)
 
-    def handle_input(self, data_type: Type[Any]) -> Union[Module, TorchDict]:
-        """Reads and returns a PyTorch model.
+    def handle_input(self, data_type: Type[Any]) -> Module:
+        """Reads and returns a PyTorch model. Only loads the model, not
+        the checkpoint.
 
         Returns:
             A loaded pytorch model.
@@ -45,21 +46,23 @@ class PyTorchMaterializer(BaseMaterializer):
         ) as f:
             return torch.load(f)  # type: ignore[no-untyped-call]  # noqa
 
-    def handle_return(self, model: Union[Module, TorchDict]) -> None:
-        """Writes a PyTorch model.
+    def handle_return(self, model: Module) -> None:
+        """Writes a PyTorch model, as a model and a checkpoint.
 
         Args:
             model: A torch.nn.Module or a dict to pass into model.save
         """
         super().handle_return(model)
 
-        # Save entire model to artifact directory, This is the default behavior for loading model in development phase (training, evaluation)
+        # Save entire model to artifact directory,
+        # This is the default behavior for loading model in development phase (training, evaluation)
         with fileio.open(
             os.path.join(self.artifact.uri, DEFAULT_FILENAME), "wb"
         ) as f:
             torch.save(model, f)
 
-        # Save model checkpoint to artifact directory, This is the default behavior for loading model in production phase (inference)
+        # Also save model checkpoint to artifact directory,
+        # This is the default behavior for loading model in production phase (inference)
         if isinstance(model, Module):
             with fileio.open(
                 os.path.join(self.artifact.uri, CHECKPOINT_FILENAME), "wb"
