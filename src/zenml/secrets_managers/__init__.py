c0d59b3e4211dd37ff5625afa07d21446866ae90
Safoine El Khabich <safoine.ext@zenml.io>
2022-05-05 09:55:57 +0000

    Adding explain text for secrets manager

--- src/zenml/secrets_managers/__init__.py
+++ src/zenml/secrets_managers/__init__.py
@@ -14,7 +14,16 @@
 """
 ## Secret Manager
 
-...
+Most projects involving either cloud infrastructure or of a certain complexity
+will involve secrets of some kind. For example, you'll use secrets to connecting
+to AWS, which requires an `access_key_id` and a `secret_access_key`. On your
+local machine this is usually stored in your `~/.aws/credentials` file.
+
+You might find you need to access those secrets from within your Kubernetes
+cluster as it runs individual steps. You might also just want a centralized
+location for the storage of secrets across your ZenML project. 
+
+ZenML gives you all this with the Secret Manager stack component.
 """
 from zenml.secrets_managers.base_secrets_manager import BaseSecretsManager
 from zenml.secrets_managers.local.local_secrets_manager import (
