8641610007ffdc06aea98c9daaba415a66649234
bcdurak <bariscandurak@hotmail.com>
2022-05-10 10:16:08 +0200

    first iteration on the metadata stores

--- src/zenml/metadata_stores/mysql_metadata_store.py
+++ src/zenml/metadata_stores/mysql_metadata_store.py
@@ -11,10 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-from typing import ClassVar, Union
+from typing import ClassVar, Optional, Union
 
 from ml_metadata.proto import metadata_store_pb2
-from tfx.orchestration import metadata
+from ml_metadata.proto.metadata_store_pb2 import MySQLDatabaseConfig
+from pydantic import root_validator
 
 from zenml.metadata_stores import BaseMetadataStore
 
@@ -27,6 +28,9 @@ class MySQLMetadataStore(BaseMetadataStore):
     database: str
     username: str
     password: str
+    ssl_key: Optional[str]
+    ssl_cert: Optional[str]
+    ssl_ca: Optional[str]
 
     # Class Configuration
     FLAVOR: ClassVar[str] = "mysql"
@@ -38,10 +42,41 @@ class MySQLMetadataStore(BaseMetadataStore):
         metadata_store_pb2.MetadataStoreClientConfig,
     ]:
         """Return tfx metadata config for mysql metadata store."""
-        return metadata.mysql_metadata_connection_config(
-            host=self.host,
-            port=self.port,
-            database=self.database,
-            username=self.username,
-            password=self.password,
-        )
+        if self.ssl_key:
+            return metadata_store_pb2.ConnectionConfig(
+                mysql=MySQLDatabaseConfig(
+                    host=self.host,
+                    port=self.port,
+                    database=self.database,
+                    user=self.username,
+                    password=self.password,
+                    ssl_options=metadata_store_pb2.MySQLDatabaseConfig.SSLOptions(
+                        key=self.ssl_key,
+                        cert=self.ssl_cert,
+                        ca=self.ssl_ca,
+                    ),
+                )
+            )
+        else:
+            return metadata_store_pb2.ConnectionConfig(
+                MySQLDatabaseConfig(
+                    host=self.host,
+                    port=self.port,
+                    database=self.database,
+                    user=self.username,
+                    password=self.password,
+                )
+            )
+
+    @root_validator()
+    def ssl_options_have_correct_keys(cls, values):
+        required_keys = ["ssl_key", "ssl_cert", "ssl_ca"]
+        if any(key in values for key in required_keys):
+            if not all(key in values for key in required_keys):
+                raise ValueError(
+                    f"Missing key(s): "
+                    f"{[key not in values for key in required_keys]} "
+                    f"in the SSL options of your MySQL Metadata Store."
+                )
+
+        return values
