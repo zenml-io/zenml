5b1becc6dd22256d4e6cfd90118f97bc34d34da1
bcdurak <bariscandurak@hotmail.com>
2022-05-10 11:37:30 +0200

    second iteration

--- src/zenml/metadata_stores/mysql_metadata_store.py
+++ src/zenml/metadata_stores/mysql_metadata_store.py
@@ -11,13 +11,17 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-from typing import ClassVar, Optional, Union
+from typing import ClassVar, Union, Any
 
 from ml_metadata.proto import metadata_store_pb2
 from ml_metadata.proto.metadata_store_pb2 import MySQLDatabaseConfig
-from pydantic import root_validator
 
 from zenml.metadata_stores import BaseMetadataStore
+from zenml.repository import Repository
+import os
+
+SSL_KEYS = ['ssl_key', 'ssl_ca', 'ssl_cert']
+BASE_PATH = os.getcwd()
 
 
 class MySQLMetadataStore(BaseMetadataStore):
@@ -26,11 +30,8 @@ class MySQLMetadataStore(BaseMetadataStore):
     host: str
     port: int
     database: str
-    username: str
-    password: str
-    ssl_key: Optional[str]
-    ssl_cert: Optional[str]
-    ssl_ca: Optional[str]
+
+    secret: str
 
     # Class Configuration
     FLAVOR: ClassVar[str] = "mysql"
@@ -42,41 +43,51 @@ class MySQLMetadataStore(BaseMetadataStore):
         metadata_store_pb2.MetadataStoreClientConfig,
     ]:
         """Return tfx metadata config for mysql metadata store."""
-        if self.ssl_key:
-            return metadata_store_pb2.ConnectionConfig(
-                mysql=MySQLDatabaseConfig(
-                    host=self.host,
-                    port=self.port,
-                    database=self.database,
-                    user=self.username,
-                    password=self.password,
-                    ssl_options=metadata_store_pb2.MySQLDatabaseConfig.SSLOptions(
-                        key=self.ssl_key,
-                        cert=self.ssl_cert,
-                        ca=self.ssl_ca,
-                    ),
-                )
-            )
-        else:
-            return metadata_store_pb2.ConnectionConfig(
-                MySQLDatabaseConfig(
-                    host=self.host,
-                    port=self.port,
-                    database=self.database,
-                    user=self.username,
-                    password=self.password,
-                )
-            )
 
-    @root_validator()
-    def ssl_options_have_correct_keys(cls, values):
-        required_keys = ["ssl_key", "ssl_cert", "ssl_ca"]
-        if any(key in values for key in required_keys):
-            if not all(key in values for key in required_keys):
-                raise ValueError(
-                    f"Missing key(s): "
-                    f"{[key not in values for key in required_keys]} "
-                    f"in the SSL options of your MySQL Metadata Store."
+        mysql_secret = self._obtain_mysql_secret()
+
+        mysql_config = MySQLDatabaseConfig(
+            host=self.host,
+            port=self.port,
+            database=self.database,
+            user=mysql_secret.username,
+            password=mysql_secret.password,
+        )
+
+        if any(key in mysql_secret for key in SSL_KEYS):
+            if not all(key in mysql_secret for key in SSL_KEYS):
+                raise RuntimeError(
+                    f"Missing ssl keys in secret: "
+                    f"{[key for key in SSL_KEYS if key not in mysql_secret]}"
                 )
 
-        return values
+            for key in SSL_KEYS:
+                content = mysql_secret[key]
+                target_path = os.path.join(BASE_PATH, f"{key}.pem")
+                with open(target_path) as f:
+                    f.write(content)
+
+            ssl_options = MySQLDatabaseConfig.SSLOptions(
+                cert=mysql_secret.cert,
+                ca=mysql_secret.ca,
+                key=mysql_secret.key,
+            )
+            mysql_config.ssl_options.CopyFrom(ssl_options)
+
+        return metadata_store_pb2.ConnectionConfig(mysql=mysql_config)
+
+    def _obtain_mysql_secret(self) -> Any:
+        secret_manager = Repository().active_stack.secrets_manager
+
+        if not secret_manager:
+            raise RuntimeError("You dont have the secret manager")  # TODO
+
+        try:
+            mysql_secret = secret_manager.get_secret(self.secret)
+        except KeyError:
+            raise RuntimeError("You dont have the right key")
+
+        # if isinstance(mysql_secret, MySQLSchema):
+        #     raise RuntimeError("You dont have the right secret schema.")
+
+        return mysql_secret
