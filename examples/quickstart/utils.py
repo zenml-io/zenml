af80ed9d2ffedbba7f15f09153cd53e832ba0e19
Hamza Tahir <htahir111@gmail.com>
2022-05-11 17:34:34 +0200

    Checkpoint for quickstart

---
+++ examples/quickstart/utils.py
@@ -0,0 +1,22 @@
+from IPython.display import HTML, Javascript, display
+
+def display_restart_button():
+    display(HTML(
+        '''
+            <script>
+                code_show = false;
+                function restart(){
+                    IPython.notebook.kernel.restart();
+                }
+                function code_toggle() {
+                    if (code_show) {
+                        $('div.input').hide(200);
+                    } else {
+                        $('div.input').show(200);
+                    }
+                    code_show = !code_show
+                }
+            </script>
+            <button onclick="restart()">Click to Restart Kernel</button>
+        '''
+    ))
