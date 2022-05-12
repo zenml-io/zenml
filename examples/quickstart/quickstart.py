a68d3e70689dbf4110a09a86a7651aef1a2033db
Hamza Tahir <htahir111@gmail.com>
2022-05-12 15:31:35 +0200

    Quickstart formatted

--- examples/quickstart/quickstart.py
+++ examples/quickstart/quickstart.py
@@ -24,11 +24,19 @@ from steps.evaluator.sklearn_evaluator import evaluator
 from steps.importer.import_digits import importer
 from steps.trainer.sklearn_svc_trainer import svc_trainer_mlflow
 
+from zenml.integrations.dash.visualizers.pipeline_run_lineage_visualizer import (
+    PipelineRunLineageVisualizer,
+)
 from zenml.integrations.evidently.steps import (
     EvidentlyProfileConfig,
     EvidentlyProfileStep,
 )
+from zenml.integrations.evidently.visualizers import EvidentlyVisualizer
+from zenml.integrations.facets.visualizers.facet_statistics_visualizer import (
+    FacetStatisticsVisualizer,
+)
 from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+from zenml.repository import Repository
 
 if __name__ == "__main__":
 
@@ -48,3 +56,30 @@ if __name__ == "__main__":
     )
 
     p.run()
+
+    repo = Repository()
+    pipelines = repo.get_pipelines()
+    my_pipeline = repo.get_pipeline(pipeline_name="quickstart_pipeline")
+    latest_run = my_pipeline.runs[-1]
+
+    PipelineRunLineageVisualizer().visualize(latest_run)
+
+    outputs = latest_run.get_step(name="get_reference_data")
+    FacetStatisticsVisualizer().visualize(outputs)
+
+    drift_detection_step = latest_run.get_step(name="drift_detector")
+    EvidentlyVisualizer().visualize(drift_detection_step)
+
+    model_deployer = repo.active_stack.model_deployer
+    services = model_deployer.find_model_server(
+        pipeline_name="quickstart_pipeline",
+        pipeline_step_name="mlflow_model_deployer_step",
+        running=True,
+    )
+    service = services[0]
+    service.check_status()
+
+    X_test = latest_run.steps[0].outputs["X_test"].read()
+    y_test = latest_run.steps[0].outputs["y_test"].read()
+
+    print(f"Model predicted {pred0}, true label was {y_test[0]}")
