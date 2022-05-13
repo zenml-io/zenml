f50e69c3a97045fc104f90f665d20f620867fdf7
Alexej Penner <thealexejpenner@gmail.com>
2022-05-13 11:19:30 +0200

    Call poetry directly

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,5 @@ set -e
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-python -m poetry config repositories.test-pypi https://test.pypi.org/legacy/
-python -m poetry publish -r testpypi --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
+poetry config repositories.test-pypi https://test.pypi.org/legacy/
+poetry publish -r testpypi --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
