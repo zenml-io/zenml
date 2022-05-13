00300b469331d4562d1d8afb4ebd1e2ab976a5a2
Alexej Penner <thealexejpenner@gmail.com>
2022-05-13 17:35:41 +0200

    Cleaned up debugging changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,4 @@ set -e
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-poetry config repositories.test-pypi https://test.pypi.org/legacy/
-poetry publish -r testpypi --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
+poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
