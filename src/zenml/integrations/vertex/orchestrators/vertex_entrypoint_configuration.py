c3f65e98341f8462a6f42b7918031c8736e11723
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-05-24 14:23:50 +0200

    Add vertex job id option

--- src/zenml/integrations/vertex/orchestrators/vertex_entrypoint_configuration.py
+++ src/zenml/integrations/vertex/orchestrators/vertex_entrypoint_configuration.py
@@ -12,13 +12,38 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from kfp.v2 import dsl
+# import os
+# from google.cloud import aiplatform as aip
+from typing import TYPE_CHECKING, Any, List, Set
 
 from zenml.entrypoints import StepEntrypointConfiguration
+
+if TYPE_CHECKING:
+    from zenml.steps import BaseStep
+
+VERTEX_JOB_ID_OPTION = "vertex_job_id"
 
 
 class VertexEntrypointConfiguration(StepEntrypointConfiguration):
     """Entrypoint configuration for running steps on Vertex AI Pipelines."""
 
+    @classmethod
+    def get_custom_entrypoint_options(cls) -> Set[str]:
+        """Vertex AI Pipelines specific entrypoint options.
+
+        The argument `VERTEX_JOB_ID_OPTION` allows to specify the job id of the
+        Vertex AI Pipeline and get it in the execution of the step, via the `get_run_name`
+        method.
+        """
+        return {VERTEX_JOB_ID_OPTION}
+
+    @classmethod
+    def get_custom_entrypoint_arguments(
+        cls, step: "BaseStep", *args: Any, **kwargs: Any
+    ) -> List[str]:
+        """Sets the value for the `VERTEX_JOB_ID_OPTION` argument."""
+        return [f"--{VERTEX_JOB_ID_OPTION}", kwargs[VERTEX_JOB_ID_OPTION]]
+
     def get_run_name(self, pipeline_name: str) -> str:
-        return dsl.PIPELINE_JOB_ID_PLACEHOLDER
+        """Returns the Vertex AI Pipeline job id."""
+        return self.entrypoint_args[VERTEX_JOB_ID_OPTION]
