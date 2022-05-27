4318b59dd5be8403bc4434fda9cdb92aa38455e0
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2022-05-27 11:25:23 +0200

    Add checking `stack.container_registry` is not `None` (mypy)

--- src/zenml/integrations/vertex/orchestrators/vertex_orchestrator.py
+++ src/zenml/integrations/vertex/orchestrators/vertex_orchestrator.py
@@ -124,7 +124,7 @@ class VertexOrchestrator(BaseOrchestrator):
 
             # Validate that the container registry is not local.
             container_registry = stack.container_registry
-            if container_registry.is_local:
+            if container_registry and container_registry.is_local:
                 return False, (
                     f"The Vertex orchestrator does not support local container registries. "
                     f"You should replace the component '{container_registry.name}' "
