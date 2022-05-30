f3e519c46b3a180c7f92cbd8701f0864befe5f44
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-05-30 15:02:42 +0200

    Make zenserver dependencies optional

--- src/zenml/__init__.py
+++ src/zenml/__init__.py
@@ -23,6 +23,11 @@ from zenml.logger import init_logging  # noqa
 
 init_logging()
 
-# Import ZenServer here because it needs to be registered in the service registry
-# early on in order to be available for use in other modules.
-from zenml.zen_server.zen_server import ZenServer
+# Try to import the ZenServer here because it needs to be registered in the 
+# service registry early on in order to be available for use in other modules.
+# If the ZenServer dependencies aren't installed, there is no need to register 
+# it anywhere so we simply pass.
+try:
+    from zenml.zen_server.zen_server import ZenServer
+except ImportError:
+    pass
