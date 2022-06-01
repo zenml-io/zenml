5cb05c747a82a42345736adfecfd88681422964a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-06-01 11:36:38 +0200

    add model_deployers to scripts

--- scripts/docstring.sh
+++ scripts/docstring.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io"}
+DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io src/zenml/model_deployers"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
