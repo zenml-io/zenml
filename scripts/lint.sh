61d011f31944462f34102b64f3daf6698d59dac6
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-06-02 05:49:21 +0200

    Docstring fixes for `entrypoints` and `experiment_trackers` modules (#672)
    * add new modules into scripts
    
    * add docstrings for entrypoints and experiment_trackers
    
    * apply PR fixes
    
    * fix typo
    
    * invalidate cache

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,7 +8,7 @@ SRC_NO_TESTS=${1:-"src/zenml"}
 # separate env variable while incrementally completing the docstring task.
 # add modules to this list as/when they are completed.
 # When we're done we can remove this and just use `SRC_NO_TESTS`.
-DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io src/zenml/model_deployers src/zenml/cli"}
+DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io src/zenml/model_deployers src/zenml/cli src/zenml/entrypoints src/zenml/experiment_trackers"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
