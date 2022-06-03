a004aa38d145c65e60542999b0fe69494b37116f
Alexej Penner <thealexejpenner@gmail.com>
2022-06-03 21:55:12 +0200

    Using correct docker login

--- scripts/setup_kubeflow_aws_connection.sh
+++ scripts/setup_kubeflow_aws_connection.sh
@@ -7,5 +7,5 @@ export AWS_EKS_CLUSTER="zenhacks-cluster"
 export ECR_REGISTRY_NAME="715803424590.dkr.ecr.us-east-1.amazonaws.com"
 export KUBE_CONTEXT="zenml-eks"
 
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY_NAME
+docker login --username AWS -p $(aws ecr get-login-password --region us-east-1) 715803424590.dkr.ecr.us-east-1.amazonaws.com
 aws eks --region $AWS_REGION update-kubeconfig --name $AWS_EKS_CLUSTER --alias $KUBE_CONTEXT
