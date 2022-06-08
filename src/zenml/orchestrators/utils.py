5984a492f0af21c720df42eaf9cd07f10d672034
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-06-08 11:52:35 +0200

    Update step cache detection, don't store unused metadata connection config in pb2 pipeline

--- src/zenml/orchestrators/utils.py
+++ src/zenml/orchestrators/utils.py
@@ -12,20 +12,13 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-import json
-from typing import TYPE_CHECKING, List, cast
+from typing import TYPE_CHECKING, List
 
 import tfx.orchestration.pipeline as tfx_pipeline
-from tfx.orchestration.portable import data_types
 from tfx.proto.orchestration.pipeline_pb2 import PipelineNode
 
 from zenml.logger import get_logger
-from zenml.repository import Repository
 from zenml.steps import BaseStep
-from zenml.steps.utils import (
-    INTERNAL_EXECUTION_PARAMETER_PREFIX,
-    PARAM_PIPELINE_PARAMETER_NAME,
-)
 
 if TYPE_CHECKING:
     from zenml.pipelines.base_pipeline import BasePipeline
@@ -44,66 +37,16 @@ def create_tfx_pipeline(
     tfx_components = [step.component for step in zenml_pipeline.steps.values()]
 
     artifact_store = stack.artifact_store
-    metadata_store = stack.metadata_store
 
+    # We do not pass the metadata connection config here as it might not be
+    # accessible. Instead it is queried from the active stack right before a
+    # step is executed (see `BaseOrchestrator.run_step(...)`)
     return tfx_pipeline.Pipeline(
         pipeline_name=zenml_pipeline.name,
         components=tfx_components,  # type: ignore[arg-type]
         pipeline_root=artifact_store.path,
-        metadata_connection_config=metadata_store.get_tfx_metadata_config(),
         enable_cache=zenml_pipeline.enable_cache,
     )
-
-
-def get_cache_status(
-    execution_info: data_types.ExecutionInfo,
-) -> bool:
-    """Returns the caching status of a step.
-
-    Args:
-        execution_info: The execution info of a `tfx` step.
-
-    Raises:
-        AttributeError: If the execution info is `None`.
-        KeyError: If no pipeline info is found in the `execution_info`.
-
-    Returns:
-        The caching status of a `tfx` step as a boolean value.
-    """
-    if execution_info is None:
-        logger.warning("No execution info found when checking cache status.")
-        return False
-
-    status = False
-    repository = Repository()
-    # TODO [ENG-706]: Get the current running stack instead of just the active
-    #   stack
-    active_stack = repository.active_stack
-    if not active_stack:
-        raise RuntimeError(
-            "No active stack is configured for the repository. Run "
-            "`zenml stack set STACK_NAME` to update the active stack."
-        )
-
-    metadata_store = active_stack.metadata_store
-
-    step_name_param = (
-        INTERNAL_EXECUTION_PARAMETER_PREFIX + PARAM_PIPELINE_PARAMETER_NAME
-    )
-    step_name = json.loads(execution_info.exec_properties[step_name_param])
-    if execution_info.pipeline_info:
-        pipeline_name = execution_info.pipeline_info.id
-    else:
-        raise KeyError(f"No pipeline info found for step `{step_name}`.")
-    pipeline_run_name = cast(str, execution_info.pipeline_run_id)
-    pipeline = metadata_store.get_pipeline(pipeline_name)
-    if pipeline is None:
-        logger.error(f"Pipeline {pipeline_name} not found in Metadata Store.")
-    else:
-        status = (
-            pipeline.get_run(pipeline_run_name).get_step(step_name).is_cached
-        )
-    return status
 
 
 def get_step_for_node(node: PipelineNode, steps: List[BaseStep]) -> BaseStep:
