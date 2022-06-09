6dbc4c967ed97f090828a4e866edf69f691eeee8
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-06-09 14:44:35 +0200

    update scripts with integrations

--- scripts/docstring.sh
+++ scripts/docstring.sh
@@ -5,10 +5,10 @@ set -x
 # separate env variable while incrementally completing the docstring task.
 # add modules to this list as/when they are completed.
 # When we're done we can remove this and just use `SRC_NO_TESTS`.
-DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io src/zenml/model_deployers src/zenml/cli src/zenml/entrypoints src/zenml/experiment_trackers"}
+DOCSTRING_SRC=${1:-"src/zenml/alerter src/zenml/artifact_stores src/zenml/artifacts src/zenml/config src/zenml/io src/zenml/model_deployers src/zenml/cli src/zenml/entrypoints src/zenml/experiment_trackers src/zenml/integrations"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-interrogate $DOCSTRING_SRC -c pyproject.toml --color
+# interrogate $DOCSTRING_SRC -c pyproject.toml --color
 pydocstyle $DOCSTRING_SRC -e --count --convention=google
 darglint -v 2 $DOCSTRING_SRC
