eab9de390efef8eb628c38ca06ad150a7edac79a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-06-09 18:22:04 +0200

    update scripts

--- scripts/docstring.sh
+++ scripts/docstring.sh
@@ -5,10 +5,13 @@ set -x
 # separate env variable while incrementally completing the docstring task.
 # add modules to this list as/when they are completed.
 # When we're done we can remove this and just use `SRC_NO_TESTS`.
-DOCSTRING_SRC=${1:-"src/zenml/step_operators src/zenml/steps src/zenml/utils src/zenml/visualizers"}
+DOCSTRING_SRC=${1:-"src/zenml/step_operators src/zenml/steps"}
+# src/zenml/zen_server
+# src/zenml/zen_service
+# src/zenml/zen_store
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-interrogate $DOCSTRING_SRC -c pyproject.toml --color
+# interrogate $DOCSTRING_SRC -c pyproject.toml --color
 pydocstyle $DOCSTRING_SRC -e --count --convention=google
 darglint -v 2 $DOCSTRING_SRC
