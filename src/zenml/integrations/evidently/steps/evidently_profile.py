ef81669a7307732d5a7074f3a69cf98d6491823a
Sangam Swadi K <sangamswadik@users.noreply.github.com>
2022-06-13 22:34:39 +0530

    fix spacing

--- src/zenml/integrations/evidently/steps/evidently_profile.py
+++ src/zenml/integrations/evidently/steps/evidently_profile.py
@@ -147,8 +147,8 @@ class EvidentlyProfileStep(BaseDriftDetectionStep):
               generated for the data drift
         """
         if ignored_columns is not None:
-            reference_dataset = reference_dataset.drop(labels=ignored_columns, axis =1)
-            comparison_dataset = comparison_dataset.drop(labels=ignored_columns, axis =1)
+            reference_dataset = reference_dataset.drop(labels=ignored_columns, axis=1)
+            comparison_dataset = comparison_dataset.drop(labels=ignored_columns, axis=1)
         sections, tabs = config.get_profile_sections_and_tabs()
         data_drift_dashboard = Dashboard(tabs=tabs)
         data_drift_dashboard.calculate(
