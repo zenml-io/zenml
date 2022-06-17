8ed1088ace4fe7f80723a7133541442fc6f1681c
Alexej Penner <thealexejpenner@gmail.com>
2022-06-17 16:55:45 +0200

    Initial refactor huggingface

---
+++ examples/huggingface/steps/evaluation/token_evaluation_step.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from datasets import DatasetDict
+from transformers import (
+    TFPreTrainedModel,
+    PreTrainedTokenizerBase,
+    DataCollatorForTokenClassification
+)
+
+from zenml.steps import step
+
+from ..configuration import HuggingfaceConfig
+
+
+@step
+def token_evaluator(
+        config: HuggingfaceConfig,
+        model: TFPreTrainedModel,
+        tokenized_datasets: DatasetDict,
+        tokenizer: PreTrainedTokenizerBase,
+) -> float:
+    """Evaluate trained model on validation set"""
+    # Needs to recompile because we are reloading model for evaluation
+    model.compile(optimizer=tf.keras.optimizers.Adam())
+
+    # Convert into tf dataset format
+    validation_set = tokenized_datasets["validation"].to_tf_dataset(
+        columns=["attention_mask", "input_ids", "labels"],
+        shuffle=False,
+        batch_size=config.batch_size,
+        collate_fn=DataCollatorForTokenClassification(
+            tokenizer, return_tensors="tf"
+        ),
+    )
+
+    # Calculate loss
+    if config.dummy_run:
+        test_loss = model.evaluate(validation_set.take(10), verbose=1)
+    else:
+        test_loss = model.evaluate(validation_set, verbose=1)
+    return test_loss
