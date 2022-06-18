fa82dbcc3e7993267d1fb890b7a4c3c3a21aae7c
Felix Altenberger <felix@zenml.io>
2022-06-18 11:04:19 +0200

    Add schedule instructions to example run.py.

--- examples/kubernetes/run.py
+++ examples/kubernetes/run.py
@@ -8,3 +8,16 @@ if __name__ == "__main__":
         evaluator=evaluator(),
         skew_comparison=skew_comparison(),
     ).run()
+
+    # In case you want to run this on a schedule, run it in the following way:
+    #
+    # from zenml.pipelines import Schedule
+    #
+    # schedule = Schedule(cron_expression="*/5 * * * *")  # every 5 minutes
+    #
+    # parallelizable_pipeline(
+    #     importer=importer(),
+    #     trainer=svc_trainer(),
+    #     evaluator=evaluator(),
+    #     skew_comparison=skew_comparison(),
+    # ).run(schedule=schedule)
