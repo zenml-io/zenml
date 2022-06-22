dd298e6a8bcfbed2737d5529c47015f520368060
Safoine El Khabich <safoine.ext@zenml.io>
2022-06-22 09:42:44 +0100

    fix typos errors in mnist_handler

--- examples/kserve_deployment/pytorch/mnist_handler.py
+++ examples/kserve_deployment/pytorch/mnist_handler.py
@@ -6,7 +6,7 @@ class MNISTDigitClassifier(ImageClassifier):
     """
     MNISTDigitClassifier handler class. This handler extends class ImageClassifier from image_classifier.py, a
     default handler. This handler takes an image and returns the number in that image.
-    Here method post-process has been overridden while others are reused from parent class.
+    Here the post-processing method has been overridden while others are reused from parent class.
     """
 
     image_processing = transforms.Compose(
@@ -17,11 +17,12 @@ class MNISTDigitClassifier(ImageClassifier):
         super(MNISTDigitClassifier, self).__init__()
 
     def postprocess(self, data):
-        """The post process of MNIST converts the predicted output response to a label.
+        """The post-processing of MNIST converts the predicted output response to a label.
         Args:
-            data (list): The predicted output from the Inference with probabilities is passed
+            data (list): The predicted output from the inference with probabilities is passed
             to the post-process function
+            
         Returns:
-            list : A list of dictionaries with predictions and explanations is returned
+            list: A list of dictionaries with predictions and explanations is returned
         """
         return data.argmax(1).tolist()
