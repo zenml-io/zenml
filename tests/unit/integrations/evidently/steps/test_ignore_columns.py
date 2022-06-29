cda1ec67a1edc0bf1fef8f1905a311831c00f1e9
Sangam Swadi K <sangamswadik@users.noreply.github.com>
2022-06-29 21:08:13 +0530

    update tests for features

---
+++ tests/unit/integrations/evidently/steps/test_ignore_columns.py
@@ -0,0 +1,106 @@
+#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
+import pandas as pd
+from zenml.integrations.evidently.steps.evidently_profile import (
+    EvidentlyProfileConfig,
+)
+from zenml.integrations.evidently.steps.evidently_profile import (
+    EvidentlyProfileStep,
+)
+from evidently.pipeline.column_mapping import ColumnMapping
+
+
+ref = pd.DataFrame()
+ref["target"] = ["A", "B", "B", "C", "A"]
+ref["prediction"] = [0.12, -0.54, 0.08, 1.78, 0.03]
+ref["ignore_col"] = [1, 5, 1, 89, 0]
+ref["var_A"] = [1, 5, 3, 10, 25]
+ref["var_B"] = [0.58, 0.23, 1.25, -0.14, 0]
+
+comp = pd.DataFrame()
+comp["target"] = ["B", "C", "A", "A", "C"]
+comp["prediction"] = [0.12, -0.54, 0.08, 1.78, 0.03]
+comp["ignore_col"] = [3, 1, 1, 120, 1]
+comp["var_A"] = [0, 27, 3, 4, 8]
+comp["var_B"] = [0.12, -0.54, 0.08, 1.78, 0.03]
+
+
+def test_ignore_features():
+    """Tests ignore cols parameter with features ignore_col and var_C
+    to be ignored"""
+
+    col_list = ["target"]
+
+    clmn_map = ColumnMapping(*col_list)
+
+    profile_config = EvidentlyProfileConfig(
+        profile_sections=["datadrift"], column_mapping=clmn_map
+    )
+
+    profile_step = EvidentlyProfileStep()
+
+    drift_obj, dash_obj = profile_step.entrypoint(
+        reference_dataset=ref,
+        comparison_dataset=comp,
+        config=profile_config,
+        ignored_columns=["ignore_col", "var_B"],
+    )
+    target = [drift_obj["data_drift"]["data"]["utility_columns"]["target"]]
+    prediction = [
+        drift_obj["data_drift"]["data"]["utility_columns"]["prediction"]
+    ]
+    cat_feat_names = drift_obj["data_drift"]["data"]["cat_feature_names"]
+    num_feat_names = drift_obj["data_drift"]["data"]["num_feature_names"]
+
+    assert (
+        "ignore_col"
+        not in target + prediction + cat_feat_names + num_feat_names
+    )
+    assert "var_B" not in target + prediction + cat_feat_names + num_feat_names
+
+
+def test_default_ignore_cols():
+    """Tests ignore cols parameter with nothing to ignore
+    i.e pass all features"""
+
+    col_list = ["target"]
+
+    clmn_map = ColumnMapping(*col_list)
+
+    profile_config = EvidentlyProfileConfig(
+        profile_sections=["datadrift"], column_mapping=clmn_map
+    )
+
+    profile_step = EvidentlyProfileStep()
+
+    drift_obj, dash_obj = profile_step.entrypoint(
+        reference_dataset=ref,
+        comparison_dataset=comp,
+        config=profile_config,
+        ignored_columns=None,
+    )
+    target = [drift_obj["data_drift"]["data"]["utility_columns"]["target"]]
+    prediction = [
+        drift_obj["data_drift"]["data"]["utility_columns"]["prediction"]
+    ]
+    cat_feat_names = drift_obj["data_drift"]["data"]["cat_feature_names"]
+    num_feat_names = drift_obj["data_drift"]["data"]["num_feature_names"]
+
+    all_cols = target + prediction + cat_feat_names + num_feat_names
+    assert "target" in all_cols
+    assert "prediction" in all_cols
+    assert "ignore_col" in all_cols
+    assert "var_A" in all_cols
+    assert "var_B" in all_cols
