1ef415ccae2d36b9f3fa0fa239fc3dbb0ecb1a78
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-08-15 16:35:43 +0200

    add validation checks test

--- tests/unit/integrations/deepchecks/test_validation_checks.py
+++ tests/unit/integrations/deepchecks/test_validation_checks.py
@@ -11,3 +11,17 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+import pytest
+
+from zenml.integrations.deepchecks.validation_checks import (
+    DeepchecksValidationCheck,
+)
+
+
+def test_validation_check_fails_when_checking_name():
+    """Ensures that the validation check fails when names not using our format."""
+    deepchecks_validation_check = DeepchecksValidationCheck(
+        "aria", "the_wrong_enum_value"
+    )
+    with pytest.raises(ValueError):
+        deepchecks_validation_check.validate_check_name("abc")
