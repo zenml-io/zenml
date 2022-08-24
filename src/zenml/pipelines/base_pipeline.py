242dbba6d7997226b13b43ea653cafe1341b80d4
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-08-24 12:10:32 +0200

    Add docs and test

--- src/zenml/pipelines/base_pipeline.py
+++ src/zenml/pipelines/base_pipeline.py
@@ -357,8 +357,8 @@ class BasePipeline(metaclass=BasePipelineMeta):
                     f"{available_step_operators}."
                 )
 
-    def _reset_step_flags(self) -> None:
-        """Reset the `_has_been_called` flag at the beginning of a pipeline run.
+    def _reset_steps(self) -> None:
+        """Reset step values from previous pipeline runs.
 
         This ensures a pipeline instance can be called more than once.
         """
@@ -438,7 +438,7 @@ class BasePipeline(metaclass=BasePipelineMeta):
             },
         )
 
-        self._reset_step_flags()
+        self._reset_steps()
         self.validate_stack(stack)
 
         # Prevent execution of nested pipelines which might lead to unexpected
