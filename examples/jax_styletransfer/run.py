ac8bfdbdd629e8ff9c45a6cdb6237ac571513882
Nicholas Junge <nicholas.junge@web.de>
2022-09-07 21:07:23 +0200

    Add pipeline and run it in the script main entrypoint

--- examples/jax_styletransfer/run.py
+++ examples/jax_styletransfer/run.py
@@ -10,6 +10,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+from pipelines import style_transfer_pipeline
 import copy
 from functools import partial
 
@@ -180,4 +181,8 @@ def trainer(
 
 
 if __name__ == '__main__':
-    pass
+    pipeline = style_transfer_pipeline(
+        image_loader=image_loader(),
+        trainer=trainer(),
+    )
+    run = pipeline.run()
