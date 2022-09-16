e15a90559f20dabe69e45938922ddde0bce2a19f
Hamza Tahir <htahir111@gmail.com>
2022-09-16 15:48:43 +0200

    Added dashboard build files

---
+++ dashboard/build/precache-manifest.6dea53a3f0e21016e657fbe39aca6e7b.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcec30f060dfe7a101b84e923c28e28",
+    "url": "./dashboard/index.html"
+  },
+  {
+    "revision": "ed511f69f26ba300169b",
+    "url": "./dashboard/static/css/2.9fa09cb7.chunk.css"
+  },
+  {
+    "revision": "146383fa47529778b466",
+    "url": "./dashboard/static/css/main.6f99e246.chunk.css"
+  },
+  {
+    "revision": "ed511f69f26ba300169b",
+    "url": "./dashboard/static/js/2.6125c98d.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "./dashboard/static/js/2.6125c98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146383fa47529778b466",
+    "url": "./dashboard/static/js/main.27bdfe74.chunk.js"
+  },
+  {
+    "revision": "c58d1d53da54426d4565",
+    "url": "./dashboard/static/js/runtime-main.0c6cbbd2.js"
+  },
+  {
+    "revision": "28aee53583b4d0612cbefd79f7b0e3ce",
+    "url": "./dashboard/static/media/AlertTriangle.28aee535.svg"
+  },
+  {
+    "revision": "5cb101ffbf7914a47204581ceb2779e8",
+    "url": "./dashboard/static/media/BookOpen.5cb101ff.svg"
+  },
+  {
+    "revision": "9b1c67d7b25b38fb8ab8ea28dc346b8b",
+    "url": "./dashboard/static/media/Burger.9b1c67d7.svg"
+  },
+  {
+    "revision": "356e11c7e3b1d9c300d219daaecc9e49",
+    "url": "./dashboard/static/media/Calendar.356e11c7.svg"
+  },
+  {
+    "revision": "dad6beb212bfe7b4a5f91cc647a997ac",
+    "url": "./dashboard/static/media/Check.dad6beb2.svg"
+  },
+  {
+    "revision": "afaaec5728433a80f6cdeb58ebf54cc7",
+    "url": "./dashboard/static/media/ChevronDown.afaaec57.svg"
+  },
+  {
+    "revision": "6642d75654f8b88986bf62fc7209fcaf",
+    "url": "./dashboard/static/media/ChevronDownLight.6642d756.svg"
+  },
+  {
+    "revision": "f6edfcdb50b2b8e9402255fae2a1fc0a",
+    "url": "./dashboard/static/media/ChevronLeft.f6edfcdb.svg"
+  },
+  {
+    "revision": "ffc9de9505dd6dfdb6fdaa46b1006cfe",
+    "url": "./dashboard/static/media/Clock.ffc9de95.svg"
+  },
+  {
+    "revision": "4f6b4fd937c7fbb3e5be6b2c6160bdac",
+    "url": "./dashboard/static/media/Close.4f6b4fd9.svg"
+  },
+  {
+    "revision": "6960930a13b093507112430a8d9b7f05",
+    "url": "./dashboard/static/media/CloseWithBorder.6960930a.svg"
+  },
+  {
+    "revision": "ef0f33b590111252ea69366c6f5307ea",
+    "url": "./dashboard/static/media/Code.ef0f33b5.svg"
+  },
+  {
+    "revision": "36e2112aa7bd47bc0454da1cd2b5a5cd",
+    "url": "./dashboard/static/media/Copy.36e2112a.svg"
+  },
+  {
+    "revision": "d05787e06ba1b179e3ed2611fd03c602",
+    "url": "./dashboard/static/media/Dashboard.d05787e0.svg"
+  },
+  {
+    "revision": "b1c3b5f8f4c60418e6231d7c7cf183c2",
+    "url": "./dashboard/static/media/Data.b1c3b5f8.svg"
+  },
+  {
+    "revision": "fba04d87232939a39b3b66ad495adfe8",
+    "url": "./dashboard/static/media/Download.fba04d87.svg"
+  },
+  {
+    "revision": "d9e4ee62e318c234ccd9c3944109a57e",
+    "url": "./dashboard/static/media/Eye.d9e4ee62.svg"
+  },
+  {
+    "revision": "1f15bacde46b52a984cb07c12e9c1c53",
+    "url": "./dashboard/static/media/FileText.1f15bacd.svg"
+  },
+  {
+    "revision": "ab6b9c0d909528180c6074a80511c0b1",
+    "url": "./dashboard/static/media/Filter.ab6b9c0d.svg"
+  },
+  {
+    "revision": "083292402dd3840ac4293d06ac9f0c52",
+    "url": "./dashboard/static/media/History.08329240.svg"
+  },
+  {
+    "revision": "0843b0d5574caf852fb8726689ef6cd0",
+    "url": "./dashboard/static/media/Home.0843b0d5.svg"
+  },
+  {
+    "revision": "5aa1c16b4af14a2f3fa5a74981a250b8",
+    "url": "./dashboard/static/media/Plus.5aa1c16b.svg"
+  },
+  {
+    "revision": "63bf7b9d5de8cc3fcb750862f9b78233",
+    "url": "./dashboard/static/media/Rocket.63bf7b9d.svg"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "./dashboard/static/media/Rubik-Medium.c87313aa.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "./dashboard/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "88aeb6d4368efa5863d2a3f24e095220",
+    "url": "./dashboard/static/media/Settings.88aeb6d4.svg"
+  },
+  {
+    "revision": "6aa718c523a2dd1f65829efa663372b4",
+    "url": "./dashboard/static/media/SignOut.6aa718c5.svg"
+  },
+  {
+    "revision": "b484b32fcec981a533e3b9694953103b",
+    "url": "./dashboard/static/media/SourceCodePro-Regular.b484b32f.ttf"
+  },
+  {
+    "revision": "543e3039df6881223887b87a8cbe0c44",
+    "url": "./dashboard/static/media/Stream.543e3039.svg"
+  },
+  {
+    "revision": "f9cececc3b93be229db62463bf032f00",
+    "url": "./dashboard/static/media/Table.f9cececc.svg"
+  },
+  {
+    "revision": "d57854868ee3cb520d216aad300e7d11",
+    "url": "./dashboard/static/media/Tool.d5785486.svg"
+  },
+  {
+    "revision": "741a99d7d7ad83088ee29a54fb54aa34",
+    "url": "./dashboard/static/media/UserPlus.741a99d7.svg"
+  },
+  {
+    "revision": "b038245f5f00f6caf10a1ec582b08321",
+    "url": "./dashboard/static/media/home-image.b038245f.png"
+  },
+  {
+    "revision": "104fd14ba5d505dc371a568f3a1eb45b",
+    "url": "./dashboard/static/media/image.104fd14b.png"
+  },
+  {
+    "revision": "93333e5c50f1a1e41803515385b8df58",
+    "url": "./dashboard/static/media/logo.93333e5c.svg"
+  }
+]);
