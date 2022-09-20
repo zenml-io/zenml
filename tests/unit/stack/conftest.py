d9d4ed8e73cb20f6bdaeda2b1e214aad3592f439
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-09-20 18:36:13 +0200

    WIP updated stub_component

--- tests/unit/stack/conftest.py
+++ tests/unit/stack/conftest.py
@@ -19,6 +19,7 @@ from zenml.artifact_stores import BaseArtifactStore
 from zenml.enums import StackComponentType
 from zenml.orchestrators import BaseOrchestrator
 from zenml.stack import Stack, StackComponent, StackValidator
+from zenml.stack.stack_component import StackComponentConfig
 
 MOCK_FLAVOR = "mock_flavor"
 
@@ -56,12 +57,15 @@ def failing_stack_validator():
 @pytest.fixture
 def stub_component():
     class _StubComponent(StackComponent):
-        name = "StubComponent"
         some_public_attribute_name = "Aria"
         _some_private_attribute_name = "Also Aria"
 
-        # Class Configuration
-        TYPE = StackComponentType.ORCHESTRATOR
-        FLAVOR = MOCK_FLAVOR
-
-    return _StubComponent()
+    return _StubComponent(
+        name="StubComponent",
+        id=uuid4(),
+        config=StackComponentConfig(),
+        flavor=MOCK_FLAVOR,
+        type=StackComponentType.ORCHESTRATOR,
+        user=uuid4(),
+        project=uuid4(),
+    )
