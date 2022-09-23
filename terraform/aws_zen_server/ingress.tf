65757a632e7dfebcb9e74459cb50425c2ef997e3
Jayesh Sharma <wjayesh@outlook.com>
2022-09-23 12:29:32 +0530

    add helm chart to recipe

--- terraform/aws_zen_server/ingress.tf
+++ terraform/aws_zen_server/ingress.tf
@@ -13,33 +13,3 @@ resource "helm_release" "nginx-controller" {
   # dependency on nginx-ns
   namespace = kubernetes_namespace.nginx-ns.metadata[0].name
 }
-
-resource "kubernetes_ingress_v1" "mlflow-ingress" {
-  metadata {
-    name = "mlflow-ingress"
-    annotations = {
-      "nginx.ingress.kubernetes.io/auth-type"   = "basic"
-      "nginx.ingress.kubernetes.io/auth-secret" = "basic-auth"
-      "nginx.ingress.kubernetes.io/auth-realm"  = "Authentication Required - mlflow"
-    }
-  }
-  spec {
-    ingress_class_name = "nginx"
-    rule {
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = "mlflow-tracking"
-              port {
-                number = 5000
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
--- terraform/aws_zen_server/zen_server.tf
+++ terraform/aws_zen_server/zen_server.tf
@@ -1,66 +1,34 @@
 # create the ZenServer deployment
 resource "helm_release" "zen-server" {
 
-  name       = "mlflow-tracking"
-  repository = "https://community-charts.github.io/helm-charts"
-  chart      = "mlflow"
+  name       = "zenml-server"
+  repository = "../../helm"
+  chart      = "zenml"
 
   # set workload identity annotations for the mlflow 
   # kubernetes service account
   set {
-    name  = "serviceAccount.annotations.iam\\.gke\\.io/gcp-service-account"
-    value = var.kubernetes_sa
+    name  = "zenml.database.url"
+    value = local.rds.create_rds? module.metadata_store.db_instance_address : local.rds.hostname
   }
 
   # set proxied access to artifact storage
   set {
-    name  = "artifactRoot.proxiedArtifactStorage"
-    value = var.artifact_Proxied_Access
+    name  = "zenml.database.sslCa"
+    value = local.rds.create_rds? "./ca.pem" : local.rds.server_ca
   }
 
   # set values for S3 artifact store
   set {
-    name  = "artifactRoot.s3.enabled"
-    value = var.artifact_S3
+    name  = "zenml.database.sslCert"
+    value = local.rds.create_rds? "./cert.pem" : local.rds.client_cert
   }
   set {
-    name  = "artifactRoot.s3.bucket"
-    value = var.artifact_S3_Bucket
+    name  = "zenml.database.sslKey"
+    value = local.rds.create_rds? "./key.pem" : local.rds.client_key
   }
   set {
-    name  = "artifactRoot.s3.awsAccessKeyId"
-    value = var.artifact_S3_Access_Key
-  }
-  set {
-    name  = "artifactRoot.s3.awsSecretAccessKey"
-    value = var.artifact_S3_Secret_Key
-  }
-
-  # set values for Azure Blob Storage
-  set {
-    name  = "artifactRoot.azureBlob.enabled"
-    value = var.artifact_Azure
-  }
-  set {
-    name  = "artifactRoot.azureBlob.storageAccount"
-    value = var.artifact_Azure_Storage_Account_Name
-  }
-  set {
-    name  = "artifactRoot.azureBlob.container"
-    value = var.artifact_Azure_Container
-  }
-  set {
-    name  = "artifactRoot.azureBlob.accessKey"
-    value = var.artifact_Azure_Access_Key
-  }
-
-  # set values for GCS artifact store
-  set {
-    name  = "artifactRoot.gcs.enabled"
-    value = var.artifact_GCS
-  }
-  set {
-    name  = "artifactRoot.gcs.bucket"
-    value = var.artifact_GCS_Bucket
+    name  = "zenml.database.sslVerifyServerCert"
+    value = true
   }
 }
