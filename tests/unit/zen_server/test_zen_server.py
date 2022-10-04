5bb6e77e78a202221d159015746a397db5bd06ec
Stefan Nica <stefan@zenml.io>
2022-10-04 22:28:14 +0200

    Fetch the zenml server logs contents on failure during unit tests

--- tests/unit/zen_server/test_zen_server.py
+++ tests/unit/zen_server/test_zen_server.py
@@ -72,7 +72,13 @@ def running_zen_server(
         ),
     )
 
-    zen_server.start(timeout=SERVER_START_STOP_TIMEOUT)
+    try:
+        zen_server.start(timeout=SERVER_START_STOP_TIMEOUT)
+    except RuntimeError:
+        print("ZenServer failed to start. Pulling logs...")
+        for line in zen_server.get_logs():
+            print(line)
+        raise
 
     yield zen_server
     zen_server.stop(timeout=SERVER_START_STOP_TIMEOUT)
