82f8dd24c61ba052334616b6b15d82e554290775
Hamza Tahir <hamza@zenml.io>
2022-10-04 11:00:44 +0200

    Removed all referenced to ZenServer in logs and comments (#915)

--- src/zenml/zen_server/deploy/docker/__init__.py
+++ src/zenml/zen_server/deploy/docker/__init__.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""ZenServer Docker Deployment."""
+"""ZenML Server Docker Deployment."""
 
 
 from zenml.zen_server.deploy.docker.docker_provider import DockerServerProvider
