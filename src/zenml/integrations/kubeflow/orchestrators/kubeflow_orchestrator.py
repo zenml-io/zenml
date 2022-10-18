d289f97d9e1a491c3db7f95bd380f7535b451379
Jayesh Sharma <wjayesh@outlook.com>
2022-10-18 20:11:51 +0530

    make local check cleaner

--- src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
+++ src/zenml/integrations/kubeflow/orchestrators/kubeflow_orchestrator.py
@@ -47,7 +47,7 @@ from zenml.client import Client
 from zenml.config.base_settings import BaseSettings
 from zenml.config.global_config import GlobalConfiguration
 from zenml.constants import ORCHESTRATOR_DOCKER_IMAGE_KEY
-from zenml.enums import ContainerRegistryFlavor, StackComponentType
+from zenml.enums import StackComponentType
 from zenml.environment import Environment
 from zenml.exceptions import ProvisioningError
 from zenml.integrations.kubeflow.flavors.kubeflow_orchestrator_flavor import (
@@ -546,11 +546,7 @@ class KubeflowOrchestrator(BaseOrchestrator):
             )
 
         image_name = deployment.pipeline.extra[ORCHESTRATOR_DOCKER_IMAGE_KEY]
-        if (
-            self.is_local
-            and stack.container_registry.flavor
-            == ContainerRegistryFlavor.DEFAULT.value
-        ):
+        if self.is_local and stack.container_registry.config.is_local:
             image_name = f"k3d-zenml-kubeflow-registry.{image_name}"
         is_scheduled_run = bool(deployment.schedule)
 
