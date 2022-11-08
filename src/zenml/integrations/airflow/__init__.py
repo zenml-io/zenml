184ff8a3c22f25b3f8e49007c825d74d3bc8716b
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-11-08 14:48:56 +0100

    Bump airflow version

--- src/zenml/integrations/airflow/__init__.py
+++ src/zenml/integrations/airflow/__init__.py
@@ -30,7 +30,7 @@ class AirflowIntegration(Integration):
     """Definition of Airflow Integration for ZenML."""
 
     NAME = AIRFLOW
-    REQUIREMENTS = ["apache-airflow==2.2.0"]
+    REQUIREMENTS = ["apache-airflow==2.4.2"]
 
     @classmethod
     def flavors(cls) -> List[Type[Flavor]]:
