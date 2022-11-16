7c983600edeb889a0453abbbd4ef6edcd1ee51d8
Safoine El Khabich <safoine.ext@zenml.io>
2022-11-16 09:46:09 +0100

    remove unused varibale from bentoml_deployment step

--- src/zenml/integrations/bentoml/steps/bentoml_deployer.py
+++ src/zenml/integrations/bentoml/steps/bentoml_deployer.py
@@ -17,7 +17,6 @@ from typing import List, Optional, cast
 import bentoml
 from bentoml._internal.bento import bento
 
-from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
 from zenml.environment import Environment
 from zenml.integrations.bentoml.model_deployers.bentoml_model_deployer import (
     BentoMLModelDeployer,
