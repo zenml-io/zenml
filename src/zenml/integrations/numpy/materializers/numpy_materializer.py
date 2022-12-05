eaef4d47d736bba3d32ca5cdae6a23719e5c4565
Hamza Tahir <htahir111@gmail.com>
2022-12-05 18:05:14 +0100

    added new integrations for pandas and numpy

---
+++ src/zenml/integrations/numpy/materializers/numpy_materializer.py
@@ -0,0 +1,109 @@
+#  Copyright (c) ZenML GmbH 2021. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+"""Implementation of the ZenML NumPy materializer."""
+
+import os
+from typing import TYPE_CHECKING, Any, Type
+
+import numpy as np
+
+from zenml.artifacts import DataArtifact
+from zenml.io import fileio
+from zenml.logger import get_logger
+from zenml.materializers.base_materializer import BaseMaterializer
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+logger = get_logger(__name__)
+
+
+NUMPY_FILENAME = "data.npy"
+
+DATA_FILENAME = "data.parquet"
+SHAPE_FILENAME = "shape.json"
+DATA_VAR = "data_var"
+
+
+class NumpyMaterializer(BaseMaterializer):
+    """Materializer to read data to and from numpy."""
+
+    ASSOCIATED_TYPES = (np.ndarray,)
+    ASSOCIATED_ARTIFACT_TYPES = (DataArtifact,)
+
+    def handle_input(self, data_type: Type[Any]) -> "Any":
+        """Reads a numpy array from a `.npy` file.
+
+        Args:
+            data_type: The type of the data to read.
+
+
+        Raises:
+            ImportError: If pyarrow is not installed.
+
+        Returns:
+            The numpy array.
+        """
+        super().handle_input(data_type)
+
+        numpy_file = os.path.join(self.artifact.uri, NUMPY_FILENAME)
+
+        if fileio.exists(numpy_file):
+            with fileio.open(numpy_file, "rb") as f:
+                return np.load(f, allow_pickle=True)  # type: ignore
+        elif fileio.exists(os.path.join(self.artifact.uri, DATA_FILENAME)):
+            logger.warning(
+                "A legacy artifact was found. "
+                "This artifact was created with an older version of "
+                "ZenML. You can still use it, but it will be "
+                "converted to the new format on the next materialization."
+            )
+            try:
+                # Import old materializer dependencies
+                import pyarrow as pa  # type: ignore
+                import pyarrow.parquet as pq  # type: ignore
+
+                from zenml.utils import yaml_utils
+
+                # Read numpy array from parquet file
+                shape_dict = yaml_utils.read_json(
+                    os.path.join(self.artifact.uri, SHAPE_FILENAME)
+                )
+                shape_tuple = tuple(shape_dict.values())
+                with fileio.open(
+                    os.path.join(self.artifact.uri, DATA_FILENAME), "rb"
+                ) as f:
+                    input_stream = pa.input_stream(f)
+                    data = pq.read_table(input_stream)
+                vals = getattr(data.to_pandas(), DATA_VAR).values
+                return np.reshape(vals, shape_tuple)
+            except ImportError:
+                raise ImportError(
+                    "You have an old version of a `NumpyMaterializer` ",
+                    "data artifact stored in the artifact store ",
+                    "as a `.parquet` file, which requires `pyarrow` for reading. ",
+                    "You can install `pyarrow` by running `pip install pyarrow`.",
+                )
+
+    def handle_return(self, arr: "NDArray[Any]") -> None:
+        """Writes a np.ndarray to the artifact store as a `.npy` file.
+
+        Args:
+            arr: The numpy array to write.
+        """
+        super().handle_return(arr)
+        with fileio.open(
+            os.path.join(self.artifact.uri, NUMPY_FILENAME), "wb"
+        ) as f:
+            np.save(f, arr)  # type: ignore
---
+++ src/zenml/integrations/pandas/materializers/pandas_materializer.py
@@ -0,0 +1,137 @@
+#  Copyright (c) ZenML GmbH 2021. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+"""Materializer for Pandas."""
+
+import os
+from typing import Any, Type, Union
+
+import pandas as pd
+
+from zenml.artifacts import DataArtifact, SchemaArtifact, StatisticsArtifact
+from zenml.artifacts.base_artifact import BaseArtifact
+from zenml.io import fileio
+from zenml.logger import get_logger
+from zenml.materializers.base_materializer import BaseMaterializer
+
+logger = get_logger(__name__)
+
+PARQUET_FILENAME = "df.parquet.gzip"
+COMPRESSION_TYPE = "gzip"
+
+CSV_FILENAME = "df.csv"
+
+
+class PandasMaterializer(BaseMaterializer):
+    """Materializer to read data to and from pandas."""
+
+    ASSOCIATED_TYPES = (pd.DataFrame, pd.Series)
+    ASSOCIATED_ARTIFACT_TYPES = (
+        DataArtifact,
+        StatisticsArtifact,
+        SchemaArtifact,
+    )
+
+    def __init__(self, artifact: "BaseArtifact"):
+        """Define `self.data_path`.
+
+        Args:
+            artifact: Artifact required by `BaseMaterializer.__init__()`.
+        """
+        super().__init__(artifact)
+        try:
+            import pyarrow  # type: ignore
+
+            self.pyarrow_exists = True
+        except ImportError:
+            self.pyarrow_exists = False
+            logger.warning(
+                "By default, the `PandasMaterializer` stores data as a "
+                "`.csv` file. If you want to store data more efficiently, "
+                "you can install `pyarrow` by running "
+                "'`pip install pyarrow`'. This will allow `PandasMaterializer` "
+                "to automatically store the data as a `.parquet` file instead."
+            )
+        finally:
+            self.parquet_path = os.path.join(
+                self.artifact.uri, PARQUET_FILENAME
+            )
+            self.csv_path = os.path.join(self.artifact.uri, CSV_FILENAME)
+
+    def handle_input(
+        self, data_type: Type[Any]
+    ) -> Union[pd.DataFrame, pd.Series]:
+        """Reads `pd.DataFrame` or `pd.Series` from a `.parquet` or `.csv` file.
+
+        Args:
+            data_type: The type of the data to read.
+
+        Raises:
+            ImportError: If pyarrow or fastparquet is not installed.
+
+        Returns:
+            The pandas dataframe or series.
+        """
+        super().handle_input(data_type)
+        if fileio.exists(self.parquet_path):
+            if self.pyarrow_exists:
+                with fileio.open(self.parquet_path, mode="rb") as f:
+                    df = pd.read_parquet(f)
+            else:
+                raise ImportError(
+                    "You have an old version of a `PandasMaterializer` "
+                    "data artifact stored in the artifact store "
+                    "as a `.parquet` file, which requires `pyarrow` "
+                    "for reading, You can install `pyarrow` by running "
+                    "'`pip install pyarrow fastparquet`'."
+                )
+        else:
+            with fileio.open(self.csv_path, mode="rb") as f:
+                df = pd.read_csv(f)
+
+        # validate the type of the data.
+        def is_dataframe_or_series(
+            df: Union[pd.DataFrame, pd.Series]
+        ) -> Union[pd.DataFrame, pd.Series]:
+            """Checks if the data is a `pd.DataFrame` or `pd.Series`.
+
+            Args:
+                df: The data to check.
+
+            Returns:
+                The data if it is a `pd.DataFrame` or `pd.Series`.
+            """
+            if issubclass(data_type, pd.Series):
+                # Taking the first column if its a series as the assumption
+                # is that there will only be one
+                assert len(df.columns) == 1
+                df = df[df.columns[0]]
+                return df
+            else:
+                return df
+
+        return is_dataframe_or_series(df)
+
+    def handle_return(self, df: Union[pd.DataFrame, pd.Series]) -> None:
+        """Writes a pandas dataframe or series to the specified filename.
+
+        Args:
+            df: The pandas dataframe or series to write.
+        """
+        super().handle_return(df)
+        if self.pyarrow_exists:
+            with fileio.open(self.parquet_path, mode="wb") as f:
+                df.to_parquet(f, compression=COMPRESSION_TYPE)
+        else:
+            with fileio.open(self.csv_path, mode="wb") as f:
+                df.to_csv(f, index=False)
