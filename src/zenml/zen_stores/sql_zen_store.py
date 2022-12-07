b9208532005fa53f8507ef1d985d2c437268fb05
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-12-07 16:15:14 +0100

    Update mysqll database creation

--- src/zenml/zen_stores/sql_zen_store.py
+++ src/zenml/zen_stores/sql_zen_store.py
@@ -29,14 +29,15 @@ from typing import (
     Type,
     TypeVar,
     Union,
+    cast,
 )
 from uuid import UUID
 
+import pymysql
 from pydantic import root_validator
 from sqlalchemy import text
-from sqlalchemy.engine import Engine
-from sqlalchemy.engine.url import make_url
-from sqlalchemy.exc import ArgumentError, NoResultFound
+from sqlalchemy.engine import URL, Engine, make_url
+from sqlalchemy.exc import ArgumentError, NoResultFound, OperationalError
 from sqlalchemy.sql.operators import is_
 from sqlmodel import Session, create_engine, or_, select
 from sqlmodel.sql.expression import Select, SelectOfScalar
@@ -147,6 +148,24 @@ Select.inherit_cache = True
 logger = get_logger(__name__)
 
 ZENML_SQLITE_DB_FILENAME = "zenml.db"
+
+
+def _is_mysql_missing_database_error(error: OperationalError) -> bool:
+    """Checks if the given error is due to a missing database.
+
+    Args:
+        error: The error to check.
+
+    Returns:
+        If the error if because the MySQL database doesn't exist.
+    """
+    from pymysql.constants.ER import BAD_DB_ERROR
+
+    if not isinstance(error.orig, pymysql.err.OperationalError):
+        return False
+
+    error_code = cast(int, error.orig.args[0])
+    return error_code == BAD_DB_ERROR
 
 
 class SQLDatabaseDriver(StrEnum):
@@ -572,14 +591,22 @@ class SqlZenStore(BaseZenStore):
             self.config.driver == SQLDatabaseDriver.MYSQL
             and self.config.database
         ):
-            master_url = self._engine.url._replace(database="mysql")
-            master_engine = create_engine(
-                url=master_url, connect_args=connect_args, **engine_args
-            )
-            query = f"CREATE DATABASE IF NOT EXISTS {self.config.database}"
-            conn = master_engine.connect()
-            conn.execute(text(query))
-            conn.close()
+            try:
+                self._engine.connect()
+            except OperationalError as e:
+                logger.debug(
+                    "Failed to connect to mysql database `%s`.",
+                    self._engine.url.database,
+                )
+
+                if _is_mysql_missing_database_error(e):
+                    self._create_mysql_database(
+                        url=self._engine.url,
+                        connect_args=connect_args,
+                        engine_args=engine_args,
+                    )
+                else:
+                    raise
 
         self._alembic = Alembic(self.engine)
         if (
@@ -587,6 +614,31 @@ class SqlZenStore(BaseZenStore):
             and ENV_ZENML_DISABLE_DATABASE_MIGRATION not in os.environ
         ):
             self.migrate_database()
+
+    def _create_mysql_database(
+        self,
+        url: URL,
+        connect_args: Dict[str, Any],
+        engine_args: Dict[str, Any],
+    ) -> None:
+        """Creates a mysql database.
+
+        Args:
+            connect_args: Connect arguments for the SQLAlchemy engine.
+            engine_args: Additional initialization arguments for the SQLAlchemy
+                engine
+        """
+        logger.info("Trying to create database %s.", url.database)
+        master_url = url._replace(database=None)
+        master_engine = create_engine(
+            url=master_url, connect_args=connect_args, **engine_args
+        )
+        query = f"CREATE DATABASE IF NOT EXISTS {self.config.database}"
+        try:
+            connection = master_engine.connect()
+            connection.execute(text(query))
+        finally:
+            connection.close()
 
     def migrate_database(self) -> None:
         """Migrate the database to the head as defined by the python package."""
