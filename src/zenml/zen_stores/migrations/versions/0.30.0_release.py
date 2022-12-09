c793cbc4ce86fe1a707db6a44748f22b47c9094f
Stefan Nica <stefan@zenml.io>
2022-12-09 23:16:06 +0100

    Fix linter issues

--- src/zenml/zen_stores/migrations/versions/0.30.0_release.py
+++ src/zenml/zen_stores/migrations/versions/0.30.0_release.py
@@ -5,9 +5,6 @@ Revises: 7834208cc3f6
 Create Date: 2022-12-09 15:46:03.980591
 
 """
-import sqlmodel
-from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -19,9 +16,7 @@ depends_on = None
 
 def upgrade() -> None:
     """Upgrade database schema and/or data, creating a new revision."""
-    pass
 
 
 def downgrade() -> None:
     """Downgrade database schema and/or data back to the previous revision."""
-    pass
