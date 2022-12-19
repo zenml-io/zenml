d872c93c37470017ae4a970d645d7ed3036a7396
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-12-19 15:42:47 +0100

    update orchestrator WIP

--- src/zenml/integrations/aws/orchestrators/sagemaker_orchestrator.py
+++ src/zenml/integrations/aws/orchestrators/sagemaker_orchestrator.py
@@ -84,6 +84,7 @@ class SagemakerOrchestrator(BaseOrchestrator):
         repo_digest = docker_image_builder.build_and_push_docker_image(
             deployment=deployment, stack=stack
         )
+        # repo_digest = "833794477945.dkr.ecr.eu-north-1.amazonaws.com/zenml:not_so_basic_pipeline"
         deployment.add_extra(ORCHESTRATOR_DOCKER_IMAGE_KEY, repo_digest)
 
     @property
