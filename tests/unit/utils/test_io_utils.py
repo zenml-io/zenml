1771101c091dd084ed9a27abffb0de84161b2a7a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2022-12-22 10:16:30 +0100

    add tests for io_utils

--- tests/unit/utils/test_io_utils.py
+++ tests/unit/utils/test_io_utils.py
@@ -55,3 +55,50 @@ def test_write_file_contents_as_string_fails_with_non_string_types(tmp_path):
     file_path = os.path.join(tmp_path, "test.txt")
     with pytest.raises(ValueError):
         io_utils.write_file_contents_as_string(file_path, 1)
+
+
+def test_read_file_contents_as_string_works(tmp_path):
+    """Tests reading from file."""
+    file_path = os.path.join(tmp_path, "test.txt")
+    io_utils.write_file_contents_as_string(file_path, "aria_best_cat")
+    assert io_utils.read_file_contents_as_string(file_path) == "aria_best_cat"
+
+
+def test_read_file_contents_as_string_raises_error_when_file_not_exists(
+    tmp_path,
+):
+    """Tests reading from file."""
+    file_path = os.path.join(tmp_path, "test.txt")
+    with pytest.raises(FileNotFoundError):
+        io_utils.read_file_contents_as_string(file_path)
+
+
+def find_files_works(tmp_path):
+    """Tests finding files."""
+    file_path = os.path.join(tmp_path, "test.txt")
+    buried_file_path = os.path.join(tmp_path, "test", "buried.txt")
+
+    io_utils.write_file_contents_as_string(file_path, "aria_best_cat")
+    io_utils.write_file_contents_as_string(buried_file_path, "aria_best_cat")
+
+    assert io_utils.find_files(tmp_path, "test.txt") == [file_path]
+    assert io_utils.find_files(tmp_path, "test*") == [file_path]
+    assert io_utils.find_files(tmp_path, "test") == [file_path]
+    assert io_utils.find_files(tmp_path, "buried.txt") == [buried_file_path]
+    assert io_utils.find_files(tmp_path, "buried*") == [buried_file_path]
+    assert io_utils.find_files(tmp_path, "buried") == [buried_file_path]
+    assert io_utils.find_files(tmp_path, "txt") == [
+        file_path,
+        buried_file_path,
+    ]
+    assert io_utils.find_files(tmp_path, "*") == [
+        file_path,
+        buried_file_path,
+    ]
+    assert io_utils.find_files(tmp_path, "") == [
+        file_path,
+        buried_file_path,
+    ]
+    assert len(io_utils.find_files(tmp_path, "*")) == 2
+
+    assert len(io_utils.find_files(tmp_path, "not_a_file.txt")) == 0
