dfcb32be1a1fb4a5c649854b0a29ed57e8a76d43
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-01-02 14:17:14 +0100

    switch out flake8 for ruff

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,7 +8,7 @@ SRC_NO_TESTS=${1:-"src/zenml tests/harness"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-flake8 $SRC
+ruff $SRC
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py --check | ( grep -v "No issues detected" || true )
 isort $SRC scripts --check-only
 black $SRC  --check
