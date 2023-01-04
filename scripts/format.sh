4129da89f51e6545ce00cae390b9ec73f3c3733b
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-01-04 13:16:48 +0100

    ignore docstrings for tests and examples

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,5 +7,5 @@ export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py
 # sorts imports
-ruff $SRC --select I --fix
+ruff $SRC --select I --fix --ignore D
 black $SRC
