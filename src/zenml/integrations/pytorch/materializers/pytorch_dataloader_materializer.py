9367ca0019392d83cff7a91bb8584acdc72ceb99
Felix Altenberger <felix@zenml.io>
2023-01-09 16:30:39 +0100

    Add extract_metadata for PyTorch DataLoader materializer

--- src/zenml/integrations/pytorch/materializers/pytorch_dataloader_materializer.py
+++ src/zenml/integrations/pytorch/materializers/pytorch_dataloader_materializer.py
@@ -14,7 +14,7 @@
 """Implementation of the PyTorch DataLoader materializer."""
 
 import os
-from typing import Any, Type
+from typing import TYPE_CHECKING, Any, Dict, Type
 
 import torch
 from torch.utils.data.dataloader import DataLoader
@@ -22,6 +22,9 @@ from torch.utils.data.dataloader import DataLoader
 from zenml.enums import ArtifactType
 from zenml.io import fileio
 from zenml.materializers.base_materializer import BaseMaterializer
+
+if TYPE_CHECKING:
+    from zenml.metadata.metadata_types import MetadataType
 
 DEFAULT_FILENAME = "entire_dataloader.pt"
 CHECKPOINT_FILENAME = "checkpoint.pt"
@@ -57,3 +60,19 @@ class PyTorchDataLoaderMaterializer(BaseMaterializer):
         # Save entire dataloader to artifact directory
         with fileio.open(os.path.join(self.uri, DEFAULT_FILENAME), "wb") as f:
             torch.save(dataloader, f)
+
+    def extract_metadata(self, dataloader: Any) -> Dict[str, "MetadataType"]:
+        """Extract metadata from the given `DataLoader` object.
+
+        Args:
+            dataloader: The `DataLoader` object to extract metadata from.
+
+        Returns:
+            The extracted metadata as a dictionary.
+        """
+        super().extract_metadata(dataloader)
+        return {
+            "num_samples": len(dataloader.dataset),
+            "batch_size": dataloader.batch_size,
+            "num_batches": len(dataloader),
+        }
