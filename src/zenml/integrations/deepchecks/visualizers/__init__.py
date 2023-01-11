82415feed75d23ccc04de3425fcfb4a0692ee851
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-01-11 10:59:11 +0100

    undo previous autoformatting changes

--- src/zenml/integrations/deepchecks/visualizers/__init__.py
+++ src/zenml/integrations/deepchecks/visualizers/__init__.py
@@ -12,3 +12,7 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """Deepchecks visualizer."""
+
+from zenml.integrations.deepchecks.visualizers.deepchecks_visualizer import (
+    DeepchecksVisualizer,
+)
