dc840fcfe8cea9894d38d266a3603560ce90663e
Michael Schuster <michael.schuster.ffb@googlemail.com>
2023-01-26 11:59:19 +0100

    Track server version

--- src/zenml/zen_stores/schemas/pipeline_run_schemas.py
+++ src/zenml/zen_stores/schemas/pipeline_run_schemas.py
@@ -100,7 +100,8 @@ class PipelineRunSchema(NamedSchema, table=True):
     status: ExecutionStatus
     pipeline_configuration: str = Field(sa_column=Column(TEXT, nullable=False))
     num_steps: Optional[int]
-    zenml_version: str
+    client_version: str
+    server_version: Optional[str] = Field(nullable=True)
     client_environment: Optional[str] = Field(
         sa_column=Column(TEXT, nullable=True)
     )
@@ -145,7 +146,8 @@ class PipelineRunSchema(NamedSchema, table=True):
             pipeline_configuration=configuration,
             num_steps=request.num_steps,
             git_sha=request.git_sha,
-            zenml_version=request.zenml_version,
+            client_version=request.client_version,
+            server_version=request.server_version,
             client_environment=client_environment,
             orchestrator_environment=orchestrator_environment,
         )
@@ -187,7 +189,8 @@ class PipelineRunSchema(NamedSchema, table=True):
                 pipeline_configuration=json.loads(self.pipeline_configuration),
                 num_steps=self.num_steps,
                 git_sha=self.git_sha,
-                zenml_version=self.zenml_version,
+                client_version=self.client_version,
+                server_version=self.server_version,
                 client_environment=client_environment,
                 orchestrator_environment=orchestrator_environment,
                 created=self.created,
@@ -212,7 +215,8 @@ class PipelineRunSchema(NamedSchema, table=True):
                 pipeline_configuration=json.loads(self.pipeline_configuration),
                 num_steps=self.num_steps,
                 git_sha=self.git_sha,
-                zenml_version=self.zenml_version,
+                client_version=self.client_version,
+                server_version=self.server_version,
                 client_environment=client_environment,
                 orchestrator_environment=orchestrator_environment,
                 created=self.created,
