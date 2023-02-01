129763fee71d10a0a7107c6cfcdcdcb12974c7bb
Hamza Tahir <htahir111@gmail.com>
2023-02-01 10:18:10 +0100

    Removed the import

--- src/zenml/integrations/argo/orchestrators/__init__.py
+++ src/zenml/integrations/argo/orchestrators/__init__.py
@@ -16,8 +16,5 @@
 from zenml.integrations.argo.orchestrators.argo_orchestrator import (  # noqa
     ArgoOrchestrator,
 )
-from zenml.integrations.argo.orchestrators.argo_entrypoint_configuration import (  # noqa
-    ArgoEntrypointConfiguration,
-)
 
-__all__ = ["ArgoOrchestrator", "ArgoEntrypointConfiguration"]
+__all__ = ["ArgoOrchestrator"]
