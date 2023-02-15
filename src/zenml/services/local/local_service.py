25219fc35704331d213e94be96f34392061e319d
Hamza Tahir <htahir111@gmail.com>
2023-02-15 10:10:22 +0100

    Woops reformat

--- src/zenml/services/local/local_service.py
+++ src/zenml/services/local/local_service.py
@@ -160,12 +160,10 @@ class LocalDaemonServiceStatus(ServiceStatus):
             try:
                 p = psutil.Process(pid)
                 cmd_line = p.cmdline()
-                
+
                 # Empty cmd_line implies no process
                 if not cmd_line:
-                    logger.debug(
-                        f"Process with PID {pid} not found!"
-                    )
+                    logger.debug(f"Process with PID {pid} not found!")
                     return None
 
                 config_file = self.config_file
