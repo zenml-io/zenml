f7b9671c1a38c75ab2326b7024eb93994c04071a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-02-15 13:33:27 +0100

    add tests for create_secret failure

--- tests/integration/functional/cli/test_secret.py
+++ tests/integration/functional/cli/test_secret.py
@@ -12,10 +12,12 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """Tests for the Secret Store CLI."""
+import pytest
 from click.testing import CliRunner
 
 from zenml.cli.cli import cli
 from zenml.client import Client
+from zenml.enums import SecretScope
 
 
 def test_create_secret(clean_client):
@@ -32,3 +34,32 @@ def test_create_secret(clean_client):
     assert created_secret is not None
     assert created_secret.values["test_value"].get_secret_value() == "aria"
     assert created_secret.values["test_value2"].get_secret_value() == "blupus"
+
+
+def test_create_secret_with_scope(clean_client):
+    """Tests creating a secret with a scope."""
+    secret_create_command = cli.commands["secret"].commands["create"]
+    runner = CliRunner()
+    result = runner.invoke(
+        secret_create_command,
+        ["test_secret", "--test_value=aria", f"--scope={SecretScope.USER}"],
+    )
+    assert result.exit_code == 0
+    client = Client()
+    created_secret = client.get_secret("test_secret")
+    assert created_secret is not None
+    assert created_secret.values["test_value"].get_secret_value() == "aria"
+    assert created_secret.scope == SecretScope.USER
+
+
+def test_create_fails_with_bad_scope(clean_client):
+    secret_create_command = cli.commands["secret"].commands["create"]
+    runner = CliRunner()
+    result = runner.invoke(
+        secret_create_command,
+        ["test_secret", "--test_value=aria", f"--scope=axl_scope"],
+    )
+    assert result.exit_code != 0
+    client = Client()
+    with pytest.raises(KeyError):
+        client.get_secret("test_secret")
