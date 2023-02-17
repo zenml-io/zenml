c4f121e266f6b291091292094f5aa4fa0fd45e50
Felix Altenberger <felix@zenml.io>
2023-02-17 17:05:33 +0100

    Implement zenml hub pull CLI command

--- src/zenml/cli/hub.py
+++ src/zenml/cli/hub.py
@@ -13,13 +13,16 @@
 #  permissions and limitations under the License.
 """CLI functionality to interact with the ZenML Hub."""
 import os
+import shutil
 import subprocess
 import sys
 from importlib.util import find_spec
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
+from uuid import uuid4
 
 import click
 import requests
+from git.repo import Repo
 from pydantic import BaseModel
 
 from zenml.cli.cli import TagGroup, cli
@@ -40,7 +43,9 @@ class PluginResponseModel(BaseModel):
     """Response model for a plugin."""
 
     name: str
-    logo: str
+    source_url: str
+    # repository_url: str
+    # repository_subdir: Optional[str]
     index_url: str
     wheel_name: str
 
@@ -49,7 +54,7 @@ def server_url() -> str:
     """Helper function to get the hub url."""
     if HUB_URL:
         return HUB_URL
-    return "https://demo-app-mc5klosfeq-lm.a.run.app"
+    return "https://staginghub.zenml.io/"
 
 
 def _hub_get(url: str) -> Json:
@@ -154,3 +159,55 @@ def uninstall_plugin(plugin_name: str) -> None:
         [sys.executable, "-m", "pip", "uninstall", wheel_name, "-y"]
     )
     logger.info(f"Successfully uninstalled plugin '{plugin_name}'.")
+
+
+@hub.command("pull")
+@click.argument("plugin_name", type=str, required=True)
+@click.option(
+    "--output_dir",
+    type=str,
+    help="Output directory to pull the plugin to.",
+)
+def pull_plugin(plugin_name: str, output_dir: Optional[str]) -> None:
+    """Pull a plugin from the hub."""
+    # Get plugin from hub
+    plugin = _get_plugin(plugin_name)
+
+    # TODO: uncomment / change below when we have new server
+    repo_url = plugin.source_url
+    parts = repo_url.split("/tree/main/")
+    repo_url = parts[0]
+    subdir = parts[1] if len(parts) > 1 else None
+    # repo_url = plugin.repository_url
+    # commit = plugin.repository_commit
+    # subdir = plugin.repository_subdir
+
+    # Clone the source repo
+    if output_dir is None:
+        output_dir = os.path.join(os.getcwd(), plugin_name)
+    logger.info(f"Pulling plugin '{plugin_name}' into {output_dir}...")
+
+    # If no subdir, we can clone directly into output_dir
+    if not subdir:
+        repo_path = plugin_dir = output_dir
+    # Otherwise, we need to clone into a random dir and then move the subdir
+    else:
+        random_repo_name = f"_{uuid4()}"
+        repo_path = os.path.abspath(
+            os.path.join(os.getcwd(), random_repo_name)
+        )
+        plugin_dir = os.path.join(repo_path, subdir)
+
+    # Clone the repo and checkout the right commit
+    Repo.clone_from(
+        url=repo_url,
+        to_path=repo_path,
+        # no_checkout=True,
+    )
+    # repo.git.checkout(commit)
+
+    # Move the subdir into the output_dir
+    if subdir:
+        shutil.move(plugin_dir, output_dir)
+        shutil.rmtree(repo_path)
+    logger.info(f"Successfully pulled plugin '{plugin_name}'.")
