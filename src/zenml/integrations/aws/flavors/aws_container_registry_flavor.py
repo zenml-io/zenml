d0353adf60048bfa533d20c44c0a6f0868e22d23
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-02-23 11:19:56 +0100

    update docs urls

--- src/zenml/integrations/aws/flavors/aws_container_registry_flavor.py
+++ src/zenml/integrations/aws/flavors/aws_container_registry_flavor.py
@@ -73,7 +73,15 @@ class AWSContainerRegistryFlavor(BaseContainerRegistryFlavor):
         Returns:
             A flavor docs url.
         """
-        return self.generate_default_docs_url()
+        from packaging.version import parse
+
+        from zenml import __version__
+
+        if parse(__version__) >= parse("0.34.0"):
+            return self.generate_default_docs_url()
+
+        old_docs_name = "amazon-ecr"
+        return self.generate_default_docs_url(component_name=old_docs_name)
 
     @property
     def logo_url(self) -> str:
