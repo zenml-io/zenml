977b0265e9746b77c5cc0660089c7c258353ebce
Michael Schuster <schustmi@users.noreply.github.com>
2023-03-01 16:48:05 +0100

    Load artifact store before loading artifact to register filesystem (#1367)
    * Load artifact store before loading artifact to register filesystem
    
    * Add missing id to log
    
    * Fix unit test

--- tests/unit/utils/test_materializer_utils.py
+++ tests/unit/utils/test_materializer_utils.py
@@ -14,6 +14,7 @@
 import os
 import shutil
 import tempfile
+from uuid import uuid4
 
 import numpy as np
 import pytest
@@ -44,6 +45,7 @@ def model_artifact(mocker):
         uri="gs://my-bucket/model.joblib",
         data_type="path/to/model/class",
         materializer="path/to/materializer/class",
+        artifact_store_id=uuid4(),
     )
 
 
