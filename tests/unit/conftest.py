fd701eeb72771a2d853a69b3c10e8b119ce4e5a7
Jayesh Sharma <wjayesh@outlook.com>
2023-03-07 14:25:25 +0530

    remove unused import

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-from click.testing import CliRunner
 from datetime import datetime
 from typing import Any, Callable, Dict, Generator, Optional
 from uuid import uuid4
