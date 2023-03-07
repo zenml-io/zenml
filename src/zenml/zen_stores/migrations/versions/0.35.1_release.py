b50a33f1043774776179cd5310641e294bf4e2c6
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-03-07 15:16:29 +0100

    add migration

---
+++ src/zenml/zen_stores/migrations/versions/0.35.1_release.py
@@ -0,0 +1,24 @@
+"""Release [0.35.1].
+
+Revision ID: 0.35.1
+Revises: a2bf3f4cebda
+Create Date: 2023-03-07 15:15:42.057028
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "0.35.1"
+down_revision = "a2bf3f4cebda"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade database schema and/or data, creating a new revision."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade database schema and/or data back to the previous revision."""
+    pass
