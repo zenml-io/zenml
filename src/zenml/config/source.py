8b5697e8cf40d3fad1a2a1df88f0b638c6942284
Michael Schuster <michael.schuster.ffb@googlemail.com>
2023-03-14 13:22:05 +0100

    More docstrings

--- src/zenml/config/source.py
+++ src/zenml/config/source.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-
+"""Source classes."""
 from enum import Enum
 from typing import Optional
 from uuid import UUID
@@ -24,6 +24,8 @@ logger = get_logger(__name__)
 
 
 class SourceType(Enum):
+    """Enum representing different types of sources."""
+
     USER = "user"
     BUILTIN = "builtin"
     INTERNAL = "internal"
@@ -33,6 +35,14 @@ class SourceType(Enum):
 
 
 class Source(BaseModel):
+    """Source specification.
+
+    Attributes:
+        module: The module name.
+        attribute: Optional name of the attribute inside the module.
+        type: The type of the source.
+    """
+
     module: str
     attribute: Optional[str] = None
     type: SourceType
@@ -52,6 +62,17 @@ class Source(BaseModel):
 
     @classmethod
     def from_import_path(cls, import_path: str) -> "Source":
+        """Creates a source from an import path.
+
+        Args:
+            import_path: The import path.
+
+        Raises:
+            ValueError: If the import path is empty.
+
+        Returns:
+            The source.
+        """
         if not import_path:
             raise ValueError(
                 "Invalid empty import path. The import path needs to refer "
@@ -74,6 +95,11 @@ class Source(BaseModel):
 
     @property
     def import_path(self) -> str:
+        """The import path of the source.
+
+        Returns:
+            The import path of the source.
+        """
         if self.attribute:
             return f"{self.module}.{self.attribute}"
         else:
@@ -81,6 +107,11 @@ class Source(BaseModel):
 
     @property
     def is_internal(self) -> bool:
+        """If the source is internal (=from the zenml package).
+
+        Returns:
+            True if the source is internal, False otherwise
+        """
         if self.type not in {
             SourceType.UNKNOWN,
             SourceType.DISTRIBUTION_PACKAGE,
@@ -91,18 +122,41 @@ class Source(BaseModel):
 
     @property
     def is_module_source(self) -> bool:
+        """If the source is a module source.
+
+        Returns:
+            If the source is a module source.
+        """
         return self.attribute is None
 
     class Config:
+        """Pydantic config class."""
         extra = Extra.allow
 
 
 class DistributionPackageSource(Source):
+    """Source representing an object from a distribution package.
+
+    Attributes:
+        version: The package version.
+    """
+
     version: Optional[str] = None
     type: SourceType = SourceType.DISTRIBUTION_PACKAGE
 
     @validator("type")
     def _validate_type(cls, value: SourceType) -> SourceType:
+        """Validate the source type.
+
+        Args:
+            value: The source type.
+
+        Raises:
+            ValueError: If the source type is not `DISTRIBUTION_PACKAGE`.
+
+        Returns:
+            The source type.
+        """
         if value != SourceType.DISTRIBUTION_PACKAGE:
             raise ValueError("Invalid source type.")
 
@@ -110,12 +164,26 @@ class DistributionPackageSource(Source):
 
     @property
     def package_name(self) -> Optional[str]:
+        """The package name.
+
+        Returns:
+            The package name if a package for the source module exists.
+        """
         from zenml.utils import source_utils_v2
 
         return source_utils_v2._get_package_for_module(module_name=self.module)
 
 
 class CodeRepositorySource(Source):
+    """Source representing an object from a code repository.
+
+    Attributes:
+        repository_id: The code repository ID.
+        commit: The commit.
+        subdirectory: The subdirectory of the source root inside the code
+            repository.
+    """
+
     repository_id: UUID
     commit: str
     subdirectory: str
@@ -123,6 +191,17 @@ class CodeRepositorySource(Source):
 
     @validator("type")
     def _validate_type(cls, value: SourceType) -> SourceType:
+        """Validate the source type.
+
+        Args:
+            value: The source type.
+
+        Raises:
+            ValueError: If the source type is not `CODE_REPOSITORY`.
+
+        Returns:
+            The source type.
+        """
         if value != SourceType.CODE_REPOSITORY:
             raise ValueError("Invalid source type.")
 
