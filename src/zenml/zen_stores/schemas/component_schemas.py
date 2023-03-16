b66de0c91c43cdbb57a55f743e73392ea9ec8515
Jayesh Sharma <wjayesh@outlook.com>
2023-03-16 14:01:57 +0530

    remove Hex

--- src/zenml/zen_stores/schemas/component_schemas.py
+++ src/zenml/zen_stores/schemas/component_schemas.py
@@ -19,7 +19,7 @@ from datetime import datetime
 from typing import TYPE_CHECKING, List, Optional
 from uuid import UUID
 
-from sqlmodel import Column, Field, Relationship
+from sqlmodel import Relationship
 
 from zenml.enums import StackComponentType
 from zenml.models.component_models import (
@@ -40,25 +40,6 @@ if TYPE_CHECKING:
     from zenml.zen_stores.schemas import ScheduleSchema
 
 
-from sqlalchemy.types import String, TypeDecorator
-
-
-class HexByteString(TypeDecorator):
-    """Convert Python bytestring to string with hexadecimal digits and back for storage."""
-
-    impl = String
-
-    def process_bind_param(self, value, dialect):
-        breakpoint()
-        if not isinstance(value, bytes):
-            raise TypeError("HexByteString columns support only bytes values.")
-        return value.hex()
-
-    def process_result_value(self, value, dialect):
-        breakpoint()
-        return bytes.fromhex(value) if value else None
-
-
 class StackComponentSchema(ShareableSchema, table=True):
     """SQL Model for stack components."""
 
@@ -67,7 +48,7 @@ class StackComponentSchema(ShareableSchema, table=True):
     type: StackComponentType
     flavor: str
     configuration: bytes
-    metadata_values: Optional[bytes] = Field(sa_column=Column(HexByteString))
+    metadata_values: Optional[bytes]
 
     workspace_id: UUID = build_foreign_key_field(
         source=__tablename__,
