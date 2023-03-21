2f1a14da713745d8f376f7b992d34b61e3f975eb
Stefan Nica <stefan@zenml.io>
2023-03-21 13:44:33 +0100

    Update seldon example integration test case

--- tests/integration/examples/test_seldon_deployment.py
+++ tests/integration/examples/test_seldon_deployment.py
@@ -1,4 +1,4 @@
-#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#  Copyright (c) ZenML GmbH 2023. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,12 +18,6 @@ import pytest
 
 from tests.integration.examples.utils import run_example
 from zenml.client import Client
-from zenml.integrations.seldon.model_deployers.seldon_model_deployer import (
-    SeldonModelDeployer,
-)
-from zenml.integrations.seldon.services.seldon_deployment import (
-    SeldonDeploymentService,
-)
 from zenml.post_execution.pipeline import get_pipeline
 
 
@@ -31,28 +25,44 @@ def test_example(request: pytest.FixtureRequest) -> None:
     """Runs the seldon_deployment example.
 
     Args:
-        tmp_path_factory: Factory to generate temporary test paths.
+        request: pytest request object.
     """
     with run_example(
         request=request,
         name="seldon_deployment",
-        pipeline_name="continuous_deployment_pipeline",
-        run_count=1,
-    ) as (example, _):
+        pipelines={
+            "continuous_deployment_pipeline": (1, 6),
+            "inference_pipeline": (1, 4),
+        },
+    ) as run_result:
+
+        from zenml.integrations.seldon.model_deployers.seldon_model_deployer import (
+            SeldonModelDeployer,
+        )
+        from zenml.integrations.seldon.services.seldon_deployment import (
+            SeldonDeploymentService,
+        )
+
+        _, pipeline_runs = run_result
 
         pipeline = get_pipeline("continuous_deployment_pipeline")
         assert pipeline
 
-        # activate the stack set up and used by the example
+        # get the active model deployer used by the example
         client = Client()
         model_deployer = client.active_stack.model_deployer
         assert isinstance(model_deployer, SeldonModelDeployer)
 
         services = model_deployer.find_model_server(
             pipeline_name="continuous_deployment_pipeline",
-            pipeline_step_name="seldon_model_deployer_step",
+            pipeline_step_name="model_deployer",
+            pipeline_run_id=pipeline_runs["continuous_deployment_pipeline"][
+                0
+            ].model.name,
         )
 
         assert services
         service = cast(SeldonDeploymentService, services[0])
         assert service.is_running
+
+        service.stop(timeout=60)
