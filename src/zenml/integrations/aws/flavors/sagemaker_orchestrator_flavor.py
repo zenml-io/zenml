f1782f9e14435a3f4ff08e11ddbde58a43d73505
Christian Versloot <c.versloot@infoplaza.nl>
2023-04-25 18:16:56 +0200

    Add missing typing imports

--- src/zenml/integrations/aws/flavors/sagemaker_orchestrator_flavor.py
+++ src/zenml/integrations/aws/flavors/sagemaker_orchestrator_flavor.py
@@ -13,7 +13,7 @@
 #  permissions and limitations under the License.
 """Amazon SageMaker orchestrator flavor."""
 
-from typing import TYPE_CHECKING, Dict, Optional, Type
+from typing import TYPE_CHECKING, Any, Dict, Optional, Type, Union
 
 from zenml.config.base_settings import BaseSettings
 from zenml.integrations.aws import AWS_SAGEMAKER_STEP_OPERATOR_FLAVOR
