2ec7147bdf8f439e2459cb7a7178f34728dcc22b
François Serra <francois.serra@adeo.com>
2023-05-05 18:03:28 +0200

    feat: Set cloud function service account to the one defined in Vertex Orchestrator config

--- src/zenml/integrations/gcp/orchestrators/vertex_orchestrator.py
+++ src/zenml/integrations/gcp/orchestrators/vertex_orchestrator.py
@@ -559,6 +559,7 @@ class VertexOrchestrator(ContainerizedOrchestrator, GoogleCredentialsMixin):
             location=self.config.location,
             function_name=run_name,
             credentials=credentials,
+            function_service_account_email=self.config.workload_service_account,
         )
 
         # Create the scheduler job
