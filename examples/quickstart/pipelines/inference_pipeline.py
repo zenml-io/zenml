eca68bf6b0cd03c1a66dd40bd3b9cf73375d5ec1
Felix Altenberger <felix@zenml.io>
2023-05-24 13:21:10 +0200

    Adjust quickstart

--- examples/quickstart/pipelines/inference_pipeline.py
+++ examples/quickstart/pipelines/inference_pipeline.py
@@ -12,26 +12,40 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+from steps import (
+    drift_detector,
+    inference_data_loader,
+    predictor,
+    training_data_loader,
+)
+
 from zenml import pipeline
 from zenml.config import DockerSettings
 from zenml.integrations.constants import SKLEARN
+from zenml.integrations.mlflow.steps.mlflow_deployer import (
+    mlflow_model_registry_deployer_step,
+)
 
 docker_settings = DockerSettings(
     required_integrations=[SKLEARN],
 )
 
 
+model_deployer = mlflow_model_registry_deployer_step.with_options(
+    parameters=dict(
+        registry_model_name="zenml-quickstart-model",
+        registry_model_version="1",
+        # or you can use the model stage if you have set it in the MLflow registry
+        # registered_model_stage="None" # "Staging", "Production", "Archived"
+    )
+)
+
+
 @pipeline(settings={"docker": docker_settings})
-def inference_pipeline(
-    inference_data_loader,
-    mlflow_model_deployer,
-    predictor,
-    training_data_loader,
-    drift_detector,
-):
+def inference_pipeline():
     """Inference pipeline with skew and drift detection."""
     inference_data = inference_data_loader()
-    model_deployment_service = mlflow_model_deployer()
-    predictor(model_deployment_service, inference_data)
+    model_deployment_service = model_deployer()
+    predictor(service=model_deployment_service, data=inference_data)
     training_data, _, _, _ = training_data_loader()
     drift_detector(training_data, inference_data)
--- examples/quickstart/steps/trainers.py
+++ examples/quickstart/steps/trainers.py
@@ -38,10 +38,11 @@ if not experiment_tracker or not isinstance(
 def svc_trainer_mlflow(
     X_train: pd.DataFrame,
     y_train: pd.Series,
+    gamma=0.01,
 ) -> ClassifierMixin:
     """Train a sklearn SVC classifier and log to MLflow."""
     mlflow.sklearn.autolog()  # log all model hparams and metrics to MLflow
-    model = SVC(gamma=0.01)
+    model = SVC(gamma=gamma)
     model.fit(X_train.to_numpy(), y_train.to_numpy())
     train_acc = model.score(X_train.to_numpy(), y_train.to_numpy())
     print(f"Train accuracy: {train_acc}")
