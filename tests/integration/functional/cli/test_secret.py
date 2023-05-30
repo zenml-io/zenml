0145588fa07dd234f2661a6e96b9dc855d396e0c
priyadutt <bhattpriyadutt@gmail.com>
2023-05-30 08:46:25 +0530

    formatting

--- tests/integration/functional/cli/test_secret.py
+++ tests/integration/functional/cli/test_secret.py
@@ -184,8 +184,7 @@ def test_get_secret_with_scope_works():
             secret_create_command,
             [
                 secret_name,
-                "--values=test_value: aria \ntest_value2: axl"
-                "--scope=user",
+                "--values=test_value: aria \ntest_value2: axl" "--scope=user",
             ],
         )
 
