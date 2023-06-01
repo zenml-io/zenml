906a1b3b74a029780d686a574e3687d9f357e054
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-01 15:25:42 +0200

    update using felix's fix

--- examples/mlflow_tracking/run.py
+++ examples/mlflow_tracking/run.py
@@ -16,14 +16,16 @@ from pipelines.training_pipeline.training_pipeline import (
     mlflow_example_pipeline,
 )
 
-from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+from zenml.constants import METADATA_EXPERIMENT_TRACKER_URL
 
 if __name__ == "__main__":
     mlflow_example_pipeline()
 
+    trainer_step = mlflow_example_pipeline.get_runs()[0].get_step("tf_trainer")
+    tracking_uri = trainer_step.metadata[METADATA_EXPERIMENT_TRACKER_URL].value
     print(
         "Now run \n "
-        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
+        f"    mlflow ui --backend-store-uri '{tracking_uri}'\n"
         "To inspect your experiment runs within the mlflow UI.\n"
         "You can find your runs tracked within the `mlflow_example_pipeline`"
         "experiment. Here you'll also be able to compare the two runs.)"
