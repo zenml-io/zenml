e3d3c795124960531e6a2483c9b43d63a1e86ec5
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-06-01 13:56:29 +0200

    Add orchestrator outputs for sandbox examples (#1579)
    * add kubeflow URL printout
    
    * remove data validator
    
    * update using metadata_url
    
    * fix pipeline name
    
    * add conditional wrapper
    
    * Updated to more logs
    
    ---------
    
    Co-authored-by: Hamza Tahir <htahir111@gmail.com>

--- examples/mlflow_tracking/run.py
+++ examples/mlflow_tracking/run.py
@@ -16,15 +16,48 @@ from pipelines.training_pipeline.training_pipeline import (
     mlflow_example_pipeline,
 )
 
+from zenml.client import Client
+from zenml.enums import StoreType
 from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+from zenml.logger import get_logger
+from zenml.post_execution import get_pipeline
+
+logger = get_logger(__name__)
+
 
 if __name__ == "__main__":
     mlflow_example_pipeline()
 
-    print(
-        "Now run \n "
-        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
-        "To inspect your experiment runs within the mlflow UI.\n"
-        "You can find your runs tracked within the `mlflow_example_pipeline`"
-        "experiment. Here you'll also be able to compare the two runs.)"
-    )
+    p = get_pipeline("mlflow_example_pipeline")
+    run_metadata = p.runs[0].metadata
+    orchestrator_url = run_metadata.get("orchestrator_url")
+    experiment_tracker_url = get_tracking_uri()
+
+    client = Client()
+
+    if client.zen_store.type == StoreType.REST:
+        url = client.zen_store.url
+        url = (
+            url
+            + f"/workspaces/{client.active_workspace.name}/all-runs/{str(p.runs[0].id)}/dag"
+        )
+        logger.info(
+            f"\n\n****Check out the ZenML dashboard to see your run:****\n{url}"
+        )
+
+    if orchestrator_url:
+        logger.info(
+            f"\n\n****See your run directly in the orchestrator:****\n{orchestrator_url}"
+        )
+
+    if experiment_tracker_url:
+        logger.info(
+            f"\n\n****See your run directly in the experiment tracker:****\n"
+        )
+        logger.info(
+            "Now run \n "
+            f"    mlflow ui --backend-store-uri '{experiment_tracker_url}'\n"
+            "To inspect your experiment runs within the mlflow UI.\n"
+            "You can find your runs tracked within the `mlflow_example_pipeline` "
+            "experiment."
+        )
