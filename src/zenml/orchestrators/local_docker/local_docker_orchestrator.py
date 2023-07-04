e845e7103d61ac2218dcd92cdf60c0fdfa61fed0
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-07-04 18:04:13 +0200

    Update Local Docker orchestrator docs (#1676)
    * add docker page link for run_args
    
    * add docker page link for run_args
    
    * update URLs and add example

--- src/zenml/orchestrators/local_docker/local_docker_orchestrator.py
+++ src/zenml/orchestrators/local_docker/local_docker_orchestrator.py
@@ -199,7 +199,9 @@ class LocalDockerOrchestratorSettings(BaseSettings):
     """Local Docker orchestrator settings.
 
     Attributes:
-        run_args: Arguments to pass to the `docker run` call.
+        run_args: Arguments to pass to the `docker run` call. (See
+            https://docker-py.readthedocs.io/en/stable/containers.html for a list
+            of what can be passed.)
     """
 
     run_args: Dict[str, Any] = {}
