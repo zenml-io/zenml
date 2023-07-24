36e0b9c307ba70c1995e3d038c13929b54783cec
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-07-24 17:28:06 +0200

    unify slack alerter

--- examples/e2e/steps/alerts/slack_notifications.py
+++ examples/e2e/steps/alerts/slack_notifications.py
@@ -1,40 +1,28 @@
-import os
+from config import NOTIFY_ON_FAILURE, NOTIFY_ON_SUCCESS
 
-from config import NOTIFY_ON_FAILURE, NOTIFY_ON_SUCCESS, ModelMetadata
-
+from zenml import get_step_context
 from zenml.client import Client
-from zenml.config.global_config import GlobalConfiguration
-from zenml.integrations.slack.steps.slack_alerter_post_step import (
-    slack_alerter_post_step,
-)
+from zenml.utils.dashboard_utils import get_run_url
+
+alerter = Client().active_stack.alerter
 
 
 def get_slack_message(status: str) -> str:
-    pipeline = Client().get_pipeline(ModelMetadata.pipeline_name)
-    latest_run = pipeline.last_run
-    run_url = os.path.join(
-        GlobalConfiguration().store.url,
-        "workspaces",
-        pipeline.workspace.name,
-        "pipelines",
-        str(pipeline.id),
-        "runs",
-        str(latest_run.id),
-        "dag",
-    )
+    step_context = get_step_context()
+    run_url = get_run_url(step_context.pipeline_run)
 
     return (
-        f"Pipeline `{pipeline.name}` [{str(pipeline.id)}] {status}!\n"
-        f"Run `{latest_run.name}` [{str(latest_run.id)}]\n"
+        f"Pipeline `{step_context.pipeline_name}` [{str(step_context.pipeline.id)}] {status}!\n"
+        f"Run `{step_context.pipeline_run.name}` [{str(step_context.pipeline_run.id)}]\n"
         f"URL: {run_url}"
     )
 
 
 def notify_on_failure() -> None:
     if NOTIFY_ON_FAILURE:
-        slack_alerter_post_step(message=get_slack_message(status="failed"))
+        alerter.post(message=get_slack_message(status="failed"))
 
 
 def notify_on_success() -> None:
     if NOTIFY_ON_SUCCESS:
-        slack_alerter_post_step(message=get_slack_message(status="succeeded"))
+        alerter.post(message=get_slack_message(status="succeeded"))
