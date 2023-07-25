bc90fe9445ae4f25b0e724863cc91f420ca553cc
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-07-25 12:01:24 +0200

    add `find_artifact_id` utility

---
+++ examples/e2e/utils/artifacts.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from zenml import step
+from zenml.client import Client
+from zenml.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+@step
+def find_artifact_id(
+    pipeline_name: str, artifact_name: str
+) -> Annotated[str, "artifact_id"]:
+    pipeline = Client().get_pipeline(pipeline_name)
+    latest_run = pipeline.runs[0]
+    artifacts = [a for a in latest_run.artifacts if a.name == artifact_name]
+    if artifacts:
+        return artifacts[0].id
+    else:
+        raise ValueError(
+            f"`{artifact_name}` not found in last run of `{pipeline_name}`"
+        )
