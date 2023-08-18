7090f74f4575845c1cf932af845bc3915e1e1291
Baris Can Durak <bariscandurak@hotmail.com>
2023-08-18 09:47:45 +0200

    formatting

--- src/zenml/logging/step_logging.py
+++ src/zenml/logging/step_logging.py
@@ -12,7 +12,6 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """ZenML logging handler."""
-import logging
 import os
 import re
 import sys
