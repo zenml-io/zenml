953cbf5df6ed2d4e2c06ed20cd532b82b702c8c5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-27 07:53:27 +0200

    add unit test

--- tests/unit/utils/test_mlstacks_utils.py
+++ tests/unit/utils/test_mlstacks_utils.py
@@ -12,10 +12,17 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-from zenml.utils.mlstacks_utils import stack_exists
+import pytest
+
+from zenml.utils.mlstacks_utils import get_stack_spec_file_path, stack_exists
 
 
 def test_stack_exists_works(local_stack):
     stack_name = "aria_test_stack"
     assert not stack_exists(stack_name)
     assert stack_exists(local_stack.name)
+
+
+def test_get_stack_spec_file_path_fails_when_no_stack():
+    with pytest.raises(KeyError):
+        get_stack_spec_file_path("blupus_stack")
