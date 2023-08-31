817f592586549a623ae19ead594f95c7e7f6ea4c
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-08-31 09:51:13 +0200

    add script to check branch divergence

---
+++ scripts/check-alembic-branches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the alembic branches command and capture the output
+output=$(alembic branches)
+
+# Check if there's any output
+if [[ -n "$output" ]]; then
+  echo "Warning: Diverging Alembic branches detected."
+  exit 1  # Exit with failure status
+else
+  echo "No diverging Alembic branches detected."
+  exit 0  # Exit with success status
+fi
