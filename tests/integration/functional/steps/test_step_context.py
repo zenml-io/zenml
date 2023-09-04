059c3a2d4b505211c9a3a8d768cfd835da978c10
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-09-04 09:24:10 +0200

    PR comments

--- tests/integration/functional/steps/test_step_context.py
+++ tests/integration/functional/steps/test_step_context.py
@@ -11,11 +11,15 @@ from zenml.materializers.base_materializer import BaseMaterializer
 
 
 class ComplexObject(BaseModel):
+    """This is a custom object to be materialized with ComplexObjectMaterializer."""
+
     name: str
     pipeline_name: Optional[str]
 
 
 class ComplexObjectMaterializer(BaseMaterializer):
+    """This materializer just tries to access step context inside save."""
+
     ASSOCIATED_TYPES = (ComplexObject,)
     ASSOCIATED_ARTIFACT_TYPE = ArtifactType.STATISTICS
 
@@ -30,6 +34,7 @@ class ComplexObjectMaterializer(BaseMaterializer):
     def save(self, data: ComplexObject) -> None:
         super().save(data)
 
+        # here we need access to the step context
         data.pipeline_name = get_step_context().pipeline.name
         with fileio.open(os.path.join(self.uri, "data.json"), "w") as f:
             f.write(data.json())
@@ -41,20 +46,28 @@ class ComplexObjectMaterializer(BaseMaterializer):
     ]
 )
 def _output_complex_object_step():
+    """This step would call `save` of `ComplexObjectMaterializer`.
+    `save` should not fail and have access to the step context"""
     return ComplexObject(name="foo")
 
 
 @step
 def _input_complex_object_step(inp: ComplexObject):
+    """This step would call `load` of `ComplexObjectMaterializer`.
+    `load` should be able to read detail of step context created on `save`."""
     assert inp.pipeline_name == get_step_context().pipeline.name
 
 
 @step
 def _access_step_context_step():
+    """This step tries to access step context while inside Step execution"""
     assert get_step_context().pipeline.name == "bar"
 
 
 def test_materializer_can_access_step_context():
+    """Call steps using `ComplexObjectMaterializer` to validate that
+    step context is available to Materializers"""
+
     @pipeline(name="bar")
     def _complex_object_materialization_pipeline():
         complex_object = _output_complex_object_step()
@@ -64,6 +77,8 @@ def test_materializer_can_access_step_context():
 
 
 def test_step_can_access_step_context():
+    """Call step using step context directly, before Materializers"""
+
     @pipeline(name="bar")
     def _simple_step_pipeline():
         _access_step_context_step()
