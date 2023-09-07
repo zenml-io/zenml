c3a47ca023d93b02e670e6298e537348583254c5
Felix Altenberger <felix@zenml.io>
2023-09-07 10:54:36 +0200

    Improve `ConnectionError` error message (#1783)
    * Improve Error Handling for ConnectionErrors
    
    * Update src/zenml/zen_stores/base_zen_store.py
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Make zenml down also disconnect from the local server
    
    * Fix tests
    
    * Also make zenml down disconnect if server is not running
    
    ---------
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>

--- src/zenml/cli/server.py
+++ src/zenml/cli/server.py
@@ -238,15 +238,16 @@ def down() -> None:
 
     if not server:
         cli_utils.declare("The local ZenML dashboard is not running.")
-        return
-    from zenml.zen_server.deploy.deployer import ServerDeployer
 
-    deployer = ServerDeployer()
-    deployer.remove_server(server.config.name)
+    else:
+        from zenml.zen_server.deploy.deployer import ServerDeployer
 
-    GlobalConfiguration()
+        deployer = ServerDeployer()
+        deployer.remove_server(server.config.name)
+        cli_utils.declare("The local ZenML dashboard has been shut down.")
 
-    cli_utils.declare("The local ZenML dashboard has been shut down.")
+    gc = GlobalConfiguration()
+    gc.set_default_store()
 
 
 @cli.command("deploy", help="Deploy ZenML in the cloud.")
