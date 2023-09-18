0099bec2f8720b963f83fb5bc90ebd91073cfeeb
Safoine El Khabich <safoine.ext@zenml.io>
2023-09-18 21:34:51 +0100

    update python version on skypilot tests

--- tests/unit/integrations/skypilot/orchestrators/test_skypilot_orchestrator.py
+++ tests/unit/integrations/skypilot/orchestrators/test_skypilot_orchestrator.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+import sys
 from contextlib import ExitStack as does_not_raise
 from datetime import datetime
 from typing import TYPE_CHECKING
@@ -28,6 +29,10 @@ if TYPE_CHECKING:
     )
 
 
+@pytest.mark.skipif(
+    sys.version_info > (3, 10),
+    reason="SkyPilot can not be installed in Python 3.10 or higher.",
+)
 def _get_skypilot_orchestrator(
     provider, **kwargs
 ) -> "SkypilotBaseOrchestrator":
