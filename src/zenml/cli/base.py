4e312bd1e4b00f160dd16ae1aa5deb05ec706228
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-10-02 10:56:17 +0200

    add helper text

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -72,7 +72,7 @@ class ZenMLProjectTemplateLocation(BaseModel):
 ZENML_PROJECT_TEMPLATES = dict(
     e2e_batch=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-e2e-batch",
-        github_tag="0.43.0",
+        github_tag="0.43.0",  # Make sure it is aligned with .github/workflows/update-e2e-template.yml
     ),
     starter=ZenMLProjectTemplateLocation(
         github_url="zenml-io/zenml-project-templates",
