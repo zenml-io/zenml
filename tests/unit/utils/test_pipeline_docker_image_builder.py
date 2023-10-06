0814ac1b990075fd28d6d901af1eaf9f017723d2
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-10-06 08:58:21 +0200

    Update tests/unit/utils/test_pipeline_docker_image_builder.py
    Co-authored-by: Felix Altenberger <felix@zenml.io>

--- tests/unit/utils/test_pipeline_docker_image_builder.py
+++ tests/unit/utils/test_pipeline_docker_image_builder.py
@@ -42,7 +42,6 @@ def test_check_user_is_set():
             download_files=False,
         )
     )
-    assert "USER root" in generated_dockerfile
     assert "USER test_user" in generated_dockerfile
 
 
