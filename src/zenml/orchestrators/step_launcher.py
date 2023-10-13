40239098714b9213c127f5bf3911a0354ce424db
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-10-13 18:33:54 +0200

    refactor

--- src/zenml/orchestrators/step_launcher.py
+++ src/zenml/orchestrators/step_launcher.py
@@ -53,6 +53,7 @@ from zenml.stack import Stack
 from zenml.utils import string_utils
 
 if TYPE_CHECKING:
+    from zenml.model import ModelConfig
     from zenml.models.artifact_models import ArtifactResponseModel
     from zenml.models.pipeline_deployment_models import (
         PipelineDeploymentResponseModel,
@@ -381,45 +382,54 @@ class StepLauncher:
                     for output_name, artifact in cached_outputs.items()
                 }
                 if model_config:
-                    from zenml.model.artifact_config import ArtifactConfig
-                    from zenml.steps.base_step import BaseStep
-                    from zenml.steps.utils import parse_return_type_annotations
-
-                    model_version = model_config.get_or_create_model_version()
-                    step_instance = BaseStep.load_from_source(
-                        self._step.spec.source
+                    self._link_cached_artifacts_to_model_version(
+                        model_config=model_config,
+                        step_run=step_run,
                     )
-                    output_annotations = parse_return_type_annotations(
-                        step_instance.entrypoint
-                    )
-                    for output_name_, output_ in step_run.outputs.items():
-                        if output_name_ in output_annotations:
-                            annotation = output_annotations.get(
-                                output_name_, None
-                            )
-                            artifact_config = (
-                                annotation.artifact_config
-                                if annotation
-                                and annotation.artifact_config is not None
-                                else ArtifactConfig()
-                            )
-                            artifact_config.model_name = (
-                                artifact_config.model_name
-                                or model_version.model.name
-                            )
-                            artifact_config.model_version = (
-                                artifact_config.model_version
-                                or model_version.name
-                            )
-                            artifact_config._pipeline_name = (
-                                self._deployment.pipeline_configuration.name
-                            )
-                            artifact_config._step_name = self._step_name
-                            artifact_config.link_to_model(output_)
                 step_run.status = ExecutionStatus.CACHED
                 step_run.end_time = step_run.start_time
 
         return execution_needed, step_run
+
+    def _link_cached_artifacts_to_model_version(
+        self,
+        model_config: "ModelConfig",
+        step_run: StepRunRequestModel,
+    ) -> None:
+        """Links the output artifacts of the cached step to the model version in Control Plane.
+
+        Args:
+            model_config: The model config of the current step.
+            step_run: The step to run.
+        """
+        from zenml.model.artifact_config import ArtifactConfig
+        from zenml.steps.base_step import BaseStep
+        from zenml.steps.utils import parse_return_type_annotations
+
+        model_version = model_config.get_or_create_model_version()
+        step_instance = BaseStep.load_from_source(self._step.spec.source)
+        output_annotations = parse_return_type_annotations(
+            step_instance.entrypoint
+        )
+        for output_name_, output_ in step_run.outputs.items():
+            if output_name_ in output_annotations:
+                annotation = output_annotations.get(output_name_, None)
+                artifact_config = (
+                    annotation.artifact_config
+                    if annotation and annotation.artifact_config is not None
+                    else ArtifactConfig()
+                )
+                artifact_config.model_name = (
+                    artifact_config.model_name or model_version.model.name
+                )
+                artifact_config.model_version = (
+                    artifact_config.model_version or model_version.name
+                )
+                artifact_config._pipeline_name = (
+                    self._deployment.pipeline_configuration.name
+                )
+                artifact_config._step_name = self._step_name
+                artifact_config.link_to_model(output_)
 
     def _run_step(
         self,
