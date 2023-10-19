018337367996bd1a2260c5a32916019d62f5323b
Baris Can Durak <bariscandurak@hotmail.com>
2023-10-19 17:45:55 +0200

    base zen model in use now

--- src/zenml/new_models/base/__init__.py
+++ src/zenml/new_models/base/__init__.py
@@ -12,6 +12,7 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 from zenml.new_models.base.base import (
+    BaseZenModel,
     BaseRequest,
     BaseResponseBody,
     BaseResponseMetadata,
