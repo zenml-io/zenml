8bb5e7d4dbfc59eb931799da8d19887e0aa729cf
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-11-03 09:26:17 +0100

    don't try to not install something that can't be installed

--- scripts/install-zenml-dev.sh
+++ scripts/install-zenml-dev.sh
@@ -31,7 +31,7 @@ install_integrations() {
     # figure out the python version
     python_version=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 
-    ignore_integrations="feast label_studio bentoml seldon kserve llama_index pycaret skypilot_aws skypilot_gcp skypilot_azure"
+    ignore_integrations="feast label_studio bentoml seldon kserve pycaret skypilot_aws skypilot_gcp skypilot_azure"
     # if python version is 3.11, exclude all integrations depending on kfp
     # because they are not yet compatible with python 3.11
     if [ "$python_version" = "3.11" ]; then
