2e19afdf2d20e8d39d6fbb2350f9896c284584e5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-11-03 09:13:09 +0100

    disable llama_index integration

--- tests/unit/integrations/llama_index/materializers/test_llama_index_document_materializer.py
+++ tests/unit/integrations/llama_index/materializers/test_llama_index_document_materializer.py
@@ -13,28 +13,28 @@
 #  permissions and limitations under the License.
 
 
-from tests.unit.test_general import _test_materializer
+# from tests.unit.test_general import _test_materializer
 
+# TODO: turn this back on when we are able to upgrade llama_index integration
+# def test_llama_index_document_materializer(clean_client):
+#     """Tests whether the steps work for the Llama Index Document
+#     materializer."""
+#     from langchain.docstore.document import Document as LCDocument
+#     from llama_index.readers.schema.base import Document
 
-def test_llama_index_document_materializer(clean_client):
-    """Tests whether the steps work for the Llama Index Document
-    materializer."""
-    from langchain.docstore.document import Document as LCDocument
-    from llama_index.readers.schema.base import Document
+#     from zenml.integrations.llama_index.materializers.document_materializer import (
+#         LlamaIndexDocumentMaterializer,
+#     )
 
-    from zenml.integrations.llama_index.materializers.document_materializer import (
-        LlamaIndexDocumentMaterializer,
-    )
+#     page_content = (
+#         "Axl, Aria and Blupus were very cold during the winter months."
+#     )
+#     langchain_document = _test_materializer(
+#         step_output=Document(text=page_content),
+#         materializer_class=LlamaIndexDocumentMaterializer,
+#         expected_metadata_size=2,
+#     )
 
-    page_content = (
-        "Axl, Aria and Blupus were very cold during the winter months."
-    )
-    langchain_document = _test_materializer(
-        step_output=Document(text=page_content),
-        materializer_class=LlamaIndexDocumentMaterializer,
-        expected_metadata_size=2,
-    )
-
-    assert langchain_document.get_type() == "Document"
-    assert langchain_document.text == page_content
-    assert isinstance(langchain_document.to_langchain_format(), LCDocument)
+#     assert langchain_document.get_type() == "Document"
+#     assert langchain_document.text == page_content
+#     assert isinstance(langchain_document.to_langchain_format(), LCDocument)
