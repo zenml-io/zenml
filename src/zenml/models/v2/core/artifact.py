0c72f61286f02aada8706648a371e9f1677f0690
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-11-25 20:35:08 +0100

    extend uri field in models

--- src/zenml/models/v2/core/artifact.py
+++ src/zenml/models/v2/core/artifact.py
@@ -19,7 +19,7 @@ from uuid import UUID
 from pydantic import BaseModel, Field
 
 from zenml.config.source import Source, convert_source_validator
-from zenml.constants import STR_FIELD_MAX_LENGTH
+from zenml.constants import STR_FIELD_MAX_LENGTH, TEXT_FIELD_MAX_LENGTH
 from zenml.enums import ArtifactType
 from zenml.logger import get_logger
 from zenml.models.tag_models import TagResponseModel
@@ -68,7 +68,7 @@ class ArtifactRequest(WorkspaceScopedRequest):
         default=None,
     )
     uri: str = Field(
-        title="URI of the artifact.", max_length=STR_FIELD_MAX_LENGTH
+        title="URI of the artifact.", max_length=TEXT_FIELD_MAX_LENGTH
     )
     materializer: Source = Field(
         title="Materializer class to use for this artifact.",
@@ -110,7 +110,7 @@ class ArtifactResponseBody(WorkspaceScopedResponseBody):
         max_length=STR_FIELD_MAX_LENGTH,
     )
     uri: str = Field(
-        title="URI of the artifact.", max_length=STR_FIELD_MAX_LENGTH
+        title="URI of the artifact.", max_length=TEXT_FIELD_MAX_LENGTH
     )
     type: ArtifactType = Field(title="Type of the artifact.")
 
