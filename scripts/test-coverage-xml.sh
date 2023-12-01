86e45d9a7c3ed6f6e47b5dc88fcc1864592192d3
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-12-01 10:42:36 +0100

    use branch coverage instead of line coverage

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -18,10 +18,10 @@ export EVIDENTLY_DISABLE_TELEMETRY=1
 
 # The '-vv' flag enables pytest-clarity output when tests fail.
 if [ -n "$1" ]; then
-    coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+    coverage run -m pytest $TEST_SRC --branch --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
 else
-    coverage run -m pytest tests/unit --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision
-    coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+    coverage run -m pytest tests/unit --branch --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision
+    coverage run -m pytest tests/integration --branch --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
 fi
 
 ./zen-test environment cleanup $TEST_ENVIRONMENT
