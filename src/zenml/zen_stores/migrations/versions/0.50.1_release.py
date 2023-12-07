e3a8bbe3b16b92ae9f29c3421e2858bf3863860d
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2023-12-07 12:05:38 +0100

    Prepare release 0.50.1

---
+++ src/zenml/zen_stores/migrations/versions/0.50.1_release.py
@@ -0,0 +1,24 @@
+"""Release [0.50.1].
+
+Revision ID: 0.50.1
+Revises: 8ed03137cacc
+Create Date: 2023-12-07 11:53:31.408085
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "0.50.1"
+down_revision = "8ed03137cacc"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade database schema and/or data, creating a new revision."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade database schema and/or data back to the previous revision."""
+    pass
