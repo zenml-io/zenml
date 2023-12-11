be6df822804fc4210b7998768d887a4b8768d061
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2023-12-11 22:09:02 +0100

    try original test script

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -3,12 +3,11 @@
 set -e
 set -x
 
-# Usage:
-# test-coverage-xml.sh [unit|integration] [environment]
-# Example to run all groups for unit tests in parallel:
-# test-coverage-xml.sh unit default
-
-TEST_TYPE=${1:-"unit"} # "unit" or "integration"
+# If only unittests are needed call
+# test-coverage-xml.sh unit
+# For only integration tests call
+# test-coverage-xml.sh integration
+TEST_SRC="tests/"${1:-""}
 TEST_ENVIRONMENT=${2:-"default"}
 
 export ZENML_DEBUG=1
@@ -17,61 +16,92 @@ export EVIDENTLY_DISABLE_TELEMETRY=1
 
 ./zen-test environment provision $TEST_ENVIRONMENT
 
-# Define the test source directory based on the test type
-if [ "$TEST_TYPE" == "unit" ]; then
-    TEST_SRC="tests/unit"
-    mv unit_test_durations .test_durations
-elif [ "$TEST_TYPE" == "integration" ]; then
-    TEST_SRC="tests/integration"
-    mv integration_test_durations .test_durations
+# The '-vv' flag enables pytest-clarity output when tests fail.
+if [ -n "$1" ]; then
+    coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
 else
-    echo "Invalid test type: $TEST_TYPE"
-    exit 1
-fi
-
-# Run the tests for all groups in parallel and store their PIDs
-pids=()
-group_statuses=()
-for GROUP in {1..4}; do
-    COVERAGE_FILE=".coverage.$TEST_TYPE.$GROUP"
-    coverage run -m pytest $TEST_SRC --durations 20 --splits 4 --group $GROUP &
-    pids+=($!)
-    group_statuses+=("pending")
-done
-
-# Wait for all background processes to finish and check their exit status
-failed_groups=()
-for i in "${!pids[@]}"; do
-    pid=${pids[$i]}
-    wait $pid
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        group_statuses[$i]="failed"
-        failed_groups+=($((i + 1))) # Store the group number (1-indexed)
-    else
-        group_statuses[$i]="passed"
-    fi
-done
-
-# Report the test results
-echo "Test summary:"
-for i in "${!group_statuses[@]}"; do
-    echo "Group $((i + 1)): ${group_statuses[$i]}"
-done
-
-# If there are any failed groups, do the cleanup and exit with code 1
-if [ ${#failed_groups[@]} -ne 0 ]; then
-    echo "The following test groups failed: ${failed_groups[*]}"
-    ./zen-test environment cleanup $TEST_ENVIRONMENT
-    exit 1
+    coverage run -m pytest tests/unit --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision
+    coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
 fi
 
 ./zen-test environment cleanup $TEST_ENVIRONMENT
 
-# Combine coverage data from all groups
-coverage combine $(ls .coverage.*)
+coverage combine
 coverage report --show-missing
 coverage xml
 
-# Clean up individual coverage files
-rm -f .coverage.*
+# set -e
+# set -x
+
+# # Usage:
+# # test-coverage-xml.sh [unit|integration] [environment]
+# # Example to run all groups for unit tests in parallel:
+# # test-coverage-xml.sh unit default
+
+# TEST_TYPE=${1:-"unit"} # "unit" or "integration"
+# TEST_ENVIRONMENT=${2:-"default"}
+
+# export ZENML_DEBUG=1
+# export ZENML_ANALYTICS_OPT_IN=false
+# export EVIDENTLY_DISABLE_TELEMETRY=1
+
+# ./zen-test environment provision $TEST_ENVIRONMENT
+
+# # Define the test source directory based on the test type
+# if [ "$TEST_TYPE" == "unit" ]; then
+#     TEST_SRC="tests/unit"
+#     mv unit_test_durations .test_durations
+# elif [ "$TEST_TYPE" == "integration" ]; then
+#     TEST_SRC="tests/integration"
+#     mv integration_test_durations .test_durations
+# else
+#     echo "Invalid test type: $TEST_TYPE"
+#     exit 1
+# fi
+
+# # Run the tests for all groups in parallel and store their PIDs
+# pids=()
+# group_statuses=()
+# for GROUP in {1..4}; do
+#     COVERAGE_FILE=".coverage.$TEST_TYPE.$GROUP"
+#     coverage run -m pytest $TEST_SRC --durations 20 --splits 4 --group $GROUP &
+#     pids+=($!)
+#     group_statuses+=("pending")
+# done
+
+# # Wait for all background processes to finish and check their exit status
+# failed_groups=()
+# for i in "${!pids[@]}"; do
+#     pid=${pids[$i]}
+#     wait $pid
+#     exit_code=$?
+#     if [ $exit_code -ne 0 ]; then
+#         group_statuses[$i]="failed"
+#         failed_groups+=($((i + 1))) # Store the group number (1-indexed)
+#     else
+#         group_statuses[$i]="passed"
+#     fi
+# done
+
+# # Report the test results
+# echo "Test summary:"
+# for i in "${!group_statuses[@]}"; do
+#     echo "Group $((i + 1)): ${group_statuses[$i]}"
+# done
+
+# # If there are any failed groups, do the cleanup and exit with code 1
+# if [ ${#failed_groups[@]} -ne 0 ]; then
+#     echo "The following test groups failed: ${failed_groups[*]}"
+#     ./zen-test environment cleanup $TEST_ENVIRONMENT
+#     exit 1
+# fi
+
+# ./zen-test environment cleanup $TEST_ENVIRONMENT
+
+# # Combine coverage data from all groups
+# coverage combine $(ls .coverage.*)
+# coverage report --show-missing
+# coverage xml
+
+# # Clean up individual coverage files
+# rm -f .coverage.*
