002996a2b7890396bb8760a1cdfaec3d0b92becf
Baris Can Durak <bariscandurak@hotmail.com>
2023-12-13 16:08:09 +0100

    tag models ready

--- src/zenml/models/v2/core/tag.py
+++ src/zenml/models/v2/core/tag.py
@@ -13,10 +13,104 @@
 #  permissions and limitations under the License.
 """Models representing tags."""
 
+import random
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from zenml.constants import STR_FIELD_MAX_LENGTH
+from zenml.enums import ColorVariants
+from zenml.models.v2.base.base import (
+    BaseRequest,
+    BaseResponse,
+    BaseResponseBody,
+    BaseResponseMetadata,
+)
+from zenml.models.v2.base.filter import BaseFilter
+
 # ------------------ Request Model ------------------
+
+
+class TagRequest(BaseRequest):
+    """Request model for tags."""
+
+    name: str = Field(
+        description="The unique title of the tag.",
+        max_length=STR_FIELD_MAX_LENGTH,
+    )
+    color: ColorVariants = Field(
+        description="The color variant assigned to the tag.",
+        default_factory=lambda: random.choice(list(ColorVariants)),
+    )
+
 
 # ------------------ Update Model ------------------
 
+
+class TagUpdate(BaseModel):
+    """Update model for tags."""
+
+    name: Optional[str]
+    color: Optional[ColorVariants]
+
+
 # ------------------ Response Model ------------------
 
+
+class TagResponseBody(BaseResponseBody):
+    """Response body for tags."""
+
+    color: ColorVariants = Field(
+        description="The color variant assigned to the tag.",
+        default_factory=lambda: random.choice(list(ColorVariants)),
+    )
+    tagged_count: int = Field(
+        description="The count of resources tagged with this tag."
+    )
+
+
+class TagResponse(BaseResponse[TagResponseBody, BaseResponseMetadata]):
+    """Response model for tags."""
+
+    name: str = Field(
+        description="The unique title of the tag.",
+        max_length=STR_FIELD_MAX_LENGTH,
+    )
+
+    def get_hydrated_version(self) -> "TagResponse":
+        """Get the hydrated version of this tag.
+
+        Returns:
+            an instance of the same entity with the metadata field attached.
+        """
+        from zenml.client import Client
+
+        return Client().zen_store.get_tag(self.id)
+
+    @property
+    def color(self) -> ColorVariants:
+        """The `color` property.
+
+        Returns:
+            the value of the property.
+        """
+        return self.get_body().color
+
+    @property
+    def tagged_count(self) -> int:
+        """The `tagged_count` property.
+
+        Returns:
+            the value of the property.
+        """
+        return self.get_body().tagged_count
+
+
 # ------------------ Filter Model ------------------
+
+
+class TagFilter(BaseFilter):
+    """Model to enable advanced filtering of all tags."""
+
+    name: Optional[str]
+    color: Optional[ColorVariants]
