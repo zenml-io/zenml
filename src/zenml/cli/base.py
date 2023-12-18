344bca464013fc323987e8c30796b6b4b43336d0
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-12-18 10:52:22 +0100

    proper clean quickstart

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -76,11 +76,11 @@ ZENML_PROJECT_TEMPLATES = dict(
     ),
     starter=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-starter",
-        github_tag="0.53.0",
+        github_tag="0.53.0",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
     nlp=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-nlp",
-        github_tag="0.45.0",
+        github_tag="0.45.0",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
 )
 
