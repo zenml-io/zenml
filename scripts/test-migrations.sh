a103dc3c57ef346b83d0dfe263b687f76c7fb5c7
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-12-20 16:45:16 +0100

    Update copier command in test-migrations.sh

--- scripts/test-migrations.sh
+++ scripts/test-migrations.sh
@@ -16,7 +16,7 @@ function run_tests_for_version() {
     # hardcoded to 0.43.0 since this is the latest template-starter repo
     # release tag
     git clone -b "release/0.43.0" https://github.com/zenml-io/template-starter
-    copier copy template-starter/ test_starter --trust --defaults
+    copier copy --trust -l --trust -r release/0.43.0 https://github.com/zenml-io/template-starter.git .
     cd test_starter
 
     export ZENML_ANALYTICS_OPT_IN=false
