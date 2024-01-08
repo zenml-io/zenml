8b628ceff6d199c4363971b7530736c436933d4f
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-01-08 09:26:16 +0100

    split mariadb from mysql db migration testing

---
+++ scripts/test-migrations-mysql.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+DB="sqlite"
+DB_STARTUP_DELAY=30 # Time in seconds to wait for the database container to start
+
+if [ -z "$1" ]; then
+  echo "No argument passed, using default: $DB"
+else
+  DB="$1"
+fi
+
+function run_tests_for_version() {
+    set -e  # Exit immediately if a command exits with a non-zero status
+    local VERSION=$1
+
+    echo "===== Testing version $VERSION ====="
+    # Initialize zenml with the appropriate template
+    # hardcoded to 0.43.0 since this is the latest template-starter repo
+    # release tag
+    copier copy -l --trust -r release/0.43.0 https://github.com/zenml-io/template-starter.git test_starter
+    cd test_starter
+
+    export ZENML_ANALYTICS_OPT_IN=false
+    export ZENML_DEBUG=true
+
+    echo "===== Installing sklearn integration ====="
+    zenml integration install sklearn -y
+
+    echo "===== Running starter template pipeline ====="
+    python3 run.py
+    # Add additional CLI tests here
+    zenml version
+
+    # Confirm DB works and is accessible
+    zenml pipeline runs list
+
+    cd ..
+    rm -rf test_starter template-starter
+    echo "===== Finished testing version $VERSION ====="
+}
+
+if [ "$1" == "mysql" ]; then
+    echo "===== Testing MySQL ====="
+    # run a mysql instance in docker
+    docker run --name mysql -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password mysql:latest
+    # mysql takes a while to start up
+    sleep $DB_STARTUP_DELAY
+elif [ "$1" == "mariadb" ]; then
+    echo "===== Testing MariaDB ====="
+    # run a mariadb instance in docker
+    docker run --name mariadb -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password mariadb:10.6
+    # mariadb takes a while to start up
+    sleep $DB_STARTUP_DELAY
+fi
+
+# List of versions to test
+VERSIONS=("0.40.0" "0.40.3" "0.41.0" "0.43.0" "0.44.1" "0.44.3" "0.45.2" "0.45.3" "0.45.4" "0.45.5" "0.45.6" "0.46.0" "0.47.0" "0.50.0" "0.51.0" "0.52.0")
+
+# Start completely fresh
+rm -rf ~/.config/zenml
+
+for VERSION in "${VERSIONS[@]}"
+do
+    set -e  # Exit immediately if a command exits with a non-zero status
+    # Create a new virtual environment
+    python3 -m venv ".venv-$VERSION"
+    source ".venv-$VERSION/bin/activate"
+
+    # Install the specific version
+    pip3 install -U pip setuptools wheel
+    pip3 install "zenml[templates,server]==$VERSION"
+    # handles unpinned sqlmodel dependency in older versions
+    pip3 install "sqlmodel==0.0.8" "bcrypt==4.0.1"
+
+    # Get the major and minor version of Python
+    PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+
+    # Check if the Python version is 3.9 and VERSION is > 0.47.0
+    if [[ "$PYTHON_VERSION" == "3.9" ]]; then
+        case "$VERSION" in
+            "0.47.0"|"0.50.0"|"0.51.0"|"0.52.0")
+                pip3 install importlib_metadata
+                ;;
+        esac
+    fi
+
+
+    if [ "$1" == "mysql" ]; then
+        zenml connect --url mysql://127.0.0.1/zenml --username root --password password
+    elif [ "$1" == "mariadb" ]; then
+        zenml connect --url mysql://127.0.0.1/zenml --username root --password password
+    fi
+
+    # Run the tests for this version
+    run_tests_for_version $VERSION
+
+    if [ "$1" == "mysql" ]; then
+        zenml disconnect
+        sleep 5
+    fi
+
+    deactivate
+done
+
+
+# Test the most recent migration with MySQL
+set -e
+python3 -m venv ".venv-current-branch"
+source ".venv-current-branch/bin/activate"
+
+pip3 install -U pip setuptools wheel
+pip3 install -e ".[templates,server]"
+pip3 install importlib_metadata
+
+if [ "$1" == "mysql" ]; then
+    zenml connect --url mysql://127.0.0.1/zenml --username root --password password
+elif [ "$1" == "mariadb" ]; then
+    zenml connect --url mysql://127.0.0.1/zenml --username root --password password
+fi
+
+run_tests_for_version current_branch_mysql
+
+if [ "$1" == "mysql" ]; then
+    zenml disconnect
+    docker rm -f mysql
+elif [ "$1" == "mariadb" ]; then
+    zenml disconnect
+    docker rm -f mariadb
+fi
+
+deactivate
+
