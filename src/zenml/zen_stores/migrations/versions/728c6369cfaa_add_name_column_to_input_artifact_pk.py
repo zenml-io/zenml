f89d55c506ab2cfb6a658c5539a71b0e41e1d7ef
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-01-08 10:43:02 +0100

    mypy fix

--- src/zenml/zen_stores/migrations/versions/728c6369cfaa_add_name_column_to_input_artifact_pk.py
+++ src/zenml/zen_stores/migrations/versions/728c6369cfaa_add_name_column_to_input_artifact_pk.py
@@ -128,16 +128,21 @@ def _disable_primary_key_requirement_if_necessary() -> None:
     """Adjusts settings based on database engine requirements."""
     engine = op.get_bind().engine
     engine_name = engine.name.lower()
-    server_version_info = engine.dialect.server_version_info
 
-    if engine_name == "mysql" and server_version_info >= (8, 0, 13):
-        potential_session_var = engine.execute(
-            text('SHOW SESSION VARIABLES LIKE "sql_require_primary_key";')
-        ).fetchone()
-        if potential_session_var and potential_session_var[1] == "ON":
-            # Temporarily disable this MySQL setting
-            # for primary key modification
-            op.execute("SET SESSION sql_require_primary_key = 0;")
+    if engine_name == "mysql":
+        server_version_info = engine.dialect.server_version_info
+        if server_version_info is not None and server_version_info >= (
+            8,
+            0,
+            13,
+        ):
+            potential_session_var = engine.execute(
+                text('SHOW SESSION VARIABLES LIKE "sql_require_primary_key";')
+            ).fetchone()
+            if potential_session_var and potential_session_var[1] == "ON":
+                # Temporarily disable this MySQL setting
+                # for primary key modification
+                op.execute("SET SESSION sql_require_primary_key = 0;")
     elif engine_name == "mariadb":
         # mariadb doesn't support session-based scoping for this setting
         op.execute("SET GLOBAL innodb_force_primary_key = 0;")
