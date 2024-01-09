1f062259005d39ebd67c90415e1e08eb1248e604
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-01-09 15:00:56 +0100

    use zenml init

--- scripts/test-migrations-mysql.sh
+++ scripts/test-migrations-mysql.sh
@@ -17,7 +17,8 @@ function run_tests_for_version() {
     # Initialize zenml with the appropriate template
     # hardcoded to 0.43.0 since this is the latest template-starter repo
     # release tag
-    copier copy -l --trust -r release/0.43.0 https://github.com/zenml-io/template-starter.git test_starter
+    mkdir test_starter
+    zenml init --template starter --path test_starter --template-with-defaults
     cd test_starter
 
     export ZENML_ANALYTICS_OPT_IN=false
