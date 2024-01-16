1fdb9642545ea2ea35e3a6dfad5d72e7e57a03c9
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2024-01-16 16:11:38 +0100

    Refactor test coverage script

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -10,8 +10,8 @@ set -x
 # To store durations, add a fourth argument 'store-durations'
 TEST_SRC="tests/"${1:-""}
 TEST_ENVIRONMENT=${2:-"default"}
-TEST_GROUP=${3:-"1"}
-TEST_SPLITS=${4:-"1"}
+TEST_SPLITS=${3:-"1"}
+TEST_GROUP=${4:-"1"}
 STORE_DURATIONS=${5:-""}
 
 export ZENML_DEBUG=1
@@ -23,17 +23,17 @@ export EVIDENTLY_DISABLE_TELEMETRY=1
 # The '-vv' flag enables pytest-clarity output when tests fail.
 if [ -n "$1" ]; then
     if [ "$STORE_DURATIONS" == "store-durations" ]; then
-        coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --store-durations --durations-path=full_test_durations
+        coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --store-durations --durations-path=test_durations
     else
-        coverage run -m pytest $TEST_SRC --color=yes -vv --durations-path=full_test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+        coverage run -m pytest $TEST_SRC --color=yes -vv --durations-path=test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
     fi
 else
     if [ "$STORE_DURATIONS" == "store-durations" ]; then
         coverage run -m pytest tests/unit --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --store-durations --durations-path=unit_test_durations
-        coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --store-durations --durations-path=integration_test_durations
+        coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --store-durations --durations-path=test_durations
     else
-        coverage run -m pytest tests/unit --color=yes -vv --durations-path=unit_test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision
-        coverage run -m pytest tests/integration --color=yes -vv integration_test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+        coverage run -m pytest tests/unit --color=yes -vv --durations-path=test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision
+        coverage run -m pytest tests/integration --color=yes -vv --durations-path=test_durations --splits=$TEST_SPLITS --group=$TEST_GROUP --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
     fi
 fi
 
