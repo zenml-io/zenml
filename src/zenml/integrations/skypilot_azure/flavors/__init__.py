36424ad49b4a7531e0413669228fc42d6d5bd36a
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2024-01-22 16:38:09 +0100

    Separate `skypilot` flavors into different folders (#2332)
    * separate skypilot flavors into different folders
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>
    
    * Update Skypilot integration package
    
    * Update import path for Skypilot GCP orchestrator flavor
    
    * Imported SKYPILOT_AZURE_ORCHESTRATOR_FLAVOR from zenml.integrations.skypilot_azure.
    
    ---------
    
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>

--- src/zenml/integrations/skypilot_azure/flavors/__init__.py
+++ src/zenml/integrations/skypilot_azure/flavors/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (c) ZenML GmbH 2023. All Rights Reserved.
+#  Copyright (c) ZenML GmbH 2024. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,11 +11,16 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Initialization of the Skypilot ZenML orchestrators."""
+"""Skypilot integration flavor for Skypilot Azure orchestrator."""
 
-from zenml.integrations.skypilot.orchestrators.skypilot_base_vm_orchestrator import (  # noqa
-    SkypilotBaseOrchestrator,
+from zenml.integrations.skypilot_azure.flavors.skypilot_orchestrator_azure_vm_flavor import (
+    SkypilotAzureOrchestratorConfig,
+    SkypilotAzureOrchestratorFlavor,
+    SkypilotAzureOrchestratorSettings,
 )
+
 __all__ = [
-    "SkypilotBaseOrchestrator",
+    "SkypilotAzureOrchestratorConfig",
+    "SkypilotAzureOrchestratorFlavor",
+    "SkypilotAzureOrchestratorSettings",
 ]
