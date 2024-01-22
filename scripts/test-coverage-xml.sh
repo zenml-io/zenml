163dce6ff1748993ab464c31a7242e0f4c071349
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-01-22 12:18:31 +0100

    Add repeating tests and instafail

--- scripts/test-coverage-xml.sh
+++ scripts/test-coverage-xml.sh
@@ -17,11 +17,13 @@ export EVIDENTLY_DISABLE_TELEMETRY=1
 ./zen-test environment provision $TEST_ENVIRONMENT
 
 # The '-vv' flag enables pytest-clarity output when tests fail.
+# Reruns a failing test 3 times with a 5 second delay.
+# Shows errors instantly in logs when test fails.
 if [ -n "$1" ]; then
-    coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+    coverage run -m pytest $TEST_SRC --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --reruns 3 --reruns-delay 5 --instafail
 else
-    coverage run -m pytest tests/unit --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision
-    coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker
+    coverage run -m pytest tests/unit --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --reruns 5 --reruns-delay 5 --instafail
+    coverage run -m pytest tests/integration --color=yes -vv --environment $TEST_ENVIRONMENT --no-provision --cleanup-docker --reruns 3 --reruns-delay 5 --instafail
 fi
 
 ./zen-test environment cleanup $TEST_ENVIRONMENT
