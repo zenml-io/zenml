9d20eda4b61e691b9e22f8d5e73fd3f4fca658d0
Alexej Penner <thealexejpenner@gmail.com>
2024-01-29 17:51:00 +0100

    Another tiny commit to test webhooks

--- src/zenml/event_sources/schedules/base_schedule_event_plugin.py
+++ src/zenml/event_sources/schedules/base_schedule_event_plugin.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Abstract BaseEvent class that all Event implementations must implement."""
+"""Abstract Schedule class that all Schedule event sources must implement."""
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, ClassVar, Type
 
