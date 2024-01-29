1139d46ab9155bccc5d067b7c49671eb2b18e983
Alexej Penner <thealexejpenner@gmail.com>
2024-01-29 17:13:45 +0100

    Fixed docstring

--- src/zenml/plugins/plugin_flavor_registry.py
+++ src/zenml/plugins/plugin_flavor_registry.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Registry all event source configurations."""
+"""Registry for all plugins."""
 from typing import TYPE_CHECKING, Dict, List, Optional, Type
 
 from pydantic import BaseModel
