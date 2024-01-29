d47d1501fd6f521e878679c4d571d7127b95936f
Alexej Penner <thealexejpenner@gmail.com>
2024-01-29 17:33:35 +0100

    Yet another test commit

--- src/zenml/event_sources/base_event_source_plugin.py
+++ src/zenml/event_sources/base_event_source_plugin.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Base implementation of the event source configuration."""
+"""Base implementation for event sources."""
 import json
 from abc import ABC, abstractmethod
 from typing import (
