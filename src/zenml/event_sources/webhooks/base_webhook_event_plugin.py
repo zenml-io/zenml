3e97841acc85ac544c564931d6ce77463840d820
Alexej Penner <thealexejpenner@gmail.com>
2024-01-30 17:21:07 +0100

    Trigger commit again

--- src/zenml/event_sources/webhooks/base_webhook_event_plugin.py
+++ src/zenml/event_sources/webhooks/base_webhook_event_plugin.py
@@ -13,7 +13,8 @@
 #  permissions and limitations under the License.
 """Abstract BaseEvent class that all Event implementations must implement."""
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, ClassVar, Type
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, Type
+from uuid import UUID
 
 from zenml.enums import PluginSubType
 from zenml.event_sources.base_event_source_plugin import (
@@ -79,6 +80,85 @@ class BaseWebhookEventSourcePlugin(BaseEventSourcePlugin, ABC):
         )
         # created_event_source.secret_value = ...
         return created_event_source
+
+    @staticmethod
+    @abstractmethod
+    def is_valid_signature(
+        body: bytes, secret_token: str, signature_header: str
+    ) -> bool:
+        """Verify that the payload was sent from GitHub by validating SHA256.
+
+        Raise and return 403 if not authorized.
+
+        Args:
+            body: original request body to verify (request.body())
+            secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+            signature_header: header received from GitHub (x-hub-signature-256)
+        """
+
+    def get_matching_triggers_for_event(
+        self,
+        incoming_event: Dict[str, Any],
+        raw_body: bytes,
+        signature_header: str
+    ) -> List[UUID]:
+        """Process the incoming event and forward with trigger_ids to event hub.
+
+        Args:
+            incoming_event: The inbound event.
+            raw_body: Raw body of the incoming request
+            signature_header: Signature header sent to the webhook.
+        """
+        event = self._interpret_event(incoming_event)
+
+        # narrow down to all sources that relate to the repo that
+        #  is responsible for the event
+        event_sources = self._get_all_relevant_event_sources(
+            event=event,
+        )
+
+        self.remove_event_sources_with_wrong_signature(
+            raw_event_body=raw_body,
+            event_sources=event_sources,
+            signature_header=signature_header,
+        )
+
+        # get all triggers that have matching event filters configured
+        if event_sources:
+            trigger_ids = self._get_matching_triggers(
+                event_sources=event_sources, event=event
+            )
+
+            # TODO: Forward the event together with the list of trigger ids
+            #  over to the EventHub
+            logger.info(
+                "An event came in, the following triggers will be"
+                " used: %s ",
+                trigger_ids,
+            )
+            return trigger_ids
+
+    def remove_event_sources_with_wrong_signature(
+        self,
+        event_sources: List[EventSourceResponse],
+        raw_event_body: bytes,
+        signature_header: str,
+    ):
+        """Removes all event_sources with mismatching signature.
+
+        Args:
+            event_sources: List of theoretically matching event sources.
+            raw_event_body: The Body received by the webhook endpoint.
+            signature_header: The signature header
+        """
+        for es in event_sources[:]:  # TO make sure we iterate over a list copy
+            if not self.is_valid_signature(
+                body=raw_event_body,
+                secret_token="asdf",  # TODO: Replace with actual secret
+                signature_header=signature_header,
+            ):
+                logger.info("Request signatures didn't match!")
+                event_sources.remove(es)
 
 
 # -------------------- Flavors ----------------------------------
