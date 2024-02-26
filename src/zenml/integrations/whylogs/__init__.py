c741d8e8a4e9dcf3b263c40fe9f4f0944106126f
Stefan Nica <stefan@zenml.io>
2024-02-26 13:18:39 +0100

    Revert whylogs dependencies change

--- src/zenml/integrations/whylogs/__init__.py
+++ src/zenml/integrations/whylogs/__init__.py
@@ -27,7 +27,7 @@ class WhylogsIntegration(Integration):
     """Definition of [whylogs](https://github.com/whylabs/whylogs) integration for ZenML."""
 
     NAME = WHYLOGS
-    REQUIREMENTS = ["whylogs[viz,whylabs]<1.1"]
+    REQUIREMENTS = ["whylogs[viz]~=1.0.5", "whylogs[whylabs]~=1.0.5"]
 
     @classmethod
     def activate(cls) -> None:
