e2732a570c9820db1794b603758895e0236ad51a
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-02-26 18:02:26 +0100

    try fixing import

--- tests/integration/examples/utils.py
+++ tests/integration/examples/utils.py
@@ -14,6 +14,7 @@
 import logging
 import os
 import shutil
+import sys
 import time
 from contextlib import contextmanager
 from datetime import datetime
@@ -68,6 +69,7 @@ class IntegrationTestExample:
         Raises:
             RuntimeError: If running the example fails.
         """
+        sys.path.insert(0, ".")
         run = import_module("run")
         run.main(*args)
 
