b02c2ba56356d134c525f4295b4c2c3e31af2e57
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-02-28 11:36:24 +0100

    zenml happens first

--- scripts/install-zenml-dev.sh
+++ scripts/install-zenml-dev.sh
@@ -6,7 +6,7 @@ parse_args () {
     while [ $# -gt 0 ]; do
         case $1 in
             -i|--integrations)
-                INTEGRATIONS="yes"
+                INTEGRATIONS="$2"
                 shift # past argument
                 shift # past value
                 ;;
@@ -22,6 +22,8 @@ parse_args () {
 }
 
 install_zenml() {
+    # install ZenML in editable mode
+
     touch zenml_requirements.txt
     echo "-e .[server,templates,terraform,secrets-aws,secrets-gcp,secrets-azure,secrets-hashicorp,s3fs,gcsfs,adlfs,dev,mlstacks]" >> zenml_requirements.txt
 
@@ -31,24 +33,32 @@ install_zenml() {
     pip install -r zenml_requirements-compiled.txt
     rm zenml_requirements.txt
     rm zenml_requirements.in
-    rm zenml_requirements-compiled.txt # Ensure this file is also removed after installation
 }
 
 install_integrations() {
+
+    # figure out the python version
     python_version=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 
     ignore_integrations="feast label_studio bentoml seldon kserve pycaret skypilot_aws skypilot_gcp skypilot_azure"
+    # if python version is 3.11, exclude all integrations depending on kfp
+    # because they are not yet compatible with python 3.11
     if [ "$python_version" = "3.11" ]; then
         ignore_integrations="$ignore_integrations kubeflow tekton gcp"
     fi
 
+    # turn the ignore integrations into a list of --ignore-integration args
     ignore_integrations_args=""
     for integration in $ignore_integrations; do
         ignore_integrations_args="$ignore_integrations_args --ignore-integration $integration"
     done
 
-    zenml integration export-requirements --output-file integration-requirements.txt $ignore_integrations_args
+    # install basic ZenML integrations
+    zenml integration export-requirements \
+        --output-file integration-requirements.txt \
+        $ignore_integrations_args
 
+    # pin pyyaml>=6.0.1
     echo "" >> integration-requirements.txt
     echo "pyyaml>=6.0.1" >> integration-requirements.txt
     echo "pyopenssl" >> integration-requirements.txt
@@ -65,6 +75,7 @@ install_integrations() {
     rm integration-requirements-compiled.txt
 }
 
+
 set -x
 set -e
 
@@ -75,9 +86,9 @@ parse_args "$@"
 
 python -m pip install --upgrade pip setuptools wheel uv
 
-# Conditionally install ZenML or ZenML with integrations
-if [ "$INTEGRATIONS" = "yes" ]; then
+install_zenml
+
+# install integrations, if requested
+if [ "$INTEGRATIONS" = yes ]; then
     install_integrations
-else
-    install_zenml
 fi
