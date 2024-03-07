bd166581c171fe252299c5653d0c7e2be2e2cd6c
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-03-07 10:50:23 +0100

    no `clean_client` in models

--- tests/integration/functional/models/test_artifact.py
+++ tests/integration/functional/models/test_artifact.py
@@ -13,7 +13,7 @@
 #  permissions and limitations under the License.
 """Integration tests for artifact models."""
 
-from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import pytest
 from typing_extensions import Annotated
@@ -25,6 +25,7 @@ from tests.integration.functional.conftest import (
 from zenml import step
 from zenml.artifacts.artifact_config import ArtifactConfig
 from zenml.artifacts.utils import load_artifact_visualization
+from zenml.client import Client
 from zenml.enums import ExecutionStatus
 from zenml.exceptions import EntityExistsError
 from zenml.models import (
@@ -34,11 +35,8 @@ from zenml.models import (
 )
 from zenml.pipelines.base_pipeline import BasePipeline
 
-if TYPE_CHECKING:
-    from zenml.client import Client
 
-
-def test_default_artifact_name(clean_client: "Client", one_step_pipeline):
+def test_default_artifact_name(one_step_pipeline):
     """Integration test for default artifact names."""
     step_ = constant_int_output_test_step()
     pipe: BasePipeline = one_step_pipeline(step_)
@@ -54,7 +52,7 @@ def custom_artifact_name_test_step() -> Annotated[int, "number_7"]:
     return 7
 
 
-def test_custom_artifact_name(clean_client: "Client", one_step_pipeline):
+def test_custom_artifact_name(one_step_pipeline):
     """Integration test for custom artifact names."""
     pipe: BasePipeline = one_step_pipeline(custom_artifact_name_test_step)
     pipe.run()
@@ -69,9 +67,7 @@ def multi_output_test_step() -> Tuple[Annotated[int, "number_7"], int]:
     return 7, 42
 
 
-def test_multi_output_artifact_names(
-    clean_client: "Client", one_step_pipeline
-):
+def test_multi_output_artifact_names(one_step_pipeline):
     """Integration test for multi-output artifact names."""
     pipe: BasePipeline = one_step_pipeline(multi_output_test_step)
     pipe.run()
@@ -102,7 +98,7 @@ def manual_int_version_step() -> (
     return 1
 
 
-def test_artifact_versioning(clean_client: "Client", one_step_pipeline):
+def test_artifact_versioning(one_step_pipeline):
     """Test artifact versioning."""
     # First auto-incremented artifact version starts at 1
     pipe: BasePipeline = one_step_pipeline(auto_versioned_step)
@@ -142,9 +138,7 @@ def duplicate_int_version_step() -> (
     return 1
 
 
-def test_artifact_versioning_duplication(
-    clean_client: "Client", one_step_pipeline
-):
+def test_artifact_versioning_duplication(one_step_pipeline):
     """Test that duplicated artifact versions are not allowed."""
     pipe: BasePipeline = one_step_pipeline(auto_versioned_step)
     pipe.run(enable_cache=False)
@@ -175,7 +169,7 @@ def tagged_artifact_step() -> (
     return 7
 
 
-def test_artifact_tagging(clean_client: "Client", one_step_pipeline):
+def test_artifact_tagging(one_step_pipeline):
     """Test artifact tagging."""
 
     pipe: BasePipeline = one_step_pipeline(tagged_artifact_step)
@@ -184,7 +178,7 @@ def test_artifact_tagging(clean_client: "Client", one_step_pipeline):
     assert {t.name for t in artifact.tags} == {"cat", "grumpy"}
 
 
-def test_artifact_step_run_linkage(clean_client: "Client", one_step_pipeline):
+def test_artifact_step_run_linkage(one_step_pipeline):
     """Integration test for `artifact.step` and `artifact.run` properties."""
     step_ = constant_int_output_test_step()
     pipe: BasePipeline = one_step_pipeline(step_)
@@ -207,137 +201,135 @@ def test_artifact_step_run_linkage(clean_client: "Client", one_step_pipeline):
     assert artifact_2.run == pipeline_run
 
 
-def test_disabling_artifact_visualization(
-    clean_client: "Client", one_step_pipeline
-):
+def test_disabling_artifact_visualization(one_step_pipeline):
     """Test that disabling artifact visualization works."""
-
+    client = Client()
     # By default, artifact visualization should be enabled
     step_ = visualizable_step()
     pipe: BasePipeline = one_step_pipeline(step_)
     pipe.configure(enable_cache=False)
     pipe.run(unlisted=True)
-    _assert_visualization_enabled(clean_client)
+    _assert_visualization_enabled()
 
     # Test disabling artifact visualization on pipeline level
     pipe.configure(enable_artifact_visualization=False)
     pipe.run(unlisted=True)
-    _assert_visualization_disabled(clean_client)
+    _assert_visualization_disabled()
 
     pipe.configure(enable_artifact_visualization=True)
     pipe.run(unlisted=True)
-    _assert_visualization_enabled(clean_client)
+    _assert_visualization_enabled()
 
     # Test disabling artifact visualization on step level
     # This should override the pipeline level setting
     step_.configure(enable_artifact_visualization=False)
     pipe.run(unlisted=True)
-    _assert_visualization_disabled(clean_client)
+    _assert_visualization_disabled()
 
     step_.configure(enable_artifact_visualization=True)
     pipe.run(unlisted=True)
-    _assert_visualization_enabled(clean_client)
+    _assert_visualization_enabled()
 
     # Test disabling artifact visualization on run level
     # This should override both the pipeline and step level setting
     pipe.run(unlisted=True, enable_artifact_visualization=False)
-    _assert_visualization_disabled(clean_client)
+    _assert_visualization_disabled()
 
     pipe.configure(enable_artifact_visualization=False)
     step_.configure(enable_artifact_visualization=False)
     pipe.run(unlisted=True, enable_artifact_visualization=True)
-    _assert_visualization_enabled(clean_client)
+    _assert_visualization_enabled()
 
 
-def test_load_artifact_visualization(clean_client, one_step_pipeline):
+def test_load_artifact_visualization(one_step_pipeline):
     """Integration test for loading artifact visualizations."""
+    client = Client()
     step_ = visualizable_step()
     pipe: BasePipeline = one_step_pipeline(step_)
     pipe.configure(enable_cache=False)
     pipe.run(unlisted=True)
 
-    artifact = _get_output_of_last_run(clean_client)
+    artifact = _get_output_of_last_run()
     assert artifact.visualizations
     for i in range(len(artifact.visualizations)):
         load_artifact_visualization(
-            artifact=artifact, index=i, zen_store=clean_client.zen_store
+            artifact=artifact, index=i, zen_store=client.zen_store
         )
 
 
-def test_disabling_artifact_metadata(clean_client, one_step_pipeline):
+def test_disabling_artifact_metadata(one_step_pipeline):
     """Test that disabling artifact metadata works."""
+    client = Client()
 
     # By default, artifact metadata should be enabled
     step_ = visualizable_step()
     pipe: BasePipeline = one_step_pipeline(step_)
     pipe.configure(enable_cache=False)
     pipe.run(unlisted=True)
-    _assert_metadata_enabled(clean_client)
+    _assert_metadata_enabled()
 
     # Test disabling artifact metadata on pipeline level
     pipe.configure(enable_artifact_metadata=False)
     pipe.run(unlisted=True)
-    _assert_metadata_disabled(clean_client)
+    _assert_metadata_disabled()
 
     pipe.configure(enable_artifact_metadata=True)
     pipe.run(unlisted=True)
-    _assert_metadata_enabled(clean_client)
+    _assert_metadata_enabled()
 
     # Test disabling artifact metadata on step level
     # This should override the pipeline level setting
     step_.configure(enable_artifact_metadata=False)
     pipe.run(unlisted=True)
-    _assert_metadata_disabled(clean_client)
+    _assert_metadata_disabled()
 
     step_.configure(enable_artifact_metadata=True)
     pipe.run(unlisted=True)
-    _assert_metadata_enabled(clean_client)
+    _assert_metadata_enabled()
 
     # Test disabling artifact metadata on run level
     # This should override both the pipeline and step level setting
     pipe.run(unlisted=True, enable_artifact_metadata=False)
-    _assert_metadata_disabled(clean_client)
+    _assert_metadata_disabled()
 
     pipe.configure(enable_artifact_metadata=False)
     step_.configure(enable_artifact_metadata=False)
     pipe.run(unlisted=True, enable_artifact_metadata=True)
-    _assert_metadata_enabled(clean_client)
+    _assert_metadata_enabled()
 
 
-def _get_output_of_last_run(clean_client: "Client") -> ArtifactVersionResponse:
+def _get_output_of_last_run() -> ArtifactVersionResponse:
     """Get the output of the last run."""
-    return list(clean_client.list_pipeline_runs()[0].steps.values())[0].output
+    return list(Client().list_pipeline_runs()[0].steps.values())[0].output
 
 
-def _get_visualizations_of_last_run(
-    clean_client: "Client",
-) -> Optional[List[ArtifactVisualizationResponse]]:
+def _get_visualizations_of_last_run() -> (
+    Optional[List[ArtifactVisualizationResponse]]
+):
     """Get the artifact visualizations of the last run."""
-    return _get_output_of_last_run(clean_client).visualizations
+    return _get_output_of_last_run().visualizations
 
 
-def _get_metadata_of_last_run(
-    clean_client: "Client",
-) -> Dict[str, "RunMetadataResponse"]:
+def _get_metadata_of_last_run() -> Dict[str, "RunMetadataResponse"]:
     """Get the artifact metadata of the last run."""
-    return _get_output_of_last_run(clean_client).run_metadata
+    return _get_output_of_last_run().run_metadata
 
 
-def _assert_visualization_enabled(clean_client: "Client"):
+def _assert_visualization_enabled():
     """Assert that artifact visualization was enabled in the last run."""
-    assert _get_visualizations_of_last_run(clean_client)
+    assert _get_visualizations_of_last_run()
 
 
-def _assert_visualization_disabled(clean_client: "Client"):
+def _assert_visualization_disabled():
     """Assert that artifact visualization was disabled in the last run."""
-    assert not _get_visualizations_of_last_run(clean_client)
+    assert not _get_visualizations_of_last_run()
 
 
-def _assert_metadata_enabled(clean_client: "Client"):
+def _assert_metadata_enabled():
     """Assert that artifact metadata was enabled in the last run."""
-    assert _get_metadata_of_last_run(clean_client)
+    assert _get_metadata_of_last_run()
 
 
-def _assert_metadata_disabled(clean_client: "Client"):
+def _assert_metadata_disabled():
     """Assert that artifact metadata was disabled in the last run."""
-    assert not _get_metadata_of_last_run(clean_client)
+    assert not _get_metadata_of_last_run()
