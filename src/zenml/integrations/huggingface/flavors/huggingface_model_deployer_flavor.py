f7daf263f6e100fa9a7136c22606dd0fceda8ba9
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2024-03-07 21:57:05 +0530

    Fix lint issue

--- src/zenml/integrations/huggingface/flavors/huggingface_model_deployer_flavor.py
+++ src/zenml/integrations/huggingface/flavors/huggingface_model_deployer_flavor.py
@@ -12,6 +12,7 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 """Hugging Face model deployer flavor."""
+
 from typing import TYPE_CHECKING, Any, Dict, Optional, Type
 
 from pydantic import BaseModel
