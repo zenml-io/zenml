2ba80a60a4a027dab52a5aac1170921482e56fe8
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-03-18 11:43:48 +0800

    Update template version

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -85,7 +85,7 @@ ZENML_PROJECT_TEMPLATES = dict(
     ),
     llm_finetuning=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-llm-finetuning",
-        github_tag="2024.03.14",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
+        github_tag="2024.03.18",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
 )
 
