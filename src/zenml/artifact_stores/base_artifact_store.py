4ac96faed6e950bfc150115f44a9b85d5be3e45c
Baris Can Durak <bariscandurak@hotmail.com>
2024-03-20 16:52:06 +0100

    fixed the validator logic

--- src/zenml/artifact_stores/base_artifact_store.py
+++ src/zenml/artifact_stores/base_artifact_store.py
@@ -211,16 +211,18 @@ class BaseArtifactStoreConfig(StackComponentConfig):
                     """
                 )
             )
-        values["path"] = values["path"].strip("'\"`")
-        if not any(
-            values["path"].startswith(i) for i in cls.SUPPORTED_SCHEMES
-        ):
-            raise ArtifactStoreInterfaceError(
-                f"The path: '{values['path']}' you defined for your "
-                f"artifact store is not supported by the implementation of "
-                f"{cls.schema()['title']}, because it does not start with "
-                f"one of its supported schemes: {cls.SUPPORTED_SCHEMES}."
-            )
+
+        if "path" in values:
+            values["path"] = values["path"].strip("'\"`")
+            if not any(
+                values["path"].startswith(i) for i in cls.SUPPORTED_SCHEMES
+            ):
+                raise ArtifactStoreInterfaceError(
+                    f"The path: '{values['path']}' you defined for your "
+                    f"artifact store is not supported by the implementation of "
+                    f"{cls.schema()['title']}, because it does not start with "
+                    f"one of its supported schemes: {cls.SUPPORTED_SCHEMES}."
+                )
 
         return values
 
