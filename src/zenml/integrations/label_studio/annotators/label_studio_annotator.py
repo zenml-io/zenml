8fcc805e4653377db024718db32972477f21eef2
Hamza Tahir <htahir111@gmail.com>
2024-04-22 23:48:28 +0200

    Update

--- src/zenml/integrations/label_studio/annotators/label_studio_annotator.py
+++ src/zenml/integrations/label_studio/annotators/label_studio_annotator.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Implementatioain of the Label Studio annotation integration."""
+"""Implementation of the Label Studio annotation integration."""
 
 import json
 import os
