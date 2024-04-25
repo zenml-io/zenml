a7a5d48e5bbafb6dff6be158da5af3cf8c716a8b
Baris Can Durak <bariscandurak@hotmail.com>
2024-04-25 10:51:59 +0200

    new ruff new problems

--- src/zenml/zen_server/deploy/local/local_provider.py
+++ src/zenml/zen_server/deploy/local/local_provider.py
@@ -48,9 +48,9 @@ class LocalServerProvider(BaseServerProvider):
     """Local ZenML server provider."""
 
     TYPE: ClassVar[ServerProviderType] = ServerProviderType.LOCAL
-    CONFIG_TYPE: ClassVar[Type[ServerDeploymentConfig]] = (
-        LocalServerDeploymentConfig
-    )
+    CONFIG_TYPE: ClassVar[
+        Type[ServerDeploymentConfig]
+    ] = LocalServerDeploymentConfig
 
     @staticmethod
     def check_local_server_dependencies() -> None:
