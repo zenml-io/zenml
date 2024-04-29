90fa65d5909051a70865c6a7b4a6da3c62765a5c
Jayesh Sharma <wjayesh@outlook.com>
2024-04-29 16:08:11 +0530

    use legacy url for stacks and stack components

--- src/zenml/utils/dashboard_utils.py
+++ src/zenml/utils/dashboard_utils.py
@@ -18,6 +18,7 @@ from uuid import UUID
 
 from zenml import constants
 from zenml.client import Client
+from zenml.config.server_config import ServerConfiguration
 from zenml.enums import EnvironmentType, StoreType
 from zenml.environment import get_environment
 from zenml.logger import get_logger
@@ -65,6 +66,15 @@ def get_stack_url(stack: StackResponse) -> Optional[str]:
     """
     client = Client()
     base_url = get_base_url()
+
+    # the cloud dashboard doesn't support stacks yet
+    # use the legacy URL for now
+    server_config = ServerConfiguration()
+    legacy_url = server_config.dashboard_url
+
+    if legacy_url:
+        return legacy_url + f"{constants.STACKS}/{stack.id}/configuration"
+
     if base_url:
         server_model = client.zen_store.get_store_info()
         if server_model.use_legacy_dashboard:
@@ -86,6 +96,18 @@ def get_component_url(component: ComponentResponse) -> Optional[str]:
     """
     client = Client()
     base_url = get_base_url()
+
+    # the cloud dashboard doesn't support components yet
+    # use the legacy URL for now
+    server_config = ServerConfiguration()
+    legacy_url = server_config.dashboard_url
+
+    if legacy_url:
+        return (
+            legacy_url
+            + f"{constants.STACK_COMPONENTS}/{component.type.value}/{component.id}/configuration"
+        )
+
     if base_url:
         server_model = client.zen_store.get_store_info()
         if server_model.use_legacy_dashboard:
