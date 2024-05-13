5397ade12705ce646b75dd94f120a5e5fab99d62
Jayesh Sharma <wjayesh@outlook.com>
2024-05-13 12:38:28 +0530

    linting

--- src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator_entrypoint.py
+++ src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator_entrypoint.py
@@ -131,7 +131,9 @@ def main() -> None:
         # Wait for pod to finish.
         logger.info(f"Waiting for pod of step `{step_name}` to start...")
         kube_utils.wait_pod(
-            kube_client_fn=lambda: orchestrator.get_kube_client(incluster=True),
+            kube_client_fn=lambda: orchestrator.get_kube_client(
+                incluster=True
+            ),
             pod_name=pod_name,
             namespace=args.kubernetes_namespace,
             exit_condition_lambda=kube_utils.pod_is_done,
