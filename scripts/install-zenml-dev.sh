af1c1ba4dc2d4984788accdc8d13f0beb2ed543a
Baris Can Durak <bariscandurak@hotmail.com>
2024-05-14 14:32:05 +0200

    added system

--- scripts/install-zenml-dev.sh
+++ scripts/install-zenml-dev.sh
@@ -26,7 +26,7 @@ install_zenml() {
     uv pip install --system -e ".[server,templates,terraform,secrets-aws,secrets-gcp,secrets-azure,secrets-hashicorp,s3fs,gcsfs,adlfs,dev]"
 
     # TODO: Remove. Temporary installation from the upgrade branch.
-    uv pip install git+https://github.com/zenml-io/mlstacks.git@feature/upgrade-to-pydantic-v2
+    uv pip install --system git+https://github.com/zenml-io/mlstacks.git@feature/upgrade-to-pydantic-v2
 
 }
 
