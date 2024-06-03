a16babdcfc3f6fcaea5c4e00f1ccee029c9fede1
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-06-03 14:30:26 +0200

    fix bugs from CI

--- src/zenml/models/v2/core/pipeline_build.py
+++ src/zenml/models/v2/core/pipeline_build.py
@@ -442,29 +442,34 @@ class PipelineBuildResponse(
 class PipelineBuildFilter(WorkspaceScopedFilter):
     """Model to enable advanced filtering of all pipeline builds."""
 
-    workspace_id: Union[UUID, str, None] = Field(
-        description="Workspace for this pipeline build."
+    workspace_id: Optional[Union[UUID, str]] = Field(
+        description="Workspace for this pipeline build.", default=None
     )
-    user_id: Union[UUID, str, None] = Field(
-        description="User that produced this pipeline build."
+    user_id: Optional[Union[UUID, str]] = Field(
+        description="User that produced this pipeline build.", default=None
     )
-    pipeline_id: Union[UUID, str, None] = Field(
+    pipeline_id: Optional[Union[UUID, str]] = Field(
         description="Pipeline associated with the pipeline build.",
+        default=None,
     )
-    stack_id: Union[UUID, str, None] = Field(
-        description="Stack used for the Pipeline Run"
+    stack_id: Optional[Union[UUID, str]] = Field(
+        description="Stack used for the Pipeline Run", default=None
     )
     is_local: Optional[bool] = Field(
         description="Whether the build images are stored in a container "
         "registry or locally.",
+        default=None,
     )
     contains_code: Optional[bool] = Field(
         description="Whether any image of the build contains user code.",
+        default=None,
     )
     zenml_version: Optional[str] = Field(
-        description="The version of ZenML used for this build."
+        description="The version of ZenML used for this build.", default=None
     )
     python_version: Optional[str] = Field(
-        description="The Python version used for this build."
+        description="The Python version used for this build.", default=None
     )
-    checksum: Optional[str] = Field(description="The build checksum.")
+    checksum: Optional[str] = Field(
+        description="The build checksum.", default=None
+    )
