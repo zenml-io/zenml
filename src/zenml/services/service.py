a05cacaf318f74398cb7fca2292e86be64f55d00
Baris Can Durak <bariscandurak@hotmail.com>
2024-06-03 15:15:30 +0200

    changing dict to model dump

--- src/zenml/services/service.py
+++ src/zenml/services/service.py
@@ -219,7 +219,7 @@ class BaseService(BaseTypedModel):
             admin_state=model.admin_state,
             config=model.config,
             status=model.status,
-            service_type=dict(model.service_type),
+            service_type=model.service_type.model_dump(),
             endpoint=model.endpoint,
         )
 
