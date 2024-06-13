5d68087a144ea9b17face83ea4eff17d7eefbcce
Barış Can Durak <36421093+bcdurak@users.noreply.github.com>
2024-06-13 13:57:57 +0200

    Update src/zenml/constants.py
    Co-authored-by: Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>

--- src/zenml/constants.py
+++ src/zenml/constants.py
@@ -90,7 +90,7 @@ def is_false_string_value(value: Any) -> bool:
     Returns:
         Whether the input value represents a string version of 'False'.
     """
-    return True if value in ["0", "n", "no", "False", "false"] else False
+    return value in ["0", "n", "no", "False", "false"]
 
 
 def handle_bool_env_var(var: str, default: bool = False) -> bool:
