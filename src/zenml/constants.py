f65766ee4e3603109e183ddec6e0ea3b95ee6175
Barış Can Durak <36421093+bcdurak@users.noreply.github.com>
2024-06-13 13:58:18 +0200

    Update src/zenml/constants.py
    Co-authored-by: Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>

--- src/zenml/constants.py
+++ src/zenml/constants.py
@@ -78,7 +78,7 @@ def is_true_string_value(value: Any) -> bool:
     Returns:
         Whether the input value represents a string version of 'True'.
     """
-    return True if value in ["1", "y", "yes", "True", "true"] else False
+    return value in ["1", "y", "yes", "True", "true"]
 
 
 def is_false_string_value(value: Any) -> bool:
