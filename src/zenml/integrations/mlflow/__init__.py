5f628d82a16ac76c3694595e1853ba9b4a496325
Baris Can Durak <bariscandurak@hotmail.com>
2024-06-13 15:45:29 +0200

    mlflow explanation

--- src/zenml/integrations/mlflow/__init__.py
+++ src/zenml/integrations/mlflow/__init__.py
@@ -38,6 +38,13 @@ class MlflowIntegration(Integration):
         "mlserver-mlflow>=1.3.3",
         # TODO: remove this requirement once rapidjson is fixed
         "python-rapidjson<1.15",
+        # When you do:
+        # pip install zenml
+        # You get all our required dependencies. However, if you follow it with:
+        # zenml integration install mlflow
+        # This downgrades pydantic to v1 even though mlflow does not have
+        # any issues with v2. This is why we have to pin it here so a downgrade
+        # will not happen.
         "pydantic>=2.7.0,<2.8.0"
     ]
 
