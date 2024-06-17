71d66559d2b81dbda399429243c39db9f5e891ac
Baris Can Durak <bariscandurak@hotmail.com>
2024-06-17 16:29:10 +0200

    fixed the version again

--- tests/integration/examples/test_tensorflow.py
+++ tests/integration/examples/test_tensorflow.py
@@ -21,7 +21,8 @@ from tests.integration.examples.utils import run_example
 
 
 @pytest.mark.skipif(
-    sys.version_info > (3, 10) or sys.version_info == (3, 8),
+    (sys.version_info.major == 3 and sys.version_info.minor == 11)
+    or (sys.version_info.major == 3 and sys.version_info.minor == 8),
     reason="Tensorflow integration does not work as expected with python3.8.",
 )
 def test_example(request: pytest.FixtureRequest) -> None:
