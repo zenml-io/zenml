66da33605ad355fea5ddf30ea0bab89497bc4d87
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-06-20 10:27:20 +0200

    Update docstring

--- src/zenml/types.py
+++ src/zenml/types.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Custom types that are used to indicate how to handle data."""
+"""Custom ZenML types."""
 
 from typing import TYPE_CHECKING, Callable, Union
 
