c297cc9a035a0d63188303f372e0345e20d3c272
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-06-20 17:23:11 +0200

    permanent tag

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -91,7 +91,7 @@ ZENML_PROJECT_TEMPLATES = dict(
     ),
     llm_finetuning=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-llm-finetuning",
-        github_tag="feature/OSSK-569-accelerated-template",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
+        github_tag="2024.06.20",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
 )
 
