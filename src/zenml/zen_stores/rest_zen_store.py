092ea2469b81cedb1ec486b5ae09fa566a9b77dd
Baris Can Durak <bariscandurak@hotmail.com>
2024-06-28 09:34:49 +0200

    rest zen store

--- src/zenml/zen_stores/rest_zen_store.py
+++ src/zenml/zen_stores/rest_zen_store.py
@@ -65,6 +65,7 @@ from zenml.constants import (
     ENV_ZENML_DISABLE_CLIENT_SERVER_MISMATCH_WARNING,
     EVENT_SOURCES,
     FLAVORS,
+    FULL_STACK,
     GET_OR_CREATE,
     INFO,
     LOGIN,
@@ -147,6 +148,7 @@ from zenml.models import (
     FlavorRequest,
     FlavorResponse,
     FlavorUpdate,
+    FullStackRequest,
     LogsResponse,
     ModelFilter,
     ModelRequest,
@@ -2741,6 +2743,21 @@ class RestZenStore(BaseZenStore):
         return self._create_workspace_scoped_resource(
             resource=stack,
             route=STACKS,
+            response_model=StackResponse,
+        )
+
+    def create_full_stack(self, full_stack: FullStackRequest) -> StackResponse:
+        """Register a full-stack.
+
+        Args:
+            full_stack: The full stack configuration.
+
+        Returns:
+            The registered stack.
+        """
+        return self._create_resource(
+            resource=full_stack,
+            route=FULL_STACK,
             response_model=StackResponse,
         )
 
