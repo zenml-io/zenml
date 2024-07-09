d712af66ae949307c0814d3265f39ff3f3fd57c8
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-07-09 14:53:38 +0200

    Fix alembic order

--- src/zenml/zen_stores/migrations/versions/b4fca5241eea_migrate_onboarding_state.py
+++ src/zenml/zen_stores/migrations/versions/b4fca5241eea_migrate_onboarding_state.py
@@ -1,7 +1,7 @@
 """Migrate onboarding state [b4fca5241eea].
 
 Revision ID: b4fca5241eea
-Revises: 0d707865f404
+Revises: 0.61.0
 Create Date: 2024-06-20 15:01:22.414801
 
 """
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "b4fca5241eea"
-down_revision = "0d707865f404"
+down_revision = "0.61.0"
 branch_labels = None
 depends_on = None
 
