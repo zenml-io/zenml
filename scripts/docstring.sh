39333b8de16f7b1c06cce1f6ca679cde4da069ff
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-07-15 20:20:18 +0200

    update script definition

--- scripts/docstring.sh
+++ scripts/docstring.sh
@@ -7,4 +7,4 @@ DOCSTRING_SRC=${1:-"src/zenml tests/harness"}
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 
-pydoclint $DOCSTRING_SRC
+pydoclint --config=.pydoclint.toml $DOCSTRING_SRC
