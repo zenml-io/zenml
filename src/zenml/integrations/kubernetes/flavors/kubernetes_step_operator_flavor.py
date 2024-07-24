959b492cfa3fd510f56fc722df0f4e5552af7db2
Hamza Tahir <hamza@zenml.io>
2024-07-24 18:54:25 +0200

    Apply suggestions from code review
    Co-authored-by: Alex Strick van Linschoten <strickvl@users.noreply.github.com>

--- src/zenml/integrations/kubernetes/flavors/kubernetes_step_operator_flavor.py
+++ src/zenml/integrations/kubernetes/flavors/kubernetes_step_operator_flavor.py
@@ -1,4 +1,4 @@
-#  Copyright (c) ZenML GmbH 2022. All Rights Reserved.
+#  Copyright (c) ZenML GmbH 2024. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Kubernetes orchestrator flavor."""
+"""Kubernetes step operator flavor."""
 
 from typing import TYPE_CHECKING, Optional, Type
 
@@ -121,7 +121,7 @@ class KubernetesStepOperatorFlavor(BaseStepOperatorFlavor):
 
     @property
     def config_class(self) -> Type[KubernetesStepOperatorConfig]:
-        """Returns `KubernetesOrchestratorConfig` config class.
+        """Returns `KubernetesStepOperatorConfig` config class.
 
         Returns:
                 The config class.
