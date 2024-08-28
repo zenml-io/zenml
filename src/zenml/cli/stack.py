735608b3057c661be0c0532f3b99a1d6b1d0adba
Baris Can Durak <bariscandurak@hotmail.com>
2024-08-28 15:34:38 +0200

    fixing the required prompts

--- src/zenml/cli/stack.py
+++ src/zenml/cli/stack.py
@@ -2477,6 +2477,10 @@ def _get_stack_component_info(
                 "Skypilot cluster",
                 is_skypilot=True,
             )
+        elif flavor == "azureml":
+            config["subscription_id"] = Prompt.ask("Enter the subscription ID:")
+            config["resource_group"] = Prompt.ask("Enter the resource group:")
+            config["workspace"] = Prompt.ask("Enter workspace name:")
         elif flavor == "vertex":
             config["location"] = query_region(
                 StackDeploymentProvider.GCP, "Vertex AI job"
