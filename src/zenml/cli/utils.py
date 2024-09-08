31ef6c9f7d8b24a7c5d0fb31b12da14de48b85a6
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-09-08 21:17:04 +0200

    docstring fixes

--- src/zenml/cli/utils.py
+++ src/zenml/cli/utils.py
@@ -163,7 +163,7 @@ def error(text: str) -> NoReturn:
     Raises:
         ClickException: when called.
     """
-    raise click.ClickException(message=click.style(text, fg="red", bold=True))
+    raise ClickException(message=click.style(text, fg="red", bold=True))
 
 
 def warning(
@@ -222,7 +222,7 @@ def print_table(
         obj: A List containing dictionaries.
         title: Title of the table.
         caption: Caption of the table.
-        columns: Optional column configurations to be used in the table.
+        **columns: Optional column configurations to be used in the table.
     """
     column_keys = {key: None for dict_ in obj for key in dict_}
     column_names = [columns.get(key, key.upper()) for key in column_keys]
@@ -2635,7 +2635,7 @@ def temporary_active_stack(
             this contextmanager will not do anything.
 
     Yields:
-        The active stack.
+        Stack: The active stack.
     """
     from zenml.client import Client
 
