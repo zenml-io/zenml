28b89b5b6e5138c91c0e54498ea00fea824d018d
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-10-07 16:41:53 +0200

    fix infinite loop while fetching artifact store

---
+++ src/zenml/logging/utils.py
@@ -0,0 +1,40 @@
+#  Copyright (c) ZenML GmbH 2024. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+"""ZenML logging utils."""
+
+import logging
+from contextlib import contextmanager
+from typing import Generator
+
+
+@contextmanager
+def suspend_debug_logging(logger_name: str) -> Generator[None, None, None]:
+    """Context manager to temporarily suspend logging.
+
+    Args:
+        logger_name: Name of the logger to suspend.
+
+    Yields:
+        None
+    """
+    logger_ = logging.getLogger(logger_name)
+    current_level = logger_.level
+    if current_level < logging.INFO:
+        logger_.setLevel(logging.INFO)
+        try:
+            yield
+        finally:
+            logger_.setLevel(current_level)
+    else:
+        yield
