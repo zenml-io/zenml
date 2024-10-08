663c2aff70b4d90d571b4229a27cdad2026482c1
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-10-08 11:39:58 +0200

    fix typing bug

--- src/zenml/artifacts/utils.py
+++ src/zenml/artifacts/utils.py
@@ -33,6 +33,7 @@ from typing import (
 from uuid import UUID, uuid4
 
 from zenml.artifacts.load_directory_materializer import (
+    PreexistingArtifactPath,
     PreexistingDataMaterializer,
 )
 from zenml.client import Client
@@ -320,7 +321,7 @@ def link_existing_data_as_artifact(
             type=ArtifactType.DATA,
             uri=folder_or_file_uri,
             materializer=source_utils.resolve(PreexistingDataMaterializer),
-            data_type=source_utils.resolve(Path),
+            data_type=source_utils.resolve(PreexistingArtifactPath),
             user=Client().active_user.id,
             workspace=Client().active_workspace.id,
             artifact_store_id=artifact_store.id,
