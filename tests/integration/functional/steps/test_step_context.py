65819e93a78cc20dace32e5d608fadd7278e0529
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-10-22 12:22:23 +0200

    Remove test that applies tags to parent artifact

--- tests/integration/functional/steps/test_step_context.py
+++ tests/integration/functional/steps/test_step_context.py
@@ -210,9 +210,5 @@ def test_metadata_and_tags_set_from_context(
     if full_name == "custom_name":
         assert av.run_metadata["config_metadata"].value == "bar"
         assert {t.name for t in av.tags} == set(tags).union({"config_tags"})
-        assert {t.name for t in artifact.tags} == set(tags).union(
-            {"config_tags"}
-        )
     else:
         assert set(tags) == {t.name for t in av.tags}
-        assert set(tags) == {t.name for t in artifact.tags}
