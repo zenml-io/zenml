e748dfcc4791941dd3a2892952853824ed455656
Baris Can Durak <bariscandurak@hotmail.com>
2024-10-23 11:22:10 +0200

    using the github CLI

--- scripts/test-migrations.sh
+++ scripts/test-migrations.sh
@@ -25,6 +25,14 @@ fi
 # List of versions to test
 VERSIONS=("0.40.3" "0.43.0" "0.44.3" "0.45.6" "0.47.0" "0.50.0" "0.51.0" "0.52.0" "0.53.1" "0.54.1" "0.55.5" "0.56.4" "0.57.1" "0.60.0" "0.61.0" "0.62.0" "0.63.0" "0.64.0" "0.65.0")
 
+# Include latest release dynamically, if not there already
+LATEST_RELEASE=$(gh release view --json tagName -q '{tag: .tagName}')
+LATEST_VERSION=$(echo "$LATEST_RELEASE" | jq -r .tag)
+
+if [[ ! " ${VERSIONS[@]} " =~ " ${LATEST_VERSION} " ]]; then
+   VERSIONS+=("${LATEST_VERSION}")
+fi
+
 # Function to compare semantic versions
 function version_compare() {
     local regex="^([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\\+([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?$"
