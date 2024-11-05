74c1a425e97a4a6063f2f52a4130eae4020c6b96
Baris Can Durak <bariscandurak@hotmail.com>
2024-11-05 23:27:36 +0100

    new operation

--- src/zenml/enums.py
+++ src/zenml/enums.py
@@ -245,6 +245,7 @@ class GenericFilterOps(StrEnum):
     CONTAINS = "contains"
     STARTSWITH = "startswith"
     ENDSWITH = "endswith"
+    ONEOF = "oneof"
     GTE = "gte"
     GT = "gt"
     LTE = "lte"
