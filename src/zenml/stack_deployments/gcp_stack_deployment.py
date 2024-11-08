50df37997a883b578edb173f2e36d688af3b51b2
Stefan Nica <stefan@zenml.io>
2024-11-08 18:26:34 +0100

    Add step operator to GCP one-click stack deployment

--- src/zenml/stack_deployments/gcp_stack_deployment.py
+++ src/zenml/stack_deployments/gcp_stack_deployment.py
@@ -70,7 +70,8 @@ and are aware of any potential costs:
 
 - A GCS bucket registered as a [ZenML artifact store](https://docs.zenml.io/stack-components/artifact-stores/gcp).
 - A Google Artifact Registry registered as a [ZenML container registry](https://docs.zenml.io/stack-components/container-registries/gcp).
-- Vertex AI registered as a [ZenML orchestrator](https://docs.zenml.io/stack-components/orchestrators/vertex).
+- Vertex AI registered as a [ZenML orchestrator](https://docs.zenml.io/stack-components/orchestrators/vertex)
+and as a [ZenML step operator](https://docs.zenml.io/stack-components/step-operators/vertex).
 - GCP Cloud Build registered as a [ZenML image builder](https://docs.zenml.io/stack-components/image-builders/gcp).
 - A GCP Service Account with the minimum necessary permissions to access the
 above resources.
