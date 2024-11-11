c41cfd2cdc215a135180d6742f4510c17cfb6406
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-11-11 11:22:23 +0100

    Refactor logs endpoint definition

--- src/zenml/zen_server/routers/logs_endpoints.py
+++ src/zenml/zen_server/routers/logs_endpoints.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Endpoint definitions for steps (and artifacts) of pipeline runs."""
+"""Endpoint definitions for logs."""
 
 from uuid import UUID
 
