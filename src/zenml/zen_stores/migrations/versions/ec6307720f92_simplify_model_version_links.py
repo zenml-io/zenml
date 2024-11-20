0ad92aa494e4ec5e5050cddeaabf9edbe57d429a
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-11-20 15:17:39 +0100

    Fix alembic order

--- src/zenml/zen_stores/migrations/versions/ec6307720f92_simplify_model_version_links.py
+++ src/zenml/zen_stores/migrations/versions/ec6307720f92_simplify_model_version_links.py
@@ -1,7 +1,7 @@
 """Simplify model version links [ec6307720f92].
 
 Revision ID: ec6307720f92
-Revises: 904464ea4041
+Revises: 0.70.0
 Create Date: 2024-11-06 16:16:43.344569
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "ec6307720f92"
-down_revision = "904464ea4041"
+down_revision = "0.70.0"
 branch_labels = None
 depends_on = None
 
