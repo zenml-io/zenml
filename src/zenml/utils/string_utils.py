0aca36e39fd1e6db184db86e716f0cbb0da2ab87
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-11-26 11:02:08 +0100

    simplify `format_name_template`

--- src/zenml/utils/string_utils.py
+++ src/zenml/utils/string_utils.py
@@ -16,10 +16,9 @@
 import base64
 import functools
 import random
-import re
 import string
 from datetime import datetime
-from typing import Any, Callable, Dict, TypeVar, cast
+from typing import Any, Callable, Dict, Optional, TypeVar, cast
 
 from pydantic import BaseModel
 
@@ -148,7 +147,7 @@ def validate_name(model: BaseModel) -> None:
 
 def format_name_template(
     name_template: str,
-    **kwargs: str,
+    substitutions: Optional[Dict[str, str]],
 ) -> str:
     """Formats a name template with the given arguments.
 
@@ -158,7 +157,7 @@ def format_name_template(
 
     Args:
         name_template: The name template to format.
-        **kwargs: The arguments to replace in the template.
+        substitutions: A dictionary of substitutions to use in the template.
 
     Returns:
         The formatted name template.
@@ -166,30 +165,27 @@ def format_name_template(
     Raises:
         KeyError: If a key in template is missing in the kwargs.
     """
-    pattern = r"\{[^}]+\}"
-    if not re.findall(pattern, name_template):
-        # nothing to evaluate
-        return name_template
+    substitutions = substitutions or {}
 
-    if ("date" not in kwargs and "{date}" in name_template) or (
-        "time" not in kwargs and "{time}" in name_template
+    if ("date" not in substitutions and "{date}" in name_template) or (
+        "time" not in substitutions and "{time}" in name_template
     ):
         from zenml import get_step_context
 
         try:
             pr = get_step_context().pipeline_run
             start_time = pr.start_time
-            kwargs.update(pr.config.substitutions or {})
+            substitutions.update(pr.config.substitutions or {})
         except RuntimeError:
             start_time = None
 
         if start_time is None:
             start_time = datetime.utcnow()
-        kwargs["date"] = start_time.strftime("%Y_%m_%d")
-        kwargs["time"] = start_time.strftime("%H_%M_%S_%f")
+        substitutions.setdefault("date", start_time.strftime("%Y_%m_%d"))
+        substitutions.setdefault("time", start_time.strftime("%H_%M_%S_%f"))
 
     try:
-        return name_template.format(**kwargs)
+        return name_template.format(**substitutions)
     except KeyError as e:
         raise KeyError(
             f"Could not format the name template `{name_template}`. "
