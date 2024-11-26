59d9867b645fa153f9ebb0e76fe20f9506301b62
Baris Can Durak <bariscandurak@hotmail.com>
2024-11-26 02:30:16 +0100

    spellcheck

--- src/zenml/models/v2/misc/run_metadata.py
+++ src/zenml/models/v2/misc/run_metadata.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Utility classes for modelling run metadata."""
+"""Utility classes for modeling run metadata."""
 
 from datetime import datetime
 from uuid import UUID
