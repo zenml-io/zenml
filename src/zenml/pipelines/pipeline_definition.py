34f3fe91693b9fada3cf1ee12e87617cc7b81cce
Michael Schuster <schustmi@users.noreply.github.com>
2024-11-28 10:54:17 +0100

    Run template CLI command and bugfix (#3225)

--- src/zenml/pipelines/pipeline_definition.py
+++ src/zenml/pipelines/pipeline_definition.py
@@ -583,6 +583,7 @@ To avoid this consider setting pipeline parameters only in one place (config or 
         config_path: Optional[str] = None,
         unlisted: bool = False,
         prevent_build_reuse: bool = False,
+        skip_schedule_registration: bool = False,
     ) -> PipelineDeploymentResponse:
         """Create a pipeline deployment.
 
@@ -609,6 +610,7 @@ To avoid this consider setting pipeline parameters only in one place (config or 
                 to any pipeline).
             prevent_build_reuse: DEPRECATED: Use
                 `DockerSettings.prevent_build_reuse` instead.
+            skip_schedule_registration: Whether to skip schedule registration.
 
         Returns:
             The pipeline deployment.
@@ -649,7 +651,7 @@ To avoid this consider setting pipeline parameters only in one place (config or 
         stack.validate()
 
         schedule_id = None
-        if schedule:
+        if schedule and not skip_schedule_registration:
             if not stack.orchestrator.config.is_schedulable:
                 raise ValueError(
                     f"Stack {stack.name} does not support scheduling. "
@@ -1445,7 +1447,9 @@ To avoid this consider setting pipeline parameters only in one place (config or 
             The created run template.
         """
         self._prepare_if_possible()
-        deployment = self._create_deployment(**self._run_args)
+        deployment = self._create_deployment(
+            **self._run_args, skip_schedule_registration=True
+        )
 
         return Client().create_run_template(
             name=name, deployment_id=deployment.id, **kwargs
