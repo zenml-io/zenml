70cbbb603fad37b8b5ab90b7df85bafaa525b033
Baris Can Durak <bariscandurak@hotmail.com>
2024-12-03 17:58:37 +0100

    adding the test back in

--- tests/unit/test_general.py
+++ tests/unit/test_general.py
@@ -95,6 +95,7 @@ def _test_materializer(
             assert len(visualizations) > 0
         for uri, value in visualizations.items():
             assert isinstance(uri, str)
+            assert "\\" not in uri
             assert isinstance(value, VisualizationType)
             assert os.path.exists(uri)
 
