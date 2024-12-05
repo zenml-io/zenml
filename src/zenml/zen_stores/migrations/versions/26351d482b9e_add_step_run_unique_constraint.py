b42cbcf958eac7f190a16c7d8db1a4e587c1c591
Michael Schuster <michael.schuster.ffb@googlemail.com>
2024-12-05 18:03:34 +0100

    Fix alembic order

--- src/zenml/zen_stores/migrations/versions/26351d482b9e_add_step_run_unique_constraint.py
+++ src/zenml/zen_stores/migrations/versions/26351d482b9e_add_step_run_unique_constraint.py
@@ -1,7 +1,7 @@
 """Add step run unique constraint [26351d482b9e].
 
 Revision ID: 26351d482b9e
-Revises: cc269488e5a9
+Revises: 0.71.0
 Create Date: 2024-12-03 11:46:57.541578
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "26351d482b9e"
-down_revision = "cc269488e5a9"
+down_revision = "0.71.0"
 branch_labels = None
 depends_on = None
 
