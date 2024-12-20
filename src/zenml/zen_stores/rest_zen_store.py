83564b2c3ca6f2a38f64a13f1aedab4e4156d8d9
Stefan Nica <stefan@zenml.io>
2024-12-20 23:10:51 +0100

    Fix the client user-agent headers

--- src/zenml/zen_stores/rest_zen_store.py
+++ src/zenml/zen_stores/rest_zen_store.py
@@ -4053,7 +4053,12 @@ class RestZenStore(BaseZenStore):
                 )
 
             data: Optional[Dict[str, str]] = None
-            headers: Dict[str, str] = {}
+
+            # Use a custom user agent to identify the ZenML client in the server
+            # logs.
+            headers: Dict[str, str] = {
+                "User-Agent": "zenml/" + zenml.__version__,
+            }
 
             # Check if an API key is configured
             api_key = credentials_store.get_api_key(self.url)
