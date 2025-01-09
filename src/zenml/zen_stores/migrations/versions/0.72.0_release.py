df9fdfaf96a23fcaa1384e682d6e36c82de048ff
ZenML GmbH <info@zenml.io>
2025-01-09 14:13:11 +0000

    Adding the new version to the necessary files.

---
+++ src/zenml/zen_stores/migrations/versions/0.72.0_release.py
@@ -0,0 +1,23 @@
+"""Release [0.72.0].
+
+Revision ID: 0.72.0
+Revises: a1237ba94fd8
+Create Date: 2025-01-09 14:13:08.280284
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0.72.0"
+down_revision = "a1237ba94fd8"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade database schema and/or data, creating a new revision."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade database schema and/or data back to the previous revision."""
+    pass
