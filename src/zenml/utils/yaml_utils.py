60d388f0d8805f58f00857de9f601c7ff503804e
Frank995 <pfrank995@gmail.com>
2025-01-20 15:57:09 +0100

    Fix docstring in write_json

--- src/zenml/utils/yaml_utils.py
+++ src/zenml/utils/yaml_utils.py
@@ -130,6 +130,7 @@ def write_json(
         file_path: Path to JSON file.
         contents: Contents of JSON file.
         encoder: Custom JSON encoder to use when saving json.
+        json_dump_args: Extra arguments to pass to json.dumps.
 
     Raises:
         FileNotFoundError: if directory does not exist.
