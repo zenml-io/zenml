3a7f09e26ae548dacae894b4e4c2e28339f4ffc4
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-01-29 17:04:55 +0100

    Fix some tests

--- tests/unit/pipelines/test_build_utils.py
+++ tests/unit/pipelines/test_build_utils.py
@@ -74,13 +74,13 @@ class StubCodeRepository(BaseCodeRepository):
 class StubLocalRepositoryContext(LocalRepositoryContext):
     def __init__(
         self,
-        code_repository_id: UUID = uuid4(),
+        code_repository: "BaseCodeRepository" = StubCodeRepository(),
         root: str = ".",
         is_dirty: bool = False,
         has_local_changes: bool = False,
         commit: str = "",
     ) -> None:
-        super().__init__(code_repository_id=code_repository_id)
+        super().__init__(code_repository=code_repository)
         self._root = root
         self._is_dirty = is_dirty
         self._has_local_changes = has_local_changes
