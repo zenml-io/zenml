095a01a943c7159c26e70b0b64756848bfddcdd7
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-01-30 11:29:53 +0100

    Add index for tag resources

--- src/zenml/zen_stores/schemas/run_metadata_schemas.py
+++ src/zenml/zen_stores/schemas/run_metadata_schemas.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""SQLModel implementation of pipeline run metadata tables."""
+"""SQLModel implementation of run metadata tables."""
 
 from typing import Optional
 from uuid import UUID, uuid4
