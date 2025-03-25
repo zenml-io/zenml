c85ed6957a7be6e2c246231bf2962803a86d1007
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-03-25 11:58:09 +0100

    Rework project activation

--- src/zenml/zen_stores/base_zen_store.py
+++ src/zenml/zen_stores/base_zen_store.py
@@ -333,28 +333,43 @@ class BaseZenStore(
                     active_project = self.get_project(user.default_project_id)
                 except (KeyError, IllegalOperationError):
                     logger.warning(
-                        "The default project for the active user is no longer "
-                        "available. Please set the active project by running "
-                        "`zenml project set <project-name>`."
+                        "The default project %s for the active user is no longer "
+                        "available.",
+                        user.default_project_id,
                     )
                 else:
                     logger.info(
                         f"Setting the {config_name} active project "
                         f"to '{active_project.name}'."
                     )
+
+        if active_project is None:
+            try:
+                projects = self.list_projects()
+            except Exception:
+                # There was some failure, we force the user to set the active
+                # project manually
+                logger.warning(
+                    "An active project is not set. Please set the active "
+                    "project by running `zenml project set "
+                    "<project-name>`."
+                )
             else:
-                try:
-                    active_project = self._get_default_project()
-                except (KeyError, IllegalOperationError):
+                if len(projects) == 0:
                     logger.warning(
-                        "An active project is not set. Please set the active "
-                        "project by running `zenml project set "
-                        "<project-name>`."
+                        "No available projects. Please create a project by "
+                        "running `zenml project register <NAME> --set`."
                     )
-                else:
+                elif len(projects) == 1:
+                    active_project = projects.items[0]
                     logger.info(
                         f"Setting the {config_name} active project "
                         f"to '{active_project.name}'."
+                    )
+                else:
+                    logger.warning(
+                        "Multiple projects are available. Please set the "
+                        "active project by running `zenml project set <NAME>`."
                     )
 
         active_stack: StackResponse
