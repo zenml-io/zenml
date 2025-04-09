09feb7c73dc626338d4684903598581cefa0496d
Stefan Nica <stefan@zenml.io>
2025-04-09 18:19:38 +0200

    Fix docstrings

--- src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator.py
+++ src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator.py
@@ -407,6 +407,9 @@ class KubernetesOrchestrator(ContainerizedOrchestrator):
 
         Raises:
             RuntimeError: If the Kubernetes orchestrator is not configured.
+            TimeoutError: If the orchestrator pod is still in a pending state
+                after the maximum wait time has elapsed.
+            Exception: If the orchestrator pod fails to start.
         """
         for step_name, step in deployment.step_configurations.items():
             if self.requires_resources_in_orchestration_environment(step):
