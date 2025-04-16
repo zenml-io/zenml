9b45c81cace861b4037600cdd0be5b6d45ef71b4
Alexej Penner <thealexejpenner@gmail.com>
2025-04-16 18:47:05 +0200

    Removed logs

--- src/zenml/integrations/github/code_repositories/github_code_repository.py
+++ src/zenml/integrations/github/code_repositories/github_code_repository.py
@@ -243,9 +243,6 @@ class GitHubCodeRepository(BaseCodeRepository):
         owner = self.config.owner
         repo = self.config.repository
          
-
-        print(f"Checking remote URL: {url} against {host} {owner} {repo}")   
-
         # Clean the input URL by removing any trailing slashes
         url = url.rstrip('/')
         
