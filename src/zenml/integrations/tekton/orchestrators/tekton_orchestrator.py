e8a71f25f97db4c6a10c5114b100a781820dd4e9
Baris Can Durak <bariscandurak@hotmail.com>
2025-04-30 15:00:56 +0200

    linting

--- src/zenml/integrations/tekton/orchestrators/tekton_orchestrator.py
+++ src/zenml/integrations/tekton/orchestrators/tekton_orchestrator.py
@@ -243,7 +243,7 @@ class TektonOrchestrator(ContainerizedOrchestrator):
             raise RuntimeError(
                 f"Error while trying to fetch tekoton cookie: {errh}"
             )
-        cookie_dict: Dict[str, str] = session.cookies.get_dict()  # type: ignore[no-untyped-call]
+        cookie_dict: Dict[str, str] = session.cookies.get_dict()
 
         if "authservice_session" not in cookie_dict:
             raise RuntimeError("Invalid username and/or password!")
