c057a59a46f73cf68b2554bec3b23100d86087fe
Hamza Tahir <hamza@zenml.io>
2025-05-04 09:26:14 +0200

    Update Skypilot integrations with omegaconf>=2.4.0.dev3

--- src/zenml/integrations/skypilot_kubernetes/__init__.py
+++ src/zenml/integrations/skypilot_kubernetes/__init__.py
@@ -32,7 +32,7 @@ class SkypilotKubernetesIntegration(Integration):
 
     NAME = SKYPILOT_KUBERNETES
     # all 0.6.x versions of skypilot[kubernetes] are compatible
-    REQUIREMENTS = ["skypilot[kubernetes]~=0.9.2"]
+    REQUIREMENTS = ["skypilot[kubernetes]~=0.9.2", "omegaconf>=2.4.0.dev3"]
     APT_PACKAGES = ["openssh-client", "rsync"]
 
     @classmethod
--- src/zenml/integrations/skypilot_lambda/__init__.py
+++ src/zenml/integrations/skypilot_lambda/__init__.py
@@ -31,7 +31,7 @@ class SkypilotLambdaIntegration(Integration):
     """Definition of Skypilot Lambda Integration for ZenML."""
 
     NAME = SKYPILOT_LAMBDA
-    REQUIREMENTS = ["skypilot[lambda]~=0.9.2"]
+    REQUIREMENTS = ["skypilot[lambda]~=0.9.2", "omegaconf>=2.4.0.dev3"]
 
     @classmethod
     def flavors(cls) -> List[Type[Flavor]]:
