cb3cfc1b1fd8264d9fcdf221eb01a131c1197e48
Hamza Tahir <htahir111@gmail.com>
2025-05-22 19:23:08 +0200

    Added ZenML Artifact Store flavor

--- src/zenml/integrations/gcp/flavors/__init__.py
+++ src/zenml/integrations/gcp/flavors/__init__.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""GCP stack component flavors."""
+"""GCP integration flavors."""
 
 from zenml.integrations.gcp.flavors.gcp_artifact_store_flavor import (
     GCPArtifactStoreFlavor,
