98c66fb40e55d1b4af8b4582f072044a120a8c2d
Baris Can Durak <bariscandurak@hotmail.com>
2025-06-30 18:22:10 +0200

    added migration script

---
+++ src/zenml/zen_stores/migrations/versions/85289fea86ff_adding_title_to_logs.py
@@ -0,0 +1,32 @@
+"""adding-title-to-logs [85289fea86ff].
+
+Revision ID: 85289fea86ff
+Revises: 5bb25e95849c
+Create Date: 2025-06-30 18:18:24.539265
+
+"""
+
+import sqlalchemy as sa
+import sqlmodel
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "85289fea86ff"
+down_revision = "5bb25e95849c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade database schema and/or data, creating a new revision."""
+    with op.batch_alter_table("logs", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column(
+                "title", sqlmodel.sql.sqltypes.AutoString(), nullable=True
+            )
+        )
+
+
+def downgrade() -> None:
+    """Downgrade database schema and/or data back to the previous revision."""
+    pass
