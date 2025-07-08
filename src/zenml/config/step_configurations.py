02aad7bd8894d90fe8e393f1b478cc5fc285e8a3
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-07-08 09:16:10 +0200

    Fix warning log during pipeline config application

--- src/zenml/config/step_configurations.py
+++ src/zenml/config/step_configurations.py
@@ -307,7 +307,11 @@ class StepConfiguration(PartialStepConfiguration):
                 exclude_none=True,
             )
 
-            updated_config = self.model_copy(update=pipeline_values, deep=True)
+            updated_config_dict = {
+                **self.model_dump(),
+                **pipeline_values,
+            }
+            updated_config = self.model_validate(updated_config_dict)
             return update_model(updated_config, original_values)
         else:
             return self.model_copy(deep=True)
