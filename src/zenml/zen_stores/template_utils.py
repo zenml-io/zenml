a47a6fd76cab3b239dc8e838ff897b828e83bf5c
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-09-01 17:37:56 +0200

    Allow bools for step operator/experiment tracker

--- src/zenml/zen_stores/template_utils.py
+++ src/zenml/zen_stores/template_utils.py
@@ -14,7 +14,7 @@
 """Utilities for run templates."""
 
 from enum import Enum
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from pydantic import create_model
 from pydantic.fields import FieldInfo
@@ -199,7 +199,7 @@ def generate_config_schema(
             "ExperimentTrackers", {e: e for e in experiment_trackers}
         )
         generic_step_fields["experiment_tracker"] = (
-            Optional[experiment_tracker_enum],
+            Optional[Union[experiment_tracker_enum, bool]],
             None,
         )
     if step_operators:
@@ -207,7 +207,7 @@ def generate_config_schema(
             "StepOperators", {s: s for s in step_operators}
         )
         generic_step_fields["step_operator"] = (
-            Optional[step_operator_enum],
+            Optional[Union[step_operator_enum, bool]],
             None,
         )
 
