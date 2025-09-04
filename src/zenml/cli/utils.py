67928666fbde30f1781aed24c45cab2988fa2537
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-09-04 18:05:25 +0200

    Simplify

--- src/zenml/cli/utils.py
+++ src/zenml/cli/utils.py
@@ -2251,9 +2251,7 @@ def get_execution_status_emoji(status: "ExecutionStatus") -> str:
     """
     from zenml.enums import ExecutionStatus
 
-    if status == ExecutionStatus.INITIALIZING:
-        return ":hourglass_flowing_sand:"
-    if status == ExecutionStatus.PROVISIONING:
+    if status in {ExecutionStatus.INITIALIZING, ExecutionStatus.PROVISIONING}:
         return ":hourglass_flowing_sand:"
     if status == ExecutionStatus.FAILED:
         return ":x:"
