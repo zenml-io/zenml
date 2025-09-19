d73a804139929a70ecdefcda02c5b1964d07239f
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-09-19 13:32:28 +0200

    Bump llm template

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -92,7 +92,7 @@ ZENML_PROJECT_TEMPLATES = dict(
     ),
     llm_finetuning=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-llm-finetuning",
-        github_tag="2025.07.14",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
+        github_tag="2025.09.19",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
 )
 
