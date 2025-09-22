1a7af20024d54801e13e7dc459e93b8d3c0e5ee1
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-09-22 12:18:58 +0200

    Bump e2e template

--- src/zenml/cli/base.py
+++ src/zenml/cli/base.py
@@ -80,7 +80,7 @@ class ZenMLProjectTemplateLocation(BaseModel):
 ZENML_PROJECT_TEMPLATES = dict(
     e2e_batch=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-e2e-batch",
-        github_tag="misc/new-dependencies",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
+        github_tag="2025.09.22",  # Make sure it is aligned with .github/workflows/update-templates-to-examples.yml
     ),
     starter=ZenMLProjectTemplateLocation(
         github_url="zenml-io/template-starter",
