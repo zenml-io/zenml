14e491a63a762f60eeee21fd30b584c9f3222fb1
Michael Schuster <michael.schuster.ffb@googlemail.com>
2025-09-29 13:43:45 +0200

    Docstring fixes

--- src/zenml/deployers/containerized_deployer.py
+++ src/zenml/deployers/containerized_deployer.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-"""Base class for all ZenML deployers."""
+"""Base class for all containerized deployers."""
 
 from abc import ABC
 from typing import (
