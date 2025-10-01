da5a5fd2e6bd8cc9276f18ebfad9a2a7462c4619
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-10-01 10:42:37 +0200

    Remove deleted functions from __all__ export list
    Cleaned up __all__ to only include functions that are actually imported
    and available in the module. Removed references to:
    - aggregate_evaluation_results_step
    - annotate_analyses_step
    - load_recent_analyses
    - render_evaluation_report_step
    
    These functions were previously deleted but their names remained in the
    export list, which could cause import errors.

--- examples/minimal_agent_production/steps/__init__.py
+++ examples/minimal_agent_production/steps/__init__.py
@@ -17,12 +17,8 @@ from .utils import clean_text_content, extract_meaningful_summary
 
 __all__ = [
     "analyze_document_step",
-    "aggregate_evaluation_results_step",
-    "annotate_analyses_step",
     "clean_text_content",
     "extract_meaningful_summary",
     "ingest_document_step",
-    "load_recent_analyses",
     "render_analysis_report_step",
-    "render_evaluation_report_step",
 ]
