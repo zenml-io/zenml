6c7906b5707403398180e9d1ff2e3c790209b84e
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2025-10-15 21:02:19 +0100

    Update src/zenml/models/v2/core/curated_visualization.py
    Co-authored-by: Stefan Nica <stefan@zenml.io>

--- src/zenml/models/v2/core/curated_visualization.py
+++ src/zenml/models/v2/core/curated_visualization.py
@@ -144,8 +144,7 @@ class CuratedVisualizationResponseMetadata(ProjectScopedResponseMetadata):
 class CuratedVisualizationResponseResources(ProjectScopedResponseResources):
     """Response resources for curated visualizations."""
 
-    artifact_version: Optional["ArtifactVersionResponse"] = Field(
-        default=None,
+    artifact_version: "ArtifactVersionResponse" = Field(
         title="The artifact version.",
         description="Artifact version from which the visualization originates.",
     )
