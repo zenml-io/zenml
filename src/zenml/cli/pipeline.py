286ddd09443afba7584b47e825d5d849de206a55
Safoine El Khabich <34200873+safoinme@users.noreply.github.com>
2025-11-05 14:57:08 +0100

    Update src/zenml/cli/pipeline.py
    Co-authored-by: Stefan Nica <stefan@zenml.io>

--- src/zenml/cli/pipeline.py
+++ src/zenml/cli/pipeline.py
@@ -491,7 +491,7 @@ def deploy_pipeline(
         dashboard_url = get_deployment_url(deployment)
         if dashboard_url:
             cli_utils.declare(
-                f"\n✅ [bold green]View in ZenML Cloud:[/bold green] "
+                f"\n✅ [bold green]View in the ZenML UI:[/bold green] "
                 f"[link]{dashboard_url}[/link]"
             )
 
