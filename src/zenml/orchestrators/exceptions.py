f4d7c877691f5d4c9252e8985565e9ab1ea73789
Iason Andriopoulos <iason.ext@zenml.io>
2025-12-08 15:53:48 +0200

    4295: Delete leftover schedule records

---
+++ src/zenml/orchestrators/exceptions.py
@@ -0,0 +1,31 @@
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#  Copyright (c) ZenML GmbH 2025. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+#
+# Parts of the `prepare_or_run_pipeline()` method of this file are
+# inspired by the Kubernetes dag runner implementation of tfx
+"""Orchestrator-specific exceptions."""
+
+
+class PipelineSubmissionError(Exception):
+    """Raised when the orchestrator fails to submit pipeline for execution."""
+
+    pass
