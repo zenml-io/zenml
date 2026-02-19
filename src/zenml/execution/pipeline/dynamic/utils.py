965263d992f59367b2f001f749129d57a95d82fb
Michael Schuster <schustmi@users.noreply.github.com>
2026-02-19 12:36:03 +0100

    Improve dynamic pipeline step monitoring (#4369)
    * Improved dynamic pipeline step monitoring
    
    * Remove unused publish_exception_info arg

--- src/zenml/execution/pipeline/dynamic/utils.py
+++ src/zenml/execution/pipeline/dynamic/utils.py
@@ -14,16 +14,19 @@
 """Dynamic pipeline execution utilities."""
 
 import time
-from typing import (
-    Generic,
-    TypeVar,
-)
+from typing import TYPE_CHECKING, Generic, TypeVar
 from uuid import UUID
 
 from zenml.client import Client
 from zenml.enums import ExecutionStatus
 from zenml.logger import get_logger
-from zenml.models import StepRunResponse
+from zenml.models import ArtifactVersionResponse, StepRunResponse
+
+if TYPE_CHECKING:
+    from zenml.execution.pipeline.dynamic.outputs import (
+        OutputArtifact,
+        StepRunOutputs,
+    )
 
 logger = get_logger(__name__)
 
@@ -84,3 +87,113 @@ def wait_for_step_run_to_finish(step_run_id: UUID) -> "StepRunResponse":
         time.sleep(sleep_interval)
         if sleep_interval < max_sleep_interval:
             sleep_interval *= 2
+
+
+def get_latest_step_run(
+    pipeline_run_id: UUID, invocation_id: str, hydrate: bool = False
+) -> "StepRunResponse":
+    """Get the latest step run for a step.
+
+    Args:
+        pipeline_run_id: The ID of the pipeline run.
+        invocation_id: The invocation ID of the step.
+        hydrate: Whether to hydrate the step run.
+
+    Raises:
+        RuntimeError: If no step run exists for the given invocation ID.
+
+    Returns:
+        The latest step run.
+    """
+    step_runs = Client().list_run_steps(
+        pipeline_run_id=pipeline_run_id,
+        name=invocation_id,
+        exclude_retried=True,
+        size=1,
+        hydrate=hydrate,
+    )
+
+    if not step_runs:
+        raise RuntimeError(
+            f"Step `{invocation_id}` not found in pipeline run "
+            f"`{pipeline_run_id}`."
+        )
+
+    return step_runs.items[0]
+
+
+def wait_for_step_to_finish(
+    pipeline_run_id: UUID, step_name: str
+) -> "StepRunResponse":
+    """Wait until a step is finished.
+
+    Args:
+        pipeline_run_id: The ID of the pipeline run.
+        step_name: The name of the step.
+
+    Returns:
+        The finished step run.
+    """
+    sleep_interval = 1
+    max_sleep_interval = 64
+
+    while True:
+        step_run = get_latest_step_run(
+            pipeline_run_id, step_name, hydrate=False
+        )
+        # If a step is in `retrying` status, another step run will be
+        # created and we will try to pick it up in the next iteration.
+        if step_run.status not in {
+            ExecutionStatus.RUNNING,
+            ExecutionStatus.RETRYING,
+        }:
+            return step_run
+
+        logger.debug(
+            "Waiting for step `%s` to finish (current status: %s)",
+            step_name,
+            step_run.status,
+        )
+
+        time.sleep(sleep_interval)
+        if sleep_interval < max_sleep_interval:
+            sleep_interval *= 2
+
+
+def load_step_run_outputs(step_run_id: UUID) -> "StepRunOutputs":
+    """Load the outputs of a step run.
+
+    Args:
+        step_run_id: The ID of the step run.
+
+    Returns:
+        The outputs of the step run.
+    """
+    from zenml.execution.pipeline.dynamic.outputs import OutputArtifact
+
+    step_run = Client().zen_store.get_run_step(step_run_id)
+
+    def _convert_output_artifact(
+        output_name: str, artifact: "ArtifactVersionResponse"
+    ) -> "OutputArtifact":
+        return OutputArtifact(
+            output_name=output_name,
+            step_name=step_run.name,
+            **artifact.model_dump(),
+        )
+
+    output_artifacts = step_run.regular_outputs
+    if len(output_artifacts) == 0:
+        return None
+    elif len(output_artifacts) == 1:
+        name, artifact = next(iter(output_artifacts.items()))
+        return _convert_output_artifact(output_name=name, artifact=artifact)
+    else:
+        # Make sure we return them in the same order as they're defined in the
+        # step configuration, as we don't enforce any ordering in the DB.
+        return tuple(
+            _convert_output_artifact(
+                output_name=name, artifact=output_artifacts[name]
+            )
+            for name in step_run.config.outputs.keys()
+        )
