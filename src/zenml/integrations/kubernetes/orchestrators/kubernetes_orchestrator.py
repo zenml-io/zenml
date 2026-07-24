dd77bdc4bfac3d3a5741582b2b15e381b67dc135
Stefan Nica <stefan@zenml.io>
2026-07-24 20:58:39 +0200

    Remove info log

--- src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator.py
+++ src/zenml/integrations/kubernetes/orchestrators/kubernetes_orchestrator.py
@@ -1047,7 +1047,7 @@ class KubernetesOrchestrator(ContainerizedOrchestrator):
                 str(e),
             )
 
-        logger.info(
+        logger.debug(
             "Launched job for step `%s`.",
             step_run_info.pipeline_step_name,
         )
